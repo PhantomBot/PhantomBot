b583a80b1b2d162a3a1f98d6c48ae10c7ccac5c0
Branden B <scania123@users.noreply.github.com>
2016-01-29 23:42:26 -0400

    - Fixed broken updates.js

--- javascript-source/core/updates.js
+++ javascript-source/core/updates.js
@@ -11,20 +11,23 @@
  * PhantomBot v2.0
  */
 (function () {
-  if (!$.inidb.exists('settings', 'installedv2') || $.inidb.exists('settings', 'installedv2') != 'true') {
-    Packages.com.gmt2001.Console.out.println('Starting PhantomBot version 2 updates...');
-    // Delete tables from version 1.6.6.1
-    // Except points and time
+  if (!$.inidb.exists('settings', 'installedv2') || $.inidb.get('settings', 'installedv2') != 'true') {
+    Packages.com.gmt2001.Console.out.println('');
+    Packages.com.gmt2001.Console.out.println('Starting PhantomBot version 2.0 updates...');
+    Packages.com.gmt2001.Console.out.println('This can take several minutes...');
+    Packages.com.gmt2001.Console.out.println('');
+
     var tableNamesList = $.inidb.GetFileList(),
-        ignoredTables = [
-            'points',
-            'time'
-        ],
+        commandsBackup,
+        timeBackup,
+        pointsBackup,
+        i,
 
         defaultDisabledModules = [
           './games/8ball.js',
           './games/adventureSystem.js',
           './games/killCommand.js',
+          './commands/topCommand.js',
           './games/random.js',
           './games/roll.js',
           './games/roulette.js',
@@ -40,23 +43,67 @@
           './systems/raffleSystem.js',
           './systems/raidSystem.js',
           './systems/youtubePlayer.js',
-        ],
-        i;
+        ];
 
+    Packages.com.gmt2001.Console.out.println('Preparing to save commands, times and points...');
+
+    Packages.com.gmt2001.Console.out.println('Saving custom commands...');
+    commandsBackup = getTableContents('command');
+    Packages.com.gmt2001.Console.out.println('Saving times...');
+    timeBackup = getTableContents('time');
+    Packages.com.gmt2001.Console.out.println('Saving points...');
+    pointsBackup = getTableContents('points');
+
+    Packages.com.gmt2001.Console.out.println('Removing old files...');
     for (i in tableNamesList) {
-      if (ignoredTables.indexOf(tableNamesList[i]) < 0) {
-        $.inidb.RemoveFile(tableNamesList[i]);
-      }
+      $.inidb.RemoveFile(tableNamesList[i]);
     }
 
+    Packages.com.gmt2001.Console.out.println('Restoring custom commands...');
+    restoreTableContents('command', commandsBackup);
+    Packages.com.gmt2001.Console.out.println('Restoring times...');
+    restoreTableContents('time', timeBackup);
+    Packages.com.gmt2001.Console.out.println('Restoring points...');
+    restoreTableContents('points', pointsBackup);
 
-    // Disable modules that are not needed by default
+    Packages.com.gmt2001.Console.out.println('Disabling some modules...');
     for (i in defaultDisabledModules) {
       $.inidb.set('modules', defaultDisabledModules[i], 'false');
     }
 
-    // DONE
     $.inidb.set('settings', 'installedv2', 'true');
-    Packages.com.gmt2001.Console.out.println('PhantomBot version 2 updates completed...');
+    Packages.com.gmt2001.Console.out.println('PhantomBot version 2.0 updates completed...');
+    Packages.com.gmt2001.Console.out.println('Now proceeding with normal start up...');
+    return;
+  }
+
+  /**
+   * @function getTableContents
+   * @param {string} tableName
+   * @returns {Array}
+   */
+  function getTableContents(tableName) {
+    var contents = [],
+        keyList = $.inidb.GetKeyList(tableName, ''),
+        i;
+
+    for (i in keyList) {
+      contents[keyList[i]] = $.inidb.get(tableName, keyList[i]);
+    }
+
+    return contents;
+  }
+
+  /**
+   * @function setTableContents
+   * @param {string} tableName
+   * @param {Array} contents
+   */
+  function restoreTableContents(tableName, contents) {
+    var i;
+
+    for (i in contents) {
+      $.inidb.set(tableName, i, contents[i]);
+    }
   }
 })();
