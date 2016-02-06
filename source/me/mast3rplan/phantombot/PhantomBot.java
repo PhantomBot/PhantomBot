e7ea684ff90adca4d64ef33a4095531b4a38a6d2
kojitsari <kojitsari@gmail.com>
2016-02-05 18:04:11 -0600

    Update PhantomBot.java

--- source/me/mast3rplan/phantombot/PhantomBot.java
+++ source/me/mast3rplan/phantombot/PhantomBot.java
@@ -127,7 +127,8 @@ public class PhantomBot implements Listener {
     com.gmt2001.Console.out.println("PhantomBot Core 2.0.2");
     com.gmt2001.Console.out.println("Build revision " + RepoVersion.getRepoVersion());
     com.gmt2001.Console.out.println("Creator: mast3rplan");
-    com.gmt2001.Console.out.println("Developers: PhantomIndex, Kojitsari, Scania, Zelakto, SimeonF & Juraji");
+    com.gmt2001.Console.out.println("Current Developers: PhantomIndex, Kojitsari, Scania, Zelakto, SimeonF");
+    com.gmt2001.Console.out.println("Past Developers: Juraji, GloriousEggroll, GMT2001");
     com.gmt2001.Console.out.println("https://phantombot.net");
     com.gmt2001.Console.out.println();
 
