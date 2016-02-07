1e48e463bbe58e02f727d554bb66076d6a4892ba
Branden B <scania123@users.noreply.github.com>
2016-02-07 17:25:26 -0400

    - Fixed points name not updating after changing it

--- javascript-source/core/pointSystem.js
+++ javascript-source/core/pointSystem.js
@@ -70,11 +70,11 @@
     if (oldName && $.commandExists(oldName)) {
       $.unregisterChatCommand(oldName);
     }
-    if (!$.commandExists(pointNameSingle)) {
-      $.registerChatCommand('./core/pointSystem.js', pointNameSingle, 7);
+    if (!$.commandExists(pointNameSingle) && !oldName) {
+      $.registerChatCommand('./core/pointSystem.js', $.pointNameSingle, 7);
     }
-    if (!$.commandExists(pointNameMultiple)) {
-      $.registerChatCommand('./core/pointSystem.js', pointNameMultiple, 7);
+    if (!$.commandExists(pointNameMultiple) && !oldName) {
+      $.registerChatCommand('./core/pointSystem.js', $.pointNameMultiple, 7);
     }
   };
 
@@ -259,17 +259,19 @@
 
           if (actionArg1 == 'single' && actionArg2) {
             temp = $.pointNameSingle;
-            pointNameSingle = actionArg2;
+            $.pointNameSingle = actionArg2;
             $.inidb.set('pointSettings', 'pointNameSingle', $.pointNameSingle);
             registerPointCommands(temp);
+            registerPointCommands();
             $.say($.whisperPrefix(sender) + $.lang.get('pointsystem.set.name.single.success', temp, $.pointNameSingle));
             return;
           }
           if (actionArg1 == 'multiple' && actionArg2) {
             temp = $.pointNameMultiple;
-            pointNameMultiple = actionArg2;
+            $.pointNameMultiple = actionArg2;
             $.inidb.set('pointSettings', 'pointNameMultiple', $.pointNameMultiple);
             registerPointCommands(temp);
+            registerPointCommands();
             $.say($.whisperPrefix(sender) + $.lang.get('pointsystem.set.name.multiple.success', temp, $.pointNameMultiple));
             return;
           }
