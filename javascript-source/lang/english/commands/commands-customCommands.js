5838380521024358501b8e0384ad92477165e9a8
Shawn Hill (IllusionaryOne) <illusionaryone@illusionaryone.com>
2016-02-15 23:00:19 -0700

    Custom Command (touser) Fix - Modified replaceCommandTags() to properly handle (touser). Kept logic when there is no parameter to just handle touser as the sender. Now reads args[0] to get the touser and checks to see if they are in the chat.

--- javascript-source/lang/english/commands/commands-customCommands.js
+++ javascript-source/lang/english/commands/commands-customCommands.js
@@ -14,6 +14,7 @@ $.lang.register('customcommands.set.perm.error.target404', 'the command !$1 does
 $.lang.register('customcommands.set.perm.success', 'permissions for command: $1 set for group: $2 and higher.');
 $.lang.register('customcommands.set.perm.unset.success', 'All recursive permissions for the command: $1 and any of its aliases have been removed.');
 $.lang.register('customcommands.set.perm.usage', 'usage: !permcom (command name) (group id/name). Restricts usage of a command to viewers with a certain permission level.');
+$.lang.register('customcommands.set.perm.404', 'This command does not seem to exist: $1');
 $.lang.register('customcommands.set.price.error.404', 'please select a command that exists and is available to non-mods.');
 $.lang.register('customcommands.set.price.error.invalid', 'please enter a valid price, 0 or greater.');
 $.lang.register('customcommands.set.price.success', 'the price for !$1 has been set to $2 $3.');
@@ -21,3 +22,4 @@ $.lang.register('customcommands.set.price.usage', 'usage: !pricecom (command) (p
 $.lang.register('customcommands.cmds', 'commands: $1');
 $.lang.register('customcommands.edit.usage', 'usage: !editcom (command) (message)');
 $.lang.register('customcommands.edit.success', 'command !$1 has been edited!');
+$.lang.register('customcommands.touser.offline', 'Sorry, but $1 appears to be offline!');
