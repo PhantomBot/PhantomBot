4ae01b2dba6d617aa1824e75b0f5758fa6a37b0b
Shawn Hill (IllusionaryOne) <illusionaryone@illusionaryone.com>
2016-02-19 20:28:08 -0700

    Updated the Core version to 2.0.4

--- source/me/mast3rplan/phantombot/PhantomBot.java
+++ source/me/mast3rplan/phantombot/PhantomBot.java
@@ -131,7 +131,7 @@ public class PhantomBot implements Listener {
         Thread.setDefaultUncaughtExceptionHandler(com.gmt2001.UncaughtExceptionHandler.instance());
 
         com.gmt2001.Console.out.println();
-        com.gmt2001.Console.out.println("PhantomBot Core 2.0.3");
+        com.gmt2001.Console.out.println("PhantomBot Core 2.0.4");
         com.gmt2001.Console.out.println("Build revision " + RepoVersion.getRepoVersion());
         com.gmt2001.Console.out.println("Creator: mast3rplan");
         com.gmt2001.Console.out.println("Developers: PhantomIndex, Kojitsari, Scania, Zelakto, IllusionaryOne, SimeonF, & Juraji");
