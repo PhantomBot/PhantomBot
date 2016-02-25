0c8a7bf8e68f827725c1d89fc511e6dab66c3682
Branden B <scania123@users.noreply.github.com>
2016-02-24 22:09:58 -0400

    Update PhantomBot.java

--- source/me/mast3rplan/phantombot/PhantomBot.java
+++ source/me/mast3rplan/phantombot/PhantomBot.java
@@ -1171,8 +1171,8 @@ public class PhantomBot implements Listener {
                 if (arg.equalsIgnoreCase("help") || arg.equalsIgnoreCase("--help") || arg.equalsIgnoreCase("-h") || arg.equalsIgnoreCase("-?")) {
                     com.gmt2001.Console.out.println("Usage: java -Dfile.encoding=UTF-8 -jar PhantomBot.jar [printlogin] [user=<bot username>] "
                                                     + "[oauth=<bot irc oauth>] [apioauth=<editor oauth>] [clientid=<oauth clientid>] [channel=<channel to join>] "
-                                                    + "[owner=<bot owner username>] [baseport=<bot webserver port, music server will be +1>] [hostname=<custom irc server>] "
-                                                    + "[port=<custom irc port>] [msglimit30=<message limit per 30 seconds>] "
+                                                    + "[owner=<bot owner username>] [baseport=<bot webserver port, music server will be +1>] [hostname=<custom irc server>] [ghostname=<custom group chat server>] "
+                                                    + "[port=<custom irc port>] [gport=<custom group chat port>] [msglimit30=<message limit per 30 seconds>] "
                                                     + "[datastore=<DataStore type, for a list, run java -jar PhantomBot.jar storetypes>] "
                                                     + "[datastoreconfig=<Optional DataStore config option, different for each DataStore type>] "
                                                     + "[youtubekey=<youtube api key>] [webenable=<true | false>] [musicenable=<true | false>]"
