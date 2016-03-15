01682b07db347014633f5c7c4746d7ec4d71a86d
Shawn Hill (IllusionaryOne) <illusionaryone@illusionaryone.com>
2016-03-15 09:15:38 -0600

    Launch Scripts

---
+++ resources/launch.osx.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# PhantomBot Launcher - OSX
+#
+# Please run the following to launch the bot, the chmod is required only once.
+# % chmod +x launch.sh
+# % ./launch.osx.sh
+#
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+cd "$DIR"
+
+java -Dinteractive -Dfile.encoding=UTF-8 -jar PhantomBot.jar
