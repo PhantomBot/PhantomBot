0ced150547b0a334f65c8427c4962ea324e1f09f
Shawn Hill (IllusionaryOne) <illusionaryone@illusionaryone.com>
2016-03-15 09:26:15 -0600

    Launch Service Additions **launch-service.osx.sh** Moved previous launch-service.sh here with OSX fixes.  Comments on how to run.
    **launch-service.sh**
    Original version. Comments on how to run.
    
    **launch.osx.sh**
    Fixed issue in comments.

--- resources/launch-service.sh
+++ resources/launch-service.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# PhantomBot Launch Service Script - Linux
+#
+# Please run the following to launch the bot, the chmod is required only once.
+# % chmod +x launch-service.sh
+# % ./launch-service.sh
+#
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-cd "$DIR"
+cd $(dirname $(readlink -f $0))
 
 java -Dfile.encoding=UTF-8 -jar PhantomBot.jar
