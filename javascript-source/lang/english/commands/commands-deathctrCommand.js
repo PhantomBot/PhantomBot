2a6ce1f44923eb2a458fca766263f284ef324797
Shawn Hill (IllusionaryOne) <illusionaryone@illusionaryone.com>
2016-03-24 10:41:28 -0600

    Death Counter - Tracks deaths per game. - Creates ./addons/deathctr/deathctr.txt - Automatically updates the addons file when !game is used.

---
+++ javascript-source/lang/english/commands/commands-deathctrCommand.js
@@ -0,0 +1,10 @@
+$.lang.register("deathcounter.set-error", "Provide a new valid death counter value.");
+$.lang.register("deathcounter.set-success", "Death counter for $1 set to $2.");
+$.lang.register("deathcounter.add-success", "$1 has died again in $2, bringing the total to $3.");
+$.lang.register("deathcounter.sub-success", "Calling back a death, the total is now $2 in $1.");
+$.lang.register("deathcounter.sub-zero", "The death counter for $1 is already zero, can't go any lower!");
+$.lang.register("deathcounter.counter", "$1 has died $3 times in $2.");
+$.lang.register("deathcounter.none", "$1 has not died in $2....yet.");
+$.lang.register("deathcounter.reset", "Reset the death counter back to 0 from $2 for $1.");
+$.lang.register("deathcounter.reset-nil", "The death counter for $1 is already 0.");
+
