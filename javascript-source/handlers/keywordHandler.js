fa52f53576e421226c8d5b1d1a35e99e4eb4dcaf
Branden B <scania123@users.noreply.github.com>
2016-03-26 05:03:05 -0300

    - Added event logging

--- javascript-source/handlers/keywordHandler.js
+++ javascript-source/handlers/keywordHandler.js
@@ -72,6 +72,7 @@
 
                 $.inidb.set('keywords', subAction, response);
                 $.say($.whisperPrefix(sender) + $.lang.get('keywordhandler.keyword.added', subAction));
+                $.logEvent('keywordHandler.js', 100, sender + ' added keyword "' + subAction + '" with the message "' + response + '"');
                 return;
             }
 
@@ -89,6 +90,7 @@
 
                 $.inidb.del('keywords', subAction);
                 $.say($.whisperPrefix(sender) + $.lang.get('keywordhandler.keyword.removed', subAction));
+                $.logEvent('keywordHandler.js', 100, sender + ' removed the keyword "' + subAction + '"');
             }
         }
     });
