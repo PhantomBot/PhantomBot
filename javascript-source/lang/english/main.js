0dd8a1ee28fb2b65ae8d14f308a0f6094db3f7c8
Branden B <brandenbrideau1@gmail.com>
2016-03-31 17:12:27 -0300

    Update main.js

--- javascript-source/lang/english/main.js
+++ javascript-source/lang/english/main.js
@@ -178,7 +178,7 @@ $.lang.register('console.received.subscriberonly.end', 'Received an end subscrib
 $.lang.register('console.received.subscriberonly.start', 'Received a start subscribers-only mode notification from jtv');
 $.lang.register('cooldown.set.togglemodcooldown', 'Command cooldowns have been $1 for moderators.');
 $.lang.register('cooldown.set.usage', 'Usage: !cooldown [command] [time in seconds]');
-$.lang.register('cooldown.global.usage', 'Usage: !globalcooldown [time in seconds]');
+$.lang.register('cooldown.global.usage', 'Usage: !globalcooldowntime [time in seconds]');
 $.lang.register('cooldown.global.toggle', 'global cooldown has been $1.');
 $.lang.register('cooldown.per.user.toggle', 'per-user cooldown has been $1.');
 $.lang.register('cooldown.global.set', 'global cooldown set to $1 seconds.');
