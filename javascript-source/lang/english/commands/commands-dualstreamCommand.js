0de9ec91e18ea88b51fc54af3175fe7f3d248bdf
Branden B <brandenbrideau1@gmail.com>
2016-04-11 21:03:44 -0300

    Update commands-dualstreamCommand.js

--- javascript-source/lang/english/commands/commands-dualstreamCommand.js
+++ javascript-source/lang/english/commands/commands-dualstreamCommand.js
@@ -7,5 +7,7 @@ $.lang.register('dualstreamcommand.timer.enabled', 'Multi timer enabled.');
 $.lang.register('dualstreamcommand.timer.disabled', 'Multi timer disabled.');
 $.lang.register('dualstreamcommand.timerinterval.usage', 'Usage: !multi timerinterval (time in minutes)');
 $.lang.register('dualstreamcommand.timerinterval.err', 'Minimum for the timer is 5 minutes.');
-$.lang.register('dualstreamcommand.timerinterval.set', 'Timer interval set!');
+$.lang.register('dualstreamcommand.timerinterval.set', 'Timer interval set to $1 minutes!');
 $.lang.register('dualstreamcommand.link', 'http://multistre.am/');
+$.lang.register('dualstreamcommand.req.usage', 'Usage: !multi reqmessage (amount of messages)');
+$.lang.register('dualstreamcommand.reqmessages.set', 'Req message set to $1 messages!');
