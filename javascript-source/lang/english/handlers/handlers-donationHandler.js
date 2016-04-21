695685ea39df2fdc547663cdc152a64773b30592
Branden B <brandenbrideau1@gmail.com>
2016-04-20 21:16:00 -0300

    - Typo fixes

--- javascript-source/lang/english/handlers/handlers-donationHandler.js
+++ javascript-source/lang/english/handlers/handlers-donationHandler.js
@@ -3,10 +3,10 @@ $.lang.register('donationhandler.donation.newreward', 'Thank you very much (name
 $.lang.register('donationhandler.lastdonation.no-donations', 'There are presently no donations.');
 $.lang.register('donationhandler.lastdonation.404', 'Cannot find last donation!');
 $.lang.register('donationhandler.lastdonation.success', 'The last donation was from (name) in the amount of (currency)$(amount).');
-$.lang.register('donationhandler.donations.usage', 'usage: !donations (announce | reward n.n | message | rewardmessage | lastmessage)');
+$.lang.register('donationhandler.donations.usage', 'usage: !donations (announce | rewardmultiplier n.n | message | lastmessage)');
 $.lang.register('donationhandler.donations.announce.disable', 'Donations will no longer be announced.');
 $.lang.register('donationhandler.donations.announce.enable', 'Donations will be announced.');
-$.lang.register('donationhandler.donations.reward.usage', 'usage: !donations reward n.n  Set to 0 to disable');
+$.lang.register('donationhandler.donations.reward.usage', 'usage: !donations rewardmultiplier n.n  Set to 0 to disable');
 $.lang.register('donationhandler.donations.reward.success', 'The reward for donations has been set to $1 $2 per whole amount of currency donated.');
 $.lang.register('donationhandler.donations.message.usage', 'usage: !donations message [message...] Tags: (name) (amount) (currency)');
 $.lang.register('donationhandler.donations.rewardmessage.usage', 'usage: !donations rewardmessage [message...] Tags: (name) (amount) (currency) (points) (pointname)');
