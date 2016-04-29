4863a6a111b2c999823cbed65269db575b2b9708
Shawn Hill (IllusionaryOne) <illusionaryone@illusionaryone.com>
2016-04-29 13:54:10 -0600

    Core Twitter Support **.gitignore** - Ignore the classes/ directory for genTwitterTokens
    **build.xml**
    - Builds GenerateTwitterTokens.jar
    
    **libraries/twitter4j-core.4.0.4.jar**
    - Twitter4J library to provide Twitter API access
    
    **resources/gen-twitter-key.bat resources/gen-twitter-key.sh**
    - Shell Script and Windows Batch File for generating Twitter OAuth keys
    
    **source/com/illusionaryone/TwitterAPI.java**
    - Contains the API interfaces with Twitter
    
    **source/me/mast3rplan/phantombot/PhantomBot.java**
    - Loads the twitter.txt configuration file and instantiates the Twitter API if needed

---
+++ resources/gen-twitter-key.bat
@@ -0,0 +1,22 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+set first=1
+for /f "delims=" %%a in ("%comspec%") do set "compath=%%~DPa"
+PATH %PATH%;%compath%;%JAVA_HOME%\bin\
+WHERE java >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 (
+    echo You must have Java installed, please install it from http://java.com
+    pause
+    exit
+)
+FOR /f tokens^=2-5^ delims^=.-_^" %%j in ('java -version 2^>^&1') do (
+    if !first!==1 set "jver=%%j%%k%%l%%m"
+    set first=0
+)
+IF %jver% LSS 17000 (
+    echo You must have Java 7 or later installed, please update it from http://java.com
+    pause
+    exit
+)
+java -jar GenerateTwitterTokens.jar
+endlocal
---
+++ source/com/illusionaryone/TwitterAPI.java
@@ -0,0 +1,240 @@
+/* astyle --style=java --indent=spaces=4 */
+
+/*
+ * Copyright (C) 2016 www.phantombot.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.illusionaryone;
+
+import com.gmt2001.UncaughtExceptionHandler;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+import twitter4j.Status;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.util.List;
+
+/*
+ * API services provided by:
+ * Twitter4J (http://twitter4j.org/)
+ * 
+ * Copyright 2007 Yusuke Yamamoto
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * Distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/*
+ * @author illusionaryone
+ */
+
+public class TwitterAPI {
+
+    private static final TwitterAPI instance = new TwitterAPI();
+    private String username;
+    private String oauthAccessToken;
+    private String oauthAccessSecret;
+    private AccessToken accessToken = null;
+    private Twitter twitter = null;
+
+    private final String consumerKey = "B7XaZDvKqq5J4QruegBjB9iMr";
+    private final String consumerSecret = "yErkwZoHZEnqH7GrpZEybP9C25sUK1xVeWcF9KszMIsCjG6U58";
+
+    /*
+     * Instance method for Twitter API.
+     */
+    public static TwitterAPI instance() {
+        return instance;
+    }
+
+    /*
+     * Constructor for Twitter API.  Instantiates the Twitter object with the keys for PhantomBot.
+     */
+    private TwitterAPI() {
+        Thread.setDefaultUncaughtExceptionHandler(com.gmt2001.UncaughtExceptionHandler.instance());
+    }
+
+    /*
+     * Stores the username that is used for generating URLs automatically.
+     *
+     * @param  username  Twitter username
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /*
+     * Stores the access token used for authenticating the user to Twitter.
+     *
+     * @param  accessToken  Twitter provided OAuth access token.
+     */
+    public void setAccessToken(String accessToken) {
+        this.oauthAccessToken = accessToken;
+    }
+
+    /*
+     * Stores the secret token used for authenticating the user to Twitter.
+     *
+     * @param  secretToken  Twitter provided OAuth secret token.
+     */
+    public void setSecretToken(String secretToken) {
+        this.oauthAccessSecret = secretToken;
+    }
+
+    /*
+     * Authenticates with Twitter using the OAuth method.  Twitter may throw an exception which is
+     * captured and reported to the error logs.  If an error does occur, accessToken is set to null
+     * so that other methods know not to try to interact with Twitter.
+     */
+    public void authenticate() {
+        com.gmt2001.Console.debug.println("TwitterAPI::authenticate()");
+        try {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.setOAuthConsumerKey(consumerKey);
+            configurationBuilder.setOAuthConsumerSecret(consumerSecret);
+            configurationBuilder.setOAuthAccessToken(oauthAccessToken);
+            configurationBuilder.setOAuthAccessTokenSecret(oauthAccessSecret);
+
+            TwitterFactory twitterFactory = new TwitterFactory(configurationBuilder.build());
+            twitter = twitterFactory.getInstance();
+
+            accessToken = twitter.getOAuthAccessToken();
+        }
+        catch (TwitterException ex) {
+            com.gmt2001.Console.err.println("TwitterAPI::authenticate: Failed: " + ex.getMessage());
+            accessToken = null;
+        }
+    }     
+
+    /*
+     * Posts a Tweet on Twitter.  This will post to the user timeline and is the same as posting any
+     * other status update on Twitter.  If there is an error posting, an exception is logged.
+     *
+     * @param statusString The string that will be posted on Twitter.
+     * @return  Boolean  true on success and false on failure
+     */
+    public Boolean updateStatus(String statusString) {
+        if (accessToken == null) {
+            return false;
+        }
+
+        try {
+            Status status = twitter.updateStatus(statusString);
+            com.gmt2001.Console.debug.println("TwitterAPI::updateStatus: Success");
+            return true;
+        } catch (TwitterException ex) {
+            com.gmt2001.Console.err.println("TwitterAPI::updateStatus: Failed: " + ex.getMessage());
+            return false;
+        }
+    }
+
+    /*
+     * Reads the user timeline on Twitter.  This includes posts only made by the authenticated user.
+     *
+     * @return  List<status>  List of Status objects on success, null on failure.
+     */
+    public List<Status> getUserTimeline() {
+        if (accessToken == null) {
+            com.gmt2001.Console.debug.println("TwitterAPI::getUserTimeline: Access Token is NULL");
+            return null;
+        }
+
+        try {
+            com.gmt2001.Console.debug.println("TwitterAPI::getUserTimeline: Polling Data");
+            List<Status> statuses = twitter.getUserTimeline();
+            return statuses;
+        } catch (TwitterException ex) {
+            com.gmt2001.Console.err.println("TwitterAPI::getUserTimeline: Failed: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    /*
+     * Reads the home timeline on Twitter.  This includes posts made by the user, retweets, and
+     * posts made by friends.  This is essentially the screen that is seen when logging into 
+     * Twitter.
+     *
+     * @return  Boolean  true on success and false on failure
+     * @return  List<Status>  List of Status objects on success, null on failure.
+     */
+    public List<Status> getHomeTimeline() {
+        if (accessToken == null) {
+            com.gmt2001.Console.debug.println("TwitterAPI::getHomeTimeline: Access Token is NULL");
+            return null;
+        }
+
+        try {
+            com.gmt2001.Console.debug.println("TwitterAPI::getHomeTimeline: Polling Data");
+            List<Status> statuses = twitter.getHomeTimeline();
+            return statuses;
+        } catch (TwitterException ex) {
+            com.gmt2001.Console.err.println("TwitterAPI::getHomeTimeline: Failed: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    /*
+     * Reads retweets on Twitter.  This includes posts made by the user that have been retweeted by others.
+     *
+     * @param   long          The last ID that was pulled from Twitter for retweets.
+     * @return  List<Status>  List of Status objects on success, null on failure.
+     */
+    public List<Status> getRetweetsOfMe(long sinceId) {
+        if (accessToken == null) {
+            com.gmt2001.Console.debug.println("TwitterAPI::getRetweetsOfMe: Access Token is NULL");
+            return null;
+        }
+
+        try {
+            com.gmt2001.Console.debug.println("TwitterAPI::getRetweetsOfMe: Polling Data");
+            List<Status> statuses = twitter.getRetweetsOfMe();
+            return statuses;
+        } catch (TwitterException ex) {
+            com.gmt2001.Console.err.println("TwitterAPI::getRetweetsOfMe: Failed: " + ex.getMessage());
+            return null;
+        }
+    }
+
+    /*
+     * Given a status, creates the URL for that status.
+     *
+     * @param   long    Twitter status ID.
+     * @return  String  URL in the format of https://twitter.com/<username>/status/<id>
+     */
+    public String getTwitterURLFromId(long id) {
+        return new String("https://twitter.com/" + username + " /status/ " + Long.toString(id));
+    }
+}
