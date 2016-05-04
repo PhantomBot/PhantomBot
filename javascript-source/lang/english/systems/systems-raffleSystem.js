f45c4d5d0adfc2a0ab223c11196d4e1b9a49f504
Branden B <brandenbrideau1@gmail.com>
2016-05-04 19:34:16 -0300

    Update systems-raffleSystem.js

--- javascript-source/lang/english/systems/systems-raffleSystem.js
+++ javascript-source/lang/english/systems/systems-raffleSystem.js
@@ -16,3 +16,5 @@ $.lang.register('rafflesystem.msg.disabled', 'Raffle message\'s have been disabl
 $.lang.register('rafflesystem.enter.error.alreadyentered', 'you already entered the raffle.');
 $.lang.register('rafflesystem.warn', 'The raffle is about to close get your entries in by saying !$1');
 $.lang.register('rafflesystem.err.negative', 'you can not set a price in the negatives.');
+$.lang.register('rafflesystem.no.repick.false', 'winners can now only be repicked once.');
+$.lang.register('rafflesystem.no.repick.true', 'winners can now be repicked more then once.');
