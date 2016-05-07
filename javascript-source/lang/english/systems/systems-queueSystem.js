4dd10da447223b230995c71e8e9c808bcd2f8f3f
Branden B <brandenbrideau1@gmail.com>
2016-05-06 23:22:23 -0300

    Create systems-queueSystem.js

---
+++ javascript-source/lang/english/systems/systems-queueSystem.js
@@ -0,0 +1,12 @@
+$.lang.register('queuesystem.queue.closed', 'the queue is currently closed.');
+$.lang.register('queuesystem.err.missing.tag', 'You must specify your gamer tag.');
+$.lang.register('queuesystem.err.in.queue', 'you are already in the queue.');
+$.lang.register('queuesystem.err.queue.full', 'the queue is currently full.');
+$.lang.register('queuesystem.added.queue', 'you have been added to the queue!');
+$.lang.register('queuesystem.queue.cleared', 'Queue has been cleared!');
+$.lang.register('queuesystem.queue.usage', 'Usage: !queue [open / close / clear / next [amount] / maxsize]');
+$.lang.register('queuesystem.err.queue.opened', 'a queue is already opened.');
+$.lang.register('queuesystem.queue.opened', 'Queue is now opened! Use !joinqueue (gamertag) to join it!');
+$.lang.register('queuesystem.err.queue.closed', 'Queue is now closed!');
+$.lang.register('queuesystem.queue.size.usage', 'Usage: !queue maxsize (amount)');
+$.lang.register('queuesystem.queue.size.set', 'queue size set to $1 players.');
