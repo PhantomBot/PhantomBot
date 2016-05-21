73ab0cdee337859c995009e04d61189db79f2137
Branden B <scaniaTV@users.noreply.github.com>
2016-05-21 12:55:02 -0300

    Update handlers-followHandler.js

--- javascript-source/lang/english/handlers/handlers-followHandler.js
+++ javascript-source/lang/english/handlers/handlers-followHandler.js
@@ -23,3 +23,7 @@ $.lang.register('followhandler.followtraintoggle.on', 'Follow train announcement
 $.lang.register('followhandler.followtraintoggle.off', 'Follow train announcements are now disabled.');
 $.lang.register('followhandler.follow.age.err.404', '$1 is not following channel $2');
 $.lang.register('followhandler.follow.age.time', '$1 has been following channel $2 for $3');
+$.lang.register('followhandler.fixfollow.usage', 'Usage: !fixfollow (username)');
+$.lang.register('followhandler.fixfollow.error', '$1 is already in the followed list.');
+$.lang.register('followhandler.fixfollow.error.404', '$1 is not following your channel.');
+$.lang.register('followhandler.fixfollow.added', 'added $1 to the followed list!');
