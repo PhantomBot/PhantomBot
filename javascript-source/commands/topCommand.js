231ea2ff650a15ea034fa7b7544f3c84d2a033cb
Branden B <scaniaTV@users.noreply.github.com>
2016-06-25 21:23:42 -0300

    - You can now set how many users show in the !top commands

--- javascript-source/commands/topCommand.js
+++ javascript-source/commands/topCommand.js
@@ -4,6 +4,33 @@
  * Build and announce lists of top viewers (Highest points, highest time spent in the channel)
  */
 (function() {
+    var bots = ['moobot', 'nightbot', 'xanbot', 'hnlbot', 'ohbot', 'wizebot', 'vivbot', 'coebot', 'branebot', 'monstercat'], // Add your own name that you want to be in the list here.
+        amountPoints = $.getSetIniDbNumber('settings', 'topListAmountPoints', 5),
+        amountTime = $.getSetIniDbNumber('settings', 'topListAmountTime', 5);
+
+     /**
+     * @function reloadTop
+     */
+    function reloadTop() {
+        amountPoints = $.getIniDbNumber('settings', 'topListAmountPoints');
+        amountTime = $.getIniDbNumber('settings', 'topListAmountTime');
+    };
+
+    /**
+     * @function isTwitchBot
+     * @param {string} username
+     * @returns {Boolean}
+     */
+    function isTwitchBot(username) {
+        var i;
+        for (i in bots) {
+            if (bots[i].equalsIgnoreCase(username)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
     /**
      * @function getTop5
      * @param {string} iniName
@@ -15,8 +42,7 @@
             i;
 
         for (i in keys) {
-            if (!$.isBot(keys[i]) && !$.isOwner(keys[i]) && !keys[i].equalsIgnoreCase('monstercat') 
-                && !keys[i].equalsIgnoreCase('moobot') && !keys[i].equalsIgnoreCase('nightbot') && !keys[i].equalsIgnoreCase('xanbot')) {
+            if (!$.isBot(keys[i]) && !$.isOwner(keys[i]) && !isTwitchBot(keys[i])) {
                 list.push({
                     username: keys[i],
                     value: $.inidb.get(iniName, keys[i])
@@ -28,7 +54,11 @@
             return (b.value - a.value);
         });
 
-        return list.splice(0, 5);
+        if (iniName == 'points') {
+            return list.splice(0, amountPoints);
+        } else {
+            return list.splice(0, amountTime);
+        }
     };
 
     /**
@@ -36,15 +66,17 @@
      */
     $.bind('command', function(event) {
         var command = event.getCommand(),
+            args = event.getArgs(),
+            sender = event.getSender(),
             top5 = [],
             c = 1,
             temp,
             i;
 
         /**
-         * @commandpath top5 - Get the top 5 viewers in the channel based on points
+         * @commandpath top - By default it will disaplay the top5 people with the most points. This can be changed.
          */
-        if (command.equalsIgnoreCase('top5')) {
+        if (command.equalsIgnoreCase('top')) {
             if (!$.bot.isModuleEnabled('./systems/pointSystem.js')) {
                 $.say($.lang.get('top5.points-disabled'));
                 return;
@@ -57,13 +89,15 @@
                 c++;
             }
 
-            $.say($.lang.get('top5.default', $.pointNameMultiple, top5.join(', ')));
+            $.say($.lang.get('top5.default', amountPoints, $.pointNameMultiple, top5.join(', ')));
+            top5 = "";
+            list = "";//Saving memory here.
         }
 
         /**
-         * @commandpath top5time - Get the top 5 viewers in the channel based on time
+         * @commandpath toptime - By default it will disaplay the top5 people with the most time. This can be changed.
          */
-        if (command.equalsIgnoreCase('top5time')) {
+        if (command.equalsIgnoreCase('toptime')) {
             temp = getTop5('time');
 
             for (i in temp) {
@@ -71,7 +105,49 @@
                 c++;
             }
 
-            $.say($.lang.get('top5.default', 'time', top5.join(', ')));
+            $.say($.lang.get('top5.default', 'time', amountTime, top5.join(', ')));
+            top5 = "";
+            list = "";//Saving memory here.
+        }
+
+        /**
+         * @commandpath topamount - Set how many people who up in the !top points list.
+         */
+        if (command.equalsIgnoreCase('topamount')) {
+            if (!args[0]) {
+                $.say($.whisperPrefix(sender) + $.lang.get('top5.amount.points.usage'));
+                return;
+            }
+            if (args[0] > 30) {
+                $.say($.whisperPrefix(sender) + $.lang.get('top5.amount.max'));
+                return;
+            }
+
+            amountPoints = args[0];
+            $.inidb.set('settings', 'topListAmountPoints', amountPoints);
+            $.say($.whisperPrefix(sender) + $.lang.get('top5.amount.points.set', amountPoints));
+        }
+
+        /**
+         * @commandpath toptimeamount - Set how many people who up in the !toptime list.
+         */
+        if (command.equalsIgnoreCase('toptimeamount')) {
+            if (!args[0]) {
+                $.say($.whisperPrefix(sender) + $.lang.get('top5.amount.time.usage'));
+                return;
+            }
+            if (args[0] > 30) {
+                $.say($.whisperPrefix(sender) + $.lang.get('top5.amount.max'));
+                return;
+            }
+
+            amountTime = args[0];
+            $.inidb.set('settings', 'topListAmountTime', amountTime);
+            $.say($.whisperPrefix(sender) + $.lang.get('top5.amount.time.set', amountTime));
+        }
+
+        if (command.equalsIgnoreCase('reloadtop')) {
+            reloadTop();
         }
     });
 
@@ -80,8 +156,11 @@
      */
     $.bind('initReady', function() {
         if ($.bot.isModuleEnabled('./commands/topCommand.js')) {
-            $.registerChatCommand('./commands/topCommand.js', 'top5', 7);
-            $.registerChatCommand('./commands/topCommand.js', 'top5time', 7);
+            $.registerChatCommand('./commands/topCommand.js', 'top', 7);
+            $.registerChatCommand('./commands/topCommand.js', 'topamount', 1);
+            $.registerChatCommand('./commands/topCommand.js', 'toptimeamount', 1);
+            $.registerChatCommand('./commands/topCommand.js', 'reloadtop', 1);
+            $.registerChatCommand('./commands/topCommand.js', 'toptime', 7);
         }
     });
 })();
