56ca5121776916b3f8196115c4b563b07c34061d
Branden B <scaniaTV@users.noreply.github.com>
2016-06-25 21:24:11 -0300

    Update commands-top10Command.js

--- javascript-source/lang/english/commands/commands-top10Command.js
+++ javascript-source/lang/english/commands/commands-top10Command.js
@@ -1,2 +1,7 @@
-$.lang.register('top5.default', 'Top 5 $1: $2');
+$.lang.register('top5.default', 'Top $1 $2: $3');
 $.lang.register('top5.points-disabled', 'Points are disabled.');
+$.lang.register('top5.amount.points.usage', 'usage: !topamount (amount) - Set how many people who up in the !top points list.');
+$.lang.register('top5.amount.max', 'the max amount is 30 users.');
+$.lang.register('top5.amount.points.set', '$1 users will now how in the !top command.');
+$.lang.register('top5.amount.time.usage', 'usage: !toptimeamount (amount) - Set how many people who up in the !toptime list.');
+$.lang.register('top5.amount.time.set', '$1 users will now how in the !toptime command.');
