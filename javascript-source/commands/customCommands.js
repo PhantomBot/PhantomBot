33e0afb9008896fa73ebbdc9d3e6571b8845d7d1
scaniaTV <scaniaTV@users.noreply.github.com>
2016-06-28 06:34:03 -0300

    - Added a countdown command tag

--- javascript-source/commands/customCommands.js
+++ javascript-source/commands/customCommands.js
@@ -5,7 +5,7 @@
         reCustomAPIJson = new RegExp(/\(customapijson ([\w\.:\/\$=\?\&]+)\s([\w\W]+)\)/), // URL[1], JSONmatch[2..n]
         reCustomAPITextTag = new RegExp(/{([\w\W]+)}/),
         reCommandTag = new RegExp(/\(command\s([\w]+)\)/),
-        tagCheck = new RegExp(/\(age\)|\(sender\)|\(@sender\)|\(baresender\)|\(random\)|\(1\)|\(count\)|\(pointname\)|\(price\)|\(#\)|\(uptime\)|\(follows\)|\(game\)|\(status\)|\(touser\)|\(echo\)|\(alert [,.\w]+\)|\(readfile/);
+        tagCheck = new RegExp(/\(age\)|\(sender\)|\(@sender\)|\(baresender\)|\(random\)|\(1\)|\(count\)|\(pointname\)|\(price\)|\(#\)|\(uptime\)|\(follows\)|\(game\)|\(status\)|\(touser\)|\(echo\)|\(alert [,.\w]+\)|\(readfile|\(1=|\(countdown=/);
 
     /**
      * @function getCustomAPIValue
@@ -67,6 +67,18 @@
                 message = $.replace(message, t, event.getArgs()[0]);
             }
             message = $.replace(message, '(1=', '(');
+        }
+
+        if (message.match(/\(countdown=[^)]+\)/g)) {
+            var t = message.match(/\([^)]+\)/)[0],
+                down,
+                time;
+
+            down = t.replace('(countdown=', '').replace(')', '');
+
+            time = (Date.parse(down) - Date.parse(new Date()));
+
+            message = $.replace(message, t, $.getTimeString(time / 1000));
         }
 
         if (message.match(/\(sender\)/g)) {
