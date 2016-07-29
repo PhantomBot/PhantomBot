745d74a37798732178412eda6a0281b9cd456704
IllusionaryOne <illusionaryone@illusionaryone.com>
2016-07-29 16:45:08 -0600

    Enhanced Reconnect Logic **TwitchWSIRC.java** - Always try to reconnect, even if local disconnection would be caused by an error in the WebSocket. - Close the WebSocket when delete() is called - pass a custom String caught by onClose()

--- source/me/mast3rplan/phantombot/twitchwsirc/TwitchWSIRC.java
+++ source/me/mast3rplan/phantombot/twitchwsirc/TwitchWSIRC.java
@@ -113,6 +113,7 @@ public class TwitchWSIRC extends WebSocketClient {
     public void delete() {
         if (instances.containsKey(this.channelName)) {
             instances.remove(this.channelName);
+            this.close(9999, "Destroyed Instance");
         }
     }
 
@@ -166,15 +167,17 @@ public class TwitchWSIRC extends WebSocketClient {
      */
     @Override
     public void onClose(int code, String reason, boolean remote) {
+        if (reason.equals("Destroyed Instance")) {
+            return;
+        }
+
         if (remote) {
             com.gmt2001.Console.out.println("Disconnected from Twitch WS-IRC Server due to Remote Hangup.");
         } else {
             com.gmt2001.Console.out.println("Disconnected from Twitch WS-IRC Server.");
         }
         com.gmt2001.Console.debug.println("Code [" + code + "] Reason [" + reason + "] Remote Hangup [" + remote + "]");
-        if (remote) {
-            session.reconnect();
-        }
+        session.reconnect();
     }
 
     /*
