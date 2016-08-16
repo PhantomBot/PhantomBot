ee397c6c44e0725eee2eb6e9244a2b7585b4e85b
Branden B <scaniaTV@users.noreply.github.com>
2016-08-16 19:02:50 -0300

    im done...

--- javascript-source/systems/pointSystem.js
+++ javascript-source/systems/pointSystem.js
@@ -791,8 +791,10 @@
             $.registerChatSubcommand('points', 'add', 1);
             $.registerChatSubcommand('points', 'give', 1);
             $.registerChatSubcommand('points', 'take', 1);
+            $.registerChatSubcommand('points', 'remove', 1);
             $.registerChatSubcommand('points', 'set', 1);
             $.registerChatSubcommand('points', 'all', 1);
+            $.registerChatSubcommand('points', 'takeall', 1);
             $.registerChatSubcommand('points', 'setname', 1);
             $.registerChatSubcommand('points', 'setgain', 1);
             $.registerChatSubcommand('points', 'setofflinegain', 1);
