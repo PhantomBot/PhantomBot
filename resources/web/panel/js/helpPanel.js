94bd77d183cd7bf385c87719fb205811cfc197ca
IllusionaryOne <illusionaryone@illusionaryone.com>
2016-10-10 12:32:40 -0600

    Added Reconnecting WebSocket Support to Panel
    **reconnectingWS.min.js**
    - Third-party API for providing Reconnecting WebSockets
    
    **index.html**
    - Loads the reconnectingWS.min.js file
    
    **helpPanel.js**
    - Provide information that a new API is used
    
    **panelUtils.js**
    - Updated to use the ReconnectingWebSocket class
    - Updated the disconnect message to indicate that a retry loop is going every 5 seconds

--- resources/web/panel/js/helpPanel.js
+++ resources/web/panel/js/helpPanel.js
@@ -58,6 +58,7 @@
                                   "    Ion.Sound 3.0.7<br>" +
                                   "    FlotCharts 0.8.3<br>" +
                                   "    FooTable 3.0.10<br>" +
+                                  "    ReconnectingWebsocket<br>" +
                                   "</small>");
         }
     }
