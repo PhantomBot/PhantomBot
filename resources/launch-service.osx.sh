b7c1bbcebf36eb961db33fd70fd8bff7d56fa697
Zackery M <zelakto@zelakto.tv>
2016-11-07 00:28:08 -0500

    Update launch-service.osx.sh

--- resources/launch-service.osx.sh
+++ resources/launch-service.osx.sh
@@ -18,4 +18,20 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd "$DIR"
 
 unset DISPLAY
-java -Dfile.encoding=UTF-8 -jar PhantomBot.jar
+
+if type -p java; then
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    _java="$JAVA_HOME/bin/java"
+else
+    echo "You don't have Java installed! Download it from https://www.java.com/en/download/"
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    if [[ "$version" > "1.8" ]]; then
+        java -Dfile.encoding=UTF-8 -jar PhantomBot.jar
+    else
+        echo Your Java is out of date! Please download Java 8 at https://www.java.com/en/download/
+    fi
+fi
