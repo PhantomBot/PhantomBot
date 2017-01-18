c8c588f2e5183b1426e93f3f6564863e620bb345
ScaniaTV <scaniatv@users.noreply.github.com>
2017-01-18 00:15:02 -0400

    Added ability to trigger audio hooks via custom command tags

--- javascript-source/commands/customCommands.js
+++ javascript-source/commands/customCommands.js
@@ -5,7 +5,7 @@
         reCustomAPIJson = new RegExp(/\(customapijson ([\w\.:\/\$=\?\&]+)\s([\w\W]+)\)/), // URL[1], JSONmatch[2..n]
         reCustomAPITextTag = new RegExp(/{([\w\W]+)}/),
         reCommandTag = new RegExp(/\(command\s([\w]+)\)/),
-        tagCheck = new RegExp(/\(age\)|\(sender\)|\(@sender\)|\(baresender\)|\(random\)|\(1\)|\(count\)|\(pointname\)|\(price\)|\(#\)|\(uptime\)|\(follows\)|\(game\)|\(status\)|\(touser\)|\(echo\)|\(alert [,.\w]+\)|\(readfile|\(1=|\(countdown=|\(downtime\)|\(paycom\)|\(onlineonly\)|\(offlineonly\)|\(code=|\(followage\)|\(gameinfo\)|\(titleinfo\)|\(gameonly=|\(playtime\)|\(gamesplayed\)|\(pointtouser\)|\(lasttip\)|\(writefile .+\)|\(customapi |\(customapijson /);
+        tagCheck = new RegExp(/\(age\)|\(sender\)|\(@sender\)|\(baresender\)|\(random\)|\(1\)|\(count\)|\(pointname\)|\(price\)|\(#\)|\(uptime\)|\(follows\)|\(game\)|\(status\)|\(touser\)|\(echo\)|\(alert [,.\w]+\)|\(readfile|\(1=|\(countdown=|\(downtime\)|\(paycom\)|\(onlineonly\)|\(offlineonly\)|\(code=|\(followage\)|\(gameinfo\)|\(titleinfo\)|\(gameonly=|\(playtime\)|\(gamesplayed\)|\(pointtouser\)|\(lasttip\)|\(writefile .+\)|\(playsound |\(customapi |\(customapijson /);
 
     /**
      * @function getCustomAPIValue
@@ -278,6 +278,11 @@
 
         if (message.match(/\(lasttip\)/g)) {
             message = $.replace(message, '(lasttip)', ($.inidb.exists('donations', 'last_donation_message') ? $.inidb.get('donations', 'last_donation_message') : 'No donations found.'));
+        }
+
+        if (message.match(/\(playsound/g)) {
+            message = $.replace(message, message.match(/\(playsound ([a-zA-Z_]+)\)/)[0], '');
+            $.panelsocketserver.triggerAudioPanel(message.match(/\(playsound ([a-zA-Z_]+)\)/)[1]);
         }
 
         if (message.match(/\(age\)/g)) {
