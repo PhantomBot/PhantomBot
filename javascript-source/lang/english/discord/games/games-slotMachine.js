1a5c6dde63bd4173e342aac64b5f997345d3c6b1
ScaniaTV <scaniatv@users.noreply.github.com>
2017-03-11 23:55:11 -0400

    Gamble command for Discord and few changes/fixes part 2

---
+++ javascript-source/lang/english/discord/games/games-slotMachine.js
@@ -0,0 +1,4 @@
+$.lang.register('discord.slotmachine.result.start', '$1: [ $2 $3 $4 ] ');
+$.lang.register('discord.slotmachine.result.win', ' +$1 ');
+$.lang.register('discord.slotmachine.rewards.usage', 'Usage: !slot rewards [val1] [val2] [val3] [val4] [val5]. Currently: $1');
+$.lang.register('discord.slotmachine.rewards.success', 'Updated rewards for the slot machine.');
