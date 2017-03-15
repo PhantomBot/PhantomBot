d03fcedbe824ee7be5628ea8018a930beabb956b
gmt2001 <garymt2001@hotmail.com>
2017-03-15 04:54:49 -0500

    Fixed broken accountLink stuff

--- javascript-source/discord/systems/pointSystem.js
+++ javascript-source/discord/systems/pointSystem.js
@@ -1,47 +1,15 @@
-<<<<<<< HEAD
-=======
-/**
- * pointSystem.js
- *
- * Manage user loyalty points and export and API to manipulate points in other modules
- * Use the $ API
- */
->>>>>>> eaaab3813091c258517522b05f1c272a39c65993
 (function () {
 
     /**
      * @event discordCommand
      */
-<<<<<<< HEAD
-    $.bind('discordCommand', function(event) {
-=======
     $.bind('discordCommand', function (event) {
->>>>>>> eaaab3813091c258517522b05f1c272a39c65993
         var sender = event.getSender(),
             command = event.getCommand(),
             channel = event.getChannel(),
             mention = event.getMention(),
             args = event.getArgs(),
             action = args[0],
-<<<<<<< HEAD
-            twitchName = $.discord.resolveTwitchName(event.getSenderId());
-
-        /**
-         * @discordcommandpath points - Tells you how many points you have if you linked in you Twitch account.
-         */
-        if (command.equalsIgnoreCase('points')) {
-            if (action === undefined) {
-                if (twitchName !== null) {
-                    $.discord.say(channel, $.discord.userPrefix(mention) + $.lang.get('discord.pointsystem.self.points', $.getPointsString($.getUserPoints(twitchName)), $.getTimeString($.getUserTime(twitchName)), $.resolveRank(twitchName)));
-                } else {
-                    $.discord.say(channel, $.discord.userPrefix(mention) + $.lang.get('discord.accountlink.linkrequired'));
-                }
-            } else if ($.user.isKnown(action.toLowerCase())) {
-                twitchName = action.replace('@', '').toLowerCase();
-                $.discord.say(channel, $.discord.userPrefix(mention) + $.lang.get('discord.pointsystem.other.points', twitchName, $.getPointsString($.getUserPoints(twitchName)), $.getTimeString($.getUserTime(twitchName)), $.resolveRank(twitchName)));
-            } else {
-                $.discord.say(channel, $.discord.userPrefix(mention) + $.lang.get('discord.pointsystem.no.points.other', $.pointNameMultiple));
-=======
             userId = event.getSenderId(),
             twitchName = $.discord.discordToTwitch(userId);
 
@@ -60,7 +28,6 @@
             } else if (action.equalsIgnoreCase("fix")) {
                 $.discord.registerCommand('./discord/systems/pointSystem.js', $.pointNameSingle, 0);
                 $.discord.registerCommand('./discord/systems/pointSystem.js', $.pointNameMultiple, 0);
->>>>>>> eaaab3813091c258517522b05f1c272a39c65993
             }
         }
     });
@@ -68,14 +35,6 @@
     /**
      * @event initReady
      */
-<<<<<<< HEAD
-    $.bind('initReady', function() {
-        if ($.bot.isModuleEnabled('./discord/systems/pointSystem.js')) {
-            $.discord.registerCommand('./discord/systems/pointSystem.js', 'points', 0);
-        }
-    });
-})();
-=======
     $.bind('initReady', function () {
         if ($.bot.isModuleEnabled('./discord/systems/pointSystem.js')) {
             $.discord.registerCommand('./discord/systems/pointSystem.js', 'points', 0);
@@ -85,5 +44,3 @@
         }
     });
 })();
-
->>>>>>> eaaab3813091c258517522b05f1c272a39c65993
