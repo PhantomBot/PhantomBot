0f0afb6afa4a35d9be25b99c3535dbb2bcb322df
ScaniaTV <scaniatv@users.noreply.github.com>
2017-04-19 16:44:47 -0300

    Fixed issue with the follow event stopping randomly

--- javascript-source/handlers/followHandler.js
+++ javascript-source/handlers/followHandler.js
@@ -11,21 +11,14 @@
  *
  */
 (function() {
-    var lastFollowTime = 0,
-        followTrain = 0,
-        announceFollows = false,
+    var followToggle = $.getSetIniDbBoolean('settings', 'followToggle', false),
         followReward = $.getSetIniDbNumber('settings', 'followReward', 0),
         followMessage = $.getSetIniDbString('settings', 'followMessage', $.lang.get('followhandler.follow.message')),
-        followToggle = $.getSetIniDbBoolean('settings', 'followToggle', false),
-        followTrainToggle = $.getSetIniDbBoolean('settings', 'followTrainToggle', false),
         followDelay = $.getSetIniDbNumber('settings', 'followDelay', 5),
-        follows = [],
-        lastFollow = 0,
-        timer = false,
-        running = false,
-        timeout = false,
-        interval;
-
+        followQueue = new java.util.concurrent.ConcurrentLinkedQueue,
+        lastFollow = $.systemTime(),
+        announceFollows = false;
+        
     /*
      * @function updateFollowConfig
      */
@@ -37,73 +30,12 @@
     }
 
     /*
-     * @function checkFollowTrain
+     * @function runFollows
      */
-    function followTrainCheck() {
-        if (followTrainToggle && ($.systemTime() - lastFollowTime) <= 3e5) {
-            if (followTrain > 1) {
-                if (followTrain == 3) {
-                    $.say($.lang.get('followhandler.followtrain.triple'));
-                } else if (followTrain == 4) {
-                    $.say($.lang.get('followhandler.followtrain.quad'));
-                } else if (followTrain == 5) {
-                    $.say($.lang.get('followhandler.followtrain.penta'));
-                } else if (followTrain > 5 && followTrain <= 10) {
-                    $.say($.lang.get('followhandler.followtrain.mega', followTrain));
-                } else if (followTrain > 10 && followTrain <= 20) {
-                    $.say($.lang.get('followhandler.followtrain.ultra', followTrain));
-                } else if (followTrain > 20) {
-                    $.say($.lang.get('followhandler.followtrain.unbelievable', followTrain));
-                }
-            }
-            followTrain = 0;
-            lastFollowTime = 0;
-            timeout = false;
-        }
-    }
-
-    /*
-     * @function runFollow
-     */
-    function runFollow(message) {
-        if (!timer && (followDelay == 0 || ($.systemTime() - lastFollow) > (followDelay * 1e3))) {
-            $.say(message);
+    function runFollows() {
+        if (!followQueue.isEmpty() && (lastFollow + (followDelay * 1e3)) < $.systemTime()) {
+            $.say(followQueue.poll());
             lastFollow = $.systemTime();
-        } else {
-            follows.push(message);
-            if (!timer) {
-                timer = true;
-                setTimeout(function() { 
-                    interval = setInterval(function() { 
-                        run(); 
-                    }, 3e2); 
-                }, 1e2);
-            }
-        }
-    }
-
-    /**
-     * Used for the follow delay
-     *
-     * @function run
-     */
-    function run() {
-        var i, s;
-        if (follows.length == 0) {
-            clearInterval(interval);
-            lastFollow = 0;
-            timer = false;
-            return;
-        }
-
-        for (i in follows) {
-            if (!running && ($.systemTime() - lastFollow) > (followDelay * 1e3)) {
-                running = true;
-                $.say(follows[i].replace(/^\/w/, ' /w'));
-                lastFollow = $.systemTime();
-                follows.splice(i, 1);
-                running = false;
-            }
         }
     }
 
@@ -111,70 +43,42 @@
      * @event twitchFollowsInitialized
      */
     $.bind('twitchFollowsInitialized', function() {
-        if (!$.bot.isModuleEnabled('./handlers/followHandler.js')) {
-            return;
-        }
-
         $.consoleLn('>> Enabling follower announcements');
-        $.log.event('Follow announcements enabled');
+
         announceFollows = true;
     });
 
-    /**
-     * Gets the new follow events from the core.
-     *
+    /*
      * @event twitchFollow
      */
     $.bind('twitchFollow', function(event) {
         var follower = event.getFollower(),
             s = followMessage;
+        
+        if (announceFollows === true && followToggle === true) {
+            if (!$.inidb.exists('follows', follower)) {
 
-        if ($.bot.isModuleEnabled('./commands/lastseenCommand.js')) {
-            $.inidb.set('lastseen', follower, $.systemTime());
-        }
-
-        if ($.bot.isModuleEnabled('./handlers/followHandler.js')) {
-            if (!$.inidb.exists('followed', follower) && followToggle) {
-                if (announceFollows) {
-                    runFollow(s.replace('(name)', $.username.resolve(follower)).replace('(reward)', $.getPointsString(followReward)));
-                    followTrain++;
-
-                    if (!timeout) {
-                        timeout = true;
-                        setTimeout(function() {
-                            followTrainCheck();
-                        }, 8e3);
-                    }
-                    lastFollowTime = $.systemTime();
-                    $.inidb.set('streamInfo', 'lastFollow', $.username.resolve(follower));
+                if (s.match(/\(name\)/)) {
+                    s = $.replace(s, '(name)', $.username.resolve(follower));
                 }
-                
+
+                if (s.match(/\(reward\)/)) {
+                    s = $.replace(s, '(reward)', $.getPointsString(followReward));
+                }
+
+                if (s.match(/^\/w/)) {
+                    s = s.replace('/w', ' /w');
+                }
+
+                followQueue.add(s);
+
                 if (followReward > 0) {
                     $.inidb.incr('points', follower, followReward);
                 }
-
-                $.writeToFile($.username.resolve(follower) + ' ', './addons/followHandler/latestFollower.txt', false);
+                
+                $.writeToFile(follower + ' ', './addons/followHandler/latestFollower.txt', false);
+                $.inidb.set('streamInfo', 'lastFollow', follower);
             }
-        }
-        
-        $.inidb.setAutoCommit(false);
-        $.setIniDbBoolean('followed', follower, true);
-        $.inidb.setAutoCommit(true);
-    });
-
-    /*
-     * @event twitchUnfollow
-     */
-    $.bind('twitchUnfollow', function(event) {
-        if (!$.bot.isModuleEnabled('./handlers/followHandler.js')) {
-            return;
-        }
-
-        var follower = event.getFollows().toLowerCase(),
-            followed = $.getIniDbBoolean('followed', follower, false);
-
-        if (followed) {
-            $.setIniDbBoolean('followed', follower, false);
         }
     });
 
@@ -182,10 +86,9 @@
      * @event command
      */
     $.bind('command', function(event) {
-        var sender = event.getSender().toLowerCase(),
+        var sender = event.getSender(),
             command = event.getCommand(),
             args = event.getArgs(),
-            argString = event.getArguments(),
             action = args[0];
 
         /*
@@ -211,7 +114,7 @@
                 return;
             }
 
-            followMessage = argString;
+            followMessage = args.slice(0).join(' ');
             $.inidb.set('settings', 'followMessage', followMessage);
             $.say($.whisperPrefix(sender) + $.lang.get('followhandler.set.followmessage.success', followMessage));
         }
@@ -220,7 +123,7 @@
          * @commandpath followdelay [message] - Set the delay in seconds between follow announcements
          */
         if (command.equalsIgnoreCase('followdelay')) {
-            if (action === undefined || isNaN(parseInt(action)) || parseInt(action) < 5) {
+            if (isNaN(parseInt(action)) || parseInt(action) < 5) {
                 $.say($.whisperPrefix(sender) + $.lang.get('followhandler.set.followdelay.usage'));
                 return;
             }
@@ -237,15 +140,6 @@
             followToggle = !followToggle;
             $.setIniDbBoolean('settings', 'followToggle', followToggle);
             $.say($.whisperPrefix(sender) + (followToggle ? $.lang.get('followhandler.followtoggle.on') : $.lang.get('followhandler.followtoggle.off')));
-        }
-
-        /*
-         * @commandpath followtaintoggle - Enable or disable the follow train anouncements
-         */
-        if (command.equalsIgnoreCase('followtraintoggle')) {
-            followTrainToggle = !followTrainToggle;
-            $.setIniDbBoolean('settings', 'followTrainToggle', followTrainToggle);
-            $.say($.whisperPrefix(sender) + (followTrainToggle ? $.lang.get('followhandler.followtraintoggle.on') : $.lang.get('followhandler.followtraintoggle.off')))
         }
 
         /*
@@ -296,15 +190,15 @@
      * @event initReady
      */
     $.bind('initReady', function() {
-        if ($.bot.isModuleEnabled('./handlers/followHandler.js')) {
-            $.registerChatCommand('./handlers/followHandler.js', 'followreward', 1);
-            $.registerChatCommand('./handlers/followHandler.js', 'followtoggle', 1);
-            $.registerChatCommand('./handlers/followHandler.js', 'followtraintoggle', 1);
-            $.registerChatCommand('./handlers/followHandler.js', 'followdelay', 1);
-            $.registerChatCommand('./handlers/followHandler.js', 'followmessage', 1);
-            $.registerChatCommand('./handlers/followHandler.js', 'checkfollow', 2);
-            $.registerChatCommand('./handlers/followHandler.js', 'shoutout', 2);
-        }
+        $.registerChatCommand('./handlers/followHandler.js', 'followreward', 1);
+        $.registerChatCommand('./handlers/followHandler.js', 'followtoggle', 1);
+        $.registerChatCommand('./handlers/followHandler.js', 'followtraintoggle', 1);
+        $.registerChatCommand('./handlers/followHandler.js', 'followdelay', 1);
+        $.registerChatCommand('./handlers/followHandler.js', 'followmessage', 1);
+        $.registerChatCommand('./handlers/followHandler.js', 'checkfollow', 2);
+        $.registerChatCommand('./handlers/followHandler.js', 'shoutout', 2);
+
+        setInterval(function() { runFollows(); }, 2e3);
     });
 
     $.updateFollowConfig = updateFollowConfig;
