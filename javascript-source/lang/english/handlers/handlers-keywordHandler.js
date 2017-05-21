0289cb6450334556ab6f530f67656dbd9dfaf130
kojitsari <kojitsari@gmail.com>
2017-05-21 08:13:00 -0500

    Grammar and typo fixes

--- javascript-source/lang/english/handlers/handlers-keywordHandler.js
+++ javascript-source/lang/english/handlers/handlers-keywordHandler.js
@@ -5,8 +5,8 @@ $.lang.register('keywordhandler.keyword.removed', 'keyword "$1" has been removed
 $.lang.register('keywordhandler.keyword.usage', 'Usage: !keyword [add / remove / price / cooldown] [keyword]');
 $.lang.register('keywordhandler.remove.usage', 'Usage: !keyword remove [keyword]');
 $.lang.register('keywordhandler.cooldown.usage', 'Usage: !keyword cooldown (keyword) (seconds) - Use -1 to remove a cooldown.');
-$.lang.register('keywordhandler.cooldown.removed', 'cooldown for key $1 has been removed.');
-$.lang.register('keywordhandler.cooldown.set', 'cooldown for key: $1 has been set to $2 seconds.');
+$.lang.register('keywordhandler.cooldown.removed', 'Cooldown for keyword $1 has been removed.');
+$.lang.register('keywordhandler.cooldown.set', 'Cooldown for keyword $1 has been set to $2 seconds.');
 $.lang.register('keywordhandler.price.usage', 'Usage: !keyword price (keyword) (cost)');
-$.lang.register('keywordhandler.price.removed', 'price for keyword: $1 has been removed.');
-$.lang.register('keywordhandler.price.set', 'price for keyword: $1 has been set to $2.');
+$.lang.register('keywordhandler.price.removed', 'Price for keyword $1 has been removed.');
+$.lang.register('keywordhandler.price.set', 'Price for keyword: $1 has been set to $2.');
