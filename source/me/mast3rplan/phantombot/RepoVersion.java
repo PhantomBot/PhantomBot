53282b9f5468164316545bf7124739acfee34b4e
Damian Staszewski <damian@stachuofficial.pl>
2017-06-01 04:24:20 +0200

    reformatting cote into 4 spaces indent

--- source/me/mast3rplan/phantombot/RepoVersion.java
+++ source/me/mast3rplan/phantombot/RepoVersion.java
@@ -18,46 +18,42 @@ package me.mast3rplan.phantombot;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Enumeration;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 public class RepoVersion {
-  
-  private static final boolean nightlyBuild = false;
-  
-  private RepoVersion() {}
-  
-  protected static String getValue(String key) {
-    try {
-      String path = PhantomBot.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-      JarFile jar = new JarFile(new File(path));
-      Manifest mf = new Manifest(jar.getManifest());
-      Attributes attrib = mf.getMainAttributes();
-      String value = attrib.getValue(key);
-      if(value != null) {
-        return value;
-      }
-    } catch (IOException e) {
-      com.gmt2001.Console.err.printStackTrace(e);
+    
+    private static final boolean nightlyBuild = false;
+    
+    private RepoVersion() {
     }
-    return null;
-  }
-  
-  public static String getPhantomBotVersion() {
-    return getValue("Bundle-Version");
-  }
-  
-  public static String getRepoVersion() {
-    return getValue("Bundle-Revision");
-  }
-  
-  public static boolean getNightlyBuild() {
-    return nightlyBuild;
-  }
+    
+    protected static String getValue(String key) {
+        try {
+            String path = PhantomBot.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            JarFile jar = new JarFile(new File(path));
+            Manifest mf = new Manifest(jar.getManifest());
+            Attributes attrib = mf.getMainAttributes();
+            String value = attrib.getValue(key);
+            if (value != null) {
+                return value;
+            }
+        } catch (IOException e) {
+            com.gmt2001.Console.err.printStackTrace(e);
+        }
+        return null;
+    }
+    
+    public static String getPhantomBotVersion() {
+        return getValue("Bundle-Version");
+    }
+    
+    public static String getRepoVersion() {
+        return getValue("Bundle-Revision");
+    }
+    
+    public static boolean getNightlyBuild() {
+        return nightlyBuild;
+    }
 }
