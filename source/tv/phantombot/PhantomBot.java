ef621d5f654519369807b50c900f35804d21755f
ScaniaTV <scaniatv@users.noreply.github.com>
2017-08-14 02:19:49 -0300

    - Added the kill method for the onExit events for session.java

--- source/tv/phantombot/PhantomBot.java
+++ source/tv/phantombot/PhantomBot.java
@@ -1122,7 +1122,7 @@ public final class PhantomBot implements Listener {
 
         /* Gonna need a way to pass this to all channels */
         if (PhantomBot.getSession(this.channelName) != null) {
-            PhantomBot.getSession(this.channelName).setAllowSendMessages(false);
+            PhantomBot.getSession(this.channelName).kill();
         }
 
         /* Shutdown all caches */
