0f74a5846ace0e76cf4d0d717a4f965a60145f7a
ScaniaTV <scaniatv@users.noreply.github.com>
2017-09-17 22:02:55 -0300

    Added support for streamelements

---
+++ javascript-source/handlers/streamElementsHandler.js
@@ -0,0 +1,209 @@
+/*
+ * streamElementsHandler.js
+ *
+ * Gets donation from the StreamElements API and posts them in Twitch chat.
+ */
+(function() {
+    var message = $.getSetIniDbString('streamElementsHandler', 'message', $.lang.get('streamElements.donation.new')),
+        toggle = $.getSetIniDbBoolean('streamElementsHandler', 'toggle', false),
+        reward = $.getSetIniDbFloat('streamElementsHandler', 'reward', 0),
+        group = $.getSetIniDbBoolean('streamElementsHandler', 'group', false),
+        groupMin = $.getSetIniDbNumber('streamElementsHandler', 'groupMin', 5),
+        dir = './addons/streamElements',
+        announce = false;
+
+    /*
+     * @function reloadStreamElements
+     */
+    function reloadStreamElements() {
+        message = $.getIniDbString('streamElementsHandler', 'message');
+        toggle = $.getIniDbBoolean('streamElementsHandler', 'toggle');
+        reward = $.getIniDbFloat('streamElementsHandler', 'reward');
+        group = $.getIniDbBoolean('streamElementsHandler', 'group');
+        groupMin = $.getIniDbNumber('streamElementsHandler', 'groupMin');
+    }
+
+    /*
+     * @event streamElementsDonationInitialized
+     */
+    $.bind('streamElementsDonationInitialized', function(event) {
+        if (!$.isDirectory(dir)) {
+            $.consoleDebug('>> Creating the StreamElements Handler Directory: ' + dir);
+            $.mkDir(dir);
+        }
+
+        $.consoleLn('>> Enabling StreamElements donation announcements');
+        $.log.event('StreamElements donation announcements enabled');
+        announce = true;
+    });
+
+    /*
+     * @event streamElementsDonation
+     */
+    $.bind('streamElementsDonation', function(event) {
+        var jsonString = event.getJsonString(),
+            JSONObject = Packages.org.json.JSONObject,
+            donationObj = new JSONObject(jsonString),
+            donationID = donationObj.getString('_id'),
+            paramObj = donationObj.getJSONObject('donation'),
+            donationUsername = paramObj.getJSONObject('user').getString('username'),
+            donationCurrency = paramObj.getString('currency'),
+            donationMessage = (paramObj.has('message') ? paramObj.getString('message') : ''),
+            donationAmount = paramObj.getInt('amount'),
+            s = message;
+
+        if ($.inidb.exists('donations', donationID)) {
+            return;
+        }
+
+        $.inidb.set('streamInfo', 'lastDonator', donationUsername);
+
+        $.inidb.set('donations', donationID, donationObj);
+
+        $.inidb.set('donations', 'last_donation', donationID);
+
+        $.inidb.set('donations', 'last_donation_message', $.lang.get('main.donation.last.tip.message', donationUsername, donationCurrency, donationAmount));
+
+        $.writeToFile(donationUsername + ": " + donationAmount + " ", dir + '/latestDonation.txt', false);
+
+        if (toggle === true && announce === true) {
+            if (s.match(/\(name\)/)) {
+                s = $.replace(s, '(name)', donationUsername);
+            }
+
+            if (s.match(/\(currency\)/)) {
+                s = $.replace(s, '(currency)', donationCurrency);
+            }
+
+            if (s.match(/\(amount\)/)) {
+                s = $.replace(s, '(amount)', parseInt(donationAmount.toFixed(2)));
+            }
+
+            if (s.match(/\(amount\.toFixed\(0\)\)/)) {
+                s = $.replace(s, '(amount.toFixed(0))', parseInt(donationAmount.toFixed(0)));
+            }
+
+            if (s.match(/\(message\)/)) {
+                s = $.replace(s, '(message)', donationMessage);
+            }
+
+            if (s.match(/\(reward\)/)) {
+                s = $.replace(s, '(reward)', $.getPointsString(Math.floor(parseFloat(donationAmount) * reward)));
+            }
+
+            $.say(s);
+        }
+
+        if (reward > 0) {
+            $.inidb.incr('points', donationUsername.toLowerCase(), Math.floor(parseFloat(donationAmount) * reward));
+        }
+
+        if (group === true) {
+            donationUsername = donationUsername.toLowerCase();
+            $.inidb.incr('donations', donationUsername, donationAmount);
+            if ($.inidb.exists('donations', donationUsername) && $.inidb.get('donations', donationUsername) >= groupMin) {
+                if ($.getUserGroupId(donationUsername) > 3) {
+                    $.setUserGroupById(donationUsername, '4');
+                }
+            }
+        }
+    });
+
+    /*
+     * @event command
+     */
+    $.bind('command', function(event) {
+        var sender = event.getSender(),
+            command = event.getCommand(),
+            args = event.getArgs(),
+            action = args[0],
+            subAction = args[1];
+
+        /*
+         * @commandpath streamelements - Controls various options for donation handling
+         */
+        if (command.equalsIgnoreCase('streamelements')) {
+            if (action === undefined) {
+                $.say($.whisperPrefix(sender) + $.lang.get('streamelements.donations.usage'));
+                return;
+            }
+
+            /*
+             * @commandpath streamelements toggledonators - Toggles the Donator's group.
+             */
+            if (action.equalsIgnoreCase('toggledonators')) {
+                group = !group;
+                $.setIniDbBoolean('streamElementsHandler', 'group', group);
+                $.say($.whisperPrefix(sender) + (group ? $.lang.get('streamelements.enabled.donators') : $.lang.get('streamelements.disabled.donators')));
+            }
+
+            /*
+             * @commandpath streamelements minmumbeforepromotion - Set the minimum before people get promoted to a Donator
+             */
+            if (action.equalsIgnoreCase('minmumbeforepromotion')) {
+                if (subAction === undefined) {
+                    $.say($.whisperPrefix(sender) + $.lang.get('streamelements.donators.min.usage'));
+                    return;
+                }
+
+                groupMin = subAction;
+                $.setIniDbNumber('streamElementsHandler', 'groupMin', groupMin);
+                $.say($.whisperPrefix(sender) + $.lang.get('streamelements.donators.min', groupMin));
+            }
+
+            /*
+             * @commandpath streamelements announce - Toggles announcements for donations off and on
+             */
+            if (action.equalsIgnoreCase('announce')) {
+                toggle = !toggle;
+                $.setIniDbBoolean('streamElementsHandler', 'toggle', toggle);
+                $.say($.whisperPrefix(sender) + (toggle ? $.lang.get('streamelements.donations.announce.enable') : $.lang.get('streamelements.donations.announce.disable')));
+            }
+
+            /*
+             * @commandpath streamelements rewardmultiplier [n.n] - Set a reward multiplier for donations.
+             */
+            if (action.equalsIgnoreCase('rewardmultiplier')) {
+                if (isNaN(parseFloat(subAction))) {
+                    $.say($.whisperPrefix(sender) + $.lang.get('streamelements.donations.reward.usage'));
+                    return;
+                }
+
+                reward = parseFloat(subAction);
+                $.setIniDbFloat('streamElementsHandler', 'reward', reward);
+                $.say($.whisperPrefix(sender) + $.lang.get('streamelements.donations.reward.success', subAction, (subAction == "1" ? $.pointNameSingle : $.pointNameMultiple).toLowerCase()));
+            }
+
+            /*
+             * @commandpath streamelements message [message text] - Set the donation message. Tags: (name), (amount), (reward), (message) and (currency)
+             */
+            if (action.equalsIgnoreCase('message')) {
+                if (subAction === undefined) {
+                    $.say($.whisperPrefix(sender) + $.lang.get('streamelements.donations.message.usage'));
+                    return;
+                }
+
+                var msg = args.splice(1).join(' ');
+                if (msg.search(/\(name\)/) == -1) {
+                    $.say($.whisperPrefix(sender) + $.lang.get('streamelements.donations.message.no-name'));
+                    return;
+                }
+
+                $.setIniDbString('streamElementsHandler', 'message', msg);
+
+                message = $.getIniDbString('streamElementsHandler', 'message');
+
+                $.say($.whisperPrefix(sender) + $.lang.get('streamelements.donations.message.success', msg));
+            }
+        }
+    });
+
+    /**
+     * @event initReady
+     */
+    $.bind('initReady', function() {
+        $.registerChatCommand('./handlers/streamElementsHandler.js', 'streamelements', 1);
+    });
+
+    $.reloadStreamElements = reloadStreamElements;
+})();
---
+++ source/com/scaniatv/StreamElementsAPIv1.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright (C) 2016-2017 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.scaniatv;
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+
+import java.net.SocketTimeoutException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.nio.charset.Charset;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import tv.phantombot.event.streamelements.donate.StreamElementsDonationEvent;
+import tv.phantombot.event.streamelements.donate.StreamElementsDonationInitializedEvent;
+
+/*
+ * @author ScaniaTV
+ */
+public class StreamElementsAPIv1 {
+
+    private static final StreamElementsAPIv1 instance = new StreamElementsAPIv1();
+    private static final String url = "https://api.streamelements.com/kappa/v1";
+    private static final int iHTTPTimeout = 2 * 1000;
+    private static String jwtToken = "";
+    private int pullLimit = 5;
+
+    /*
+     * Returns the current instance.
+     */
+    public static StreamElementsAPIv1 instance() {
+        return instance;
+    }
+
+    /*
+     * Builds the instance for this class.
+     */
+    private StreamElementsAPIv1() {
+        Thread.setDefaultUncaughtExceptionHandler(com.gmt2001.UncaughtExceptionHandler.instance());
+    }
+
+    /*
+     * Reads data from a stream.
+     */
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    /*
+     * Populates additional information into a JSON object to be digested
+     * as needed.
+     */
+    private static void fillJSONObject(JSONObject jsonObject, boolean success, String type, String url, int responseCode, String exception, String exceptionMessage, String jsonContent) {
+        jsonObject.put("_success", success);
+        jsonObject.put("_type", type);
+        jsonObject.put("_url", url);
+        jsonObject.put("_http", responseCode);
+        jsonObject.put("_exception", exception);
+        jsonObject.put("_exceptionMessage", exceptionMessage);
+        jsonObject.put("_content", jsonContent);
+    }
+
+    /*
+     * Reads data from an API. In this case its tipeeestream.
+     */
+    @SuppressWarnings("UseSpecificCatch")
+    private static JSONObject readJsonFromUrl(String urlAddress) {
+        JSONObject jsonResult = new JSONObject("{}");
+        InputStream inputStream = null;
+        URL urlRaw;
+        HttpsURLConnection urlConn;
+        String jsonText = "";
+
+        try {
+            urlRaw = new URL(urlAddress);
+            urlConn = (HttpsURLConnection) urlRaw.openConnection();
+            urlConn.setDoInput(true);
+            urlConn.setRequestMethod("GET");
+            urlConn.addRequestProperty("Content-Type", "application/json");
+            urlConn.addRequestProperty("Authorization", "Bearer " + jwtToken);
+            urlConn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.52 Safari/537.36 PhantomBotJ/2015");
+            urlConn.connect();
+
+            if (urlConn.getResponseCode() == 200) {
+                inputStream = urlConn.getInputStream();
+            } else {
+                inputStream = urlConn.getErrorStream();
+            }
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
+            jsonText = readAll(rd);
+            jsonResult = new JSONObject(jsonText);
+            fillJSONObject(jsonResult, true, "GET", urlAddress, urlConn.getResponseCode(), "", "", jsonText);
+        } catch (JSONException ex) {
+            fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "JSONException", ex.getMessage(), jsonText);
+            com.gmt2001.Console.debug.println("StreamElementsAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
+        } catch (NullPointerException ex) {
+            fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "NullPointerException", ex.getMessage(), "");
+            com.gmt2001.Console.debug.println("StreamElementsAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
+        } catch (MalformedURLException ex) {
+            fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "MalformedURLException", ex.getMessage(), "");
+            com.gmt2001.Console.debug.println("StreamElementsAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
+        } catch (SocketTimeoutException ex) {
+            fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "SocketTimeoutException", ex.getMessage(), "");
+            com.gmt2001.Console.debug.println("StreamElementsAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
+        } catch (IOException ex) {
+            fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "IOException", ex.getMessage(), "");
+            com.gmt2001.Console.debug.println("StreamElementsAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
+        } catch (Exception ex) {
+            fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "Exception", ex.getMessage(), "");
+            com.gmt2001.Console.debug.println("StreamElementsAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException ex) {
+                    fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "IOException", ex.getMessage(), "");
+                    com.gmt2001.Console.debug.println("StreamElementsAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
+                }
+            }
+        }
+
+        return jsonResult;
+    }
+
+    /*
+     * Sets the jwt token to access the api
+     *
+     * @param {String}  jwtToken  jwt key that the user added in the bot login.
+     */
+    public void SetJWT(String token) {
+        jwtToken = token;
+    }
+
+    /*
+     * Sets the api pull limit.
+     *
+     * @param {Int}  pullLimit  Amount of donations to pull, default is 5.
+     */
+    public void SetLimit(int pullLimit) {
+        this.pullLimit = pullLimit;
+    }
+
+    /*
+     * Pulls the 5 last donations from the API.
+     *
+     * @return {JSONObject}  The last 5 donations from the api.
+     */
+    public JSONObject GetDonations() {
+        return readJsonFromUrl(url + "/tips?limit=" + this.pullLimit);
+    }
+}
