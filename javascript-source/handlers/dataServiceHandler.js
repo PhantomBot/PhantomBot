9b2cf101d2617ce5620f793dc8dbebdf139079ea
IllusionaryOne <illusionaryone@illusionaryone.com>
2017-09-28 01:01:06 -0600

    Data Render Service Patches
    **dataServiceHandler.js**
    - Moved to timer.
    - Fixed issues with data building for objects.
    
    **DataRenderServiceAPIv1.java**
    - Added method to check if an API key exists or not.

--- javascript-source/handlers/dataServiceHandler.js
+++ javascript-source/handlers/dataServiceHandler.js
@@ -1,83 +1,89 @@
 (function() {
 
     /**
+     * @function drsTimer
+     */
+    function drsTimer() {
+        var keys, 
+            apiStatus,
+            lastTime = parseInt($.getSetIniDbNumber('datarenderservice', 'last_time', 0)),
+            checkTime = lastTime + parseInt(125 * 6e4),
+            JSONStringer = Packages.org.json.JSONStringer;
+
+        /* Run only once every 125 minutes. Do not change this. The Data Service has a rate limit
+         * and will reject the data.
+         */
+        if (lastTime > 0 && checkTime > $.systemTime()) {
+            return;
+        }
+
+        $.consoleLn('DataRenderService: Processing Data');
+        $.log.event('DataRenderService: Handler Process Start');
+
+        keys = $.inidb.GetKeyList('command', '');
+        jsonStringer = new JSONStringer();
+        jsonStringer.object().key('commands').array();
+        for (var idx in keys) {
+            if (!$.inidb.exists('disabledCommands', keys[idx])) {
+                jsonStringer.object().key('command').value(keys[idx] + '').endObject();
+            }
+        }
+        jsonStringer.endArray().endObject();
+        apiStatus = $.dataRenderServiceAPI.postData(jsonStringer.toString(), $.channelName, 'commands');
+        $.log.event('DataRenderService: Commands API status : ' + apiStatus);
+
+        keys = $.inidb.GetKeyList('quotes', '');
+        jsonStringer = new JSONStringer();
+        jsonStringer.object().key('quotes').array();
+        for (var idx in keys) {
+            var quoteObj = JSON.parse($.inidb.get('quotes', keys[idx]));
+            jsonStringer.object();
+            jsonStringer.key('id').value(parseInt(keys[idx]));
+            jsonStringer.key('user').value(quoteObj[0] + '');
+            jsonStringer.key('quote').value(quoteObj[1] + '');
+            jsonStringer.endObject();
+        }
+        jsonStringer.endArray().endObject();
+        apiStatus = $.dataRenderServiceAPI.postData(jsonStringer.toString(), $.channelName, 'quotes');
+        $.log.event('DataRenderService: Quotes API status : ' + apiStatus);
+        
+        keys = $.inidb.GetKeyList('points', '');
+        jsonStringer = new JSONStringer();
+        jsonStringer.object().key('pointsName').value($.pointNameMultiple).key('points').array();
+        for (var idx in keys) {
+            jsonStringer.object();
+            jsonStringer.key('user').value(keys[idx] + '');
+            jsonStringer.key('points').value($.inidb.get('points', keys[idx]));
+            jsonStringer.endObject();
+        }
+        jsonStringer.endArray().endObject();
+        apiStatus = $.dataRenderServiceAPI.postData(jsonStringer.toString(), $.channelName, 'points');
+        $.log.event('DataRenderService: Points API status : ' + apiStatus);
+
+        keys = $.inidb.GetKeyList('time', '');
+        jsonStringer = new JSONStringer();
+        jsonStringer.object().key('times').array();
+        for (var idx in keys) {
+            jsonStringer.object();
+            jsonStringer.key('user').value(keys[idx] + '');
+            jsonStringer.key('time').value($.inidb.get('time', keys[idx]));
+            jsonStringer.endObject();
+        }
+        jsonStringer.endArray().endObject();
+        apiStatus = $.dataRenderServiceAPI.postData(jsonStringer.toString(), $.channelName, 'times');
+        $.log.event('DataRenderService: Times API status : ' + apiStatus);
+
+        $.log.event('DataRenderService: Handler Process Complete');
+        $.setIniDbNumber('datarenderservice', 'last_time', $.systemTime());
+    }
+
+    /**
      * @event command
      */
     $.bind('command', function(event) {
         var sender = event.getSender().toLowerCase(),
             command = event.getCommand(),
-            args = event.getArgs(),
-            keys, 
-            apiStatus,
-            jsonObject = [],
-            JSONStringer = Packages.org.json.JSONStringer;
-
-        /**
-         * @commandpath updatecommandsapi - Sends command data to the Render Data Service API.
-         */
-        if (command.equalsIgnoreCase('updatecommandsapi')) {
-            keys = $.inidb.GetKeyList('command', '');
-
-            jsonObject['commands'] = [];
-            for (var idx in keys) {
-                if (!$.inidb.exists('disabledCommands', keys[idx])) {
-                    jsonObject['commands'].push({ command: keys[idx] + '' });
-                }
-            }
-            apiStatus = $.dataRenderServiceAPI.postData(JSON.stringify(jsonObject), $.channelName, 'commands');
-            $.say($.whisperPrefix(sender) + $.lang.get('dataservicehandler.update.status.' + apiStatus));
-            return;
-        }
-
-        /**
-         * @commandpath updatequotesapi - Sends quote data to the Render Data Service API.
-         */
-        if (command.equalsIgnoreCase('updatequotesapi')) {
-            keys = $.inidb.GetKeyList('quotes', '');
-
-            jsonObject['quotes'] = [];
-            for (var idx in keys) {
-                var quoteObj = JSON.parse($.inidb.get('quotes', keys[idx]));
-                jsonObject['quotes'].push({ id: parseInt(keys[idx]), user: quoteObj[0] + '', quote: quoteObj[1] + '' });
-            }
-            apiStatus = $.dataRenderServiceAPI.postData(JSON.stringify(jsonObject), $.channelName, 'quotes');
-            $.say($.whisperPrefix(sender) + $.lang.get('dataservicehandler.update.status.' + apiStatus));
-            return;
-        }
-
-        /**
-         * @commandpath updatepointsapi - Sends points data to the Render Data Service API.
-         */
-        if (command.equalsIgnoreCase('updatepointsapi')) {
-            keys = $.inidb.GetKeyList('points', '');
-
-            jsonStringer = new JSONStringer();
-            jsonStringer.object().key('pointsName').value($.pointNameMultiple).key('points').array();
-            for (var idx in keys) {
-                jsonStringer.object().key(keys[idx]).value($.inidb.get('points', keys[idx])).endObject();
-            }
-            jsonStringer.endArray().endObject();
-            apiStatus = $.dataRenderServiceAPI.postData(jsonStringer.toString(), $.channelName, 'points');
-            $.say($.whisperPrefix(sender) + $.lang.get('dataservicehandler.update.status.' + apiStatus));
-            return;
-        }
-
-        /**
-         * @commandpath updatetimesapi - Sends time data to the Render Data Service API.
-         */
-        if (command.equalsIgnoreCase('updatetimesapi')) {
-            keys = $.inidb.GetKeyList('points', '');
-
-            jsonStringer = new JSONStringer();
-            jsonStringer.object().key('times').array();
-            for (var idx in keys) {
-                jsonStringer.object().key(keys[idx]).value($.inidb.get('time', keys[idx])).endObject();
-            }
-            jsonStringer.endArray().endObject();
-            apiStatus = $.dataRenderServiceAPI.postData(jsonStringer.toString(), $.channelName, 'times');
-            $.say($.whisperPrefix(sender) + $.lang.get('dataservicehandler.update.status.' + apiStatus));
-            return;
-        }
+            args = event.getArgs();
 
         /**
          * @commandpath terminatedataserviceapi - Removes all account and PhantomBot data from the Render Data Service.
@@ -95,11 +101,12 @@
      */
     $.bind('initReady', function() {
         if ($.bot.isModuleEnabled('./handlers/dataServiceHandler.js')) {
-            $.registerChatCommand('./handlers/dataServiceHandler.js', 'updatecommandsapi', 1);
-            $.registerChatCommand('./handlers/dataServiceHandler.js', 'updatequotesapi', 1);
-            $.registerChatCommand('./handlers/dataServiceHandler.js', 'updatepointsapi', 1);
-            $.registerChatCommand('./handlers/dataServiceHandler.js', 'updatetimesapi', 1);
             $.registerChatCommand('./handlers/dataServiceHandler.js', 'terminatedataserviceapi', 1);
+
+            if ($.dataRenderServiceAPI.hasAPIKey()) {
+                $.consoleLn('Data Render Service API Key Present, Enabling Data Feed');
+                setInterval(function() { drsTimer(); }, 6e4); 
+            }
         }
     });
 
