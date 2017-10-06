1f3e128efebb754c635f8c4aa6013c6dd8e894cf
Caricatore <3536146+Caricatore@users.noreply.github.com>
2017-10-07 05:30:30 +0700

    Update pointsPanel.js
    Impossible to set "0" value through the web panel.

--- resources/web/panel/js/pointsPanel.js
+++ resources/web/panel/js/pointsPanel.js
@@ -313,7 +313,7 @@
             return;
         }
 
-        if (value.indexOf('.') !== -1 || parseInt(value) < 1) {
+        if (value.indexOf('.') !== -1 || (parseInt(value) < 1 && parseInt(value) !== 0)) {
             $("#setPointGainInput_" + action).val('Only natural numbers are allowed.');
             setTimeout(function() { $("#setPointGainInput_" + action).val(''); }, TIMEOUT_WAIT_TIME * 3);
             return;
