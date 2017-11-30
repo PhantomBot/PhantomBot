475574c69098e3d5dd3b9c7061ecfa2cc7b17f2f
Wayne <wayne6324@gmail.com>
2017-11-30 14:33:49 +0000

    Fixed sender
    function giveAll(amount, sender) {

--- javascript-source/systems/pointSystem.js
+++ javascript-source/systems/pointSystem.js
@@ -290,7 +290,7 @@
     * @function giveAll
     * @param {Number} action
     */
-    function giveAll(amount) {
+    function giveAll(amount, sender) {
         if (amount < 0) {
             $.say($.whisperPrefix(sender) + $.lang.get('pointsystem.add.error.negative', pointNameMultiple));
             return;
