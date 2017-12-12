5a65da96bebf591ea86ae1dc6ff23ed541597025
ScaniaTV <scaniatv@users.noreply.github.com>
2017-12-11 20:38:43 -0400

    - Added missing renamed classes

---
+++ source/tv/phantombot/event/twitch/bits/TwitchBitsEvent.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016-2017 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package tv.phantombot.event.twitch.bits;
+
+import tv.phantombot.event.twitch.TwitchEvent;
+
+public class TwitchBitsEvent extends TwitchEvent {
+	private final String username;
+    private final String bits;
+
+    /*
+     * Class constructor.
+     *
+     * @param {String} username
+     * @param {String} bits
+     */
+	public TwitchBitsEvent(String username, String bits) {
+        this.username = username;
+        this.bits = bits;
+    }
+
+    /*
+     * Method that returns the username who cheered.
+     *
+     * @return {String} username
+     */
+    public String getUsername() {
+        return this.username;
+    }
+
+    /*
+     * Method that returns the amount of bit the user cheered.
+     *
+     * @return {String} bits
+     */
+    public String getBits() {
+        return this.bits;
+    }
+}
---
+++ source/tv/phantombot/event/twitch/subscriber/TwitchSubscriberEvent.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2016-2017 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package tv.phantombot.event.twitch.subscriber;
+
+import tv.phantombot.event.twitch.TwitchEvent;
+
+public class TwitchSubscriberEvent extends TwitchEvent {
+	private final String subscriber;
+    private final String plan;
+
+    /*
+     * Class constructor.
+     *
+     * @param {String} subscriber
+     */
+	public TwitchSubscriberEvent(String subscriber) {
+        this.subscriber = subscriber;
+        this.plan = null;
+    }
+
+    /*
+     * Class constructor.
+     *
+     * @param {String} subscriber
+     * @param {String} plan
+     */
+    public TwitchSubscriberEvent(String subscriber, String plan) {
+        this.subscriber = subscriber;
+        this.plan = plan;
+    }
+
+    /*
+     * Method that returns the subscriber's name.
+     *
+     * @return {String} subscriber
+     */
+    public String getSubscriber() {
+        return this.subscriber;
+    }
+
+    /*
+     * Method that returns the subcription plan. (1000, 2000, 3000 and Prime)
+     *
+     * @return {String} plan
+     */
+    public String getPlan() {
+        return this.plan;
+    }
+}
