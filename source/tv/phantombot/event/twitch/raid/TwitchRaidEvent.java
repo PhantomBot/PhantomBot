45f883b4ce4a52c27973c77c3c80eae464490994
ScaniaTV <scaniatv@users.noreply.github.com>
2017-12-11 20:38:43 -0400

    - Added missing renamed classes

---
+++ source/tv/phantombot/event/twitch/raid/TwitchRaidEvent.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2016-2017 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package tv.phantombot.event.twitch.raid;
+
+import tv.phantombot.event.twitch.TwitchEvent;
+
+public class TwitchRaidEvent extends TwitchEvent {
+    private final String username;
+    private final String viewers;
+
+    /*
+     * Class constructor.
+     *
+     * @param {String} username
+     * @param {String} viewers
+     */
+    public TwitchRaidEvent(String username, String viewers) {
+        this.username = username;
+        this.viewers = viewers;
+    }
+
+    /*
+     * Method that returns the username who hosted.
+     *
+     * @return {String} username
+     */
+    public String getUsername() {
+        return this.username;
+    }
+
+    /*
+     * Method that returns the amount of users who raided.
+     *
+     * @return {String} viewers
+     */
+    public String getViewers() {
+        return this.viewers;
+    }
+}
---
+++ source/tv/phantombot/event/twitch/subscriber/TwitchReSubscriberEvent.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2016-2017 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package tv.phantombot.event.twitch.subscriber;
+
+import tv.phantombot.event.twitch.TwitchEvent;
+
+public class TwitchReSubscriberEvent extends TwitchEvent {
+	private final String resubscriber;
+    private final String months;
+    private final String plan;
+
+    /*
+     * Class constructor.
+     *
+     * @param {String} resubscriber
+     * @param {String} months
+     */
+	public TwitchReSubscriberEvent(String resubscriber, String months) {
+        this.resubscriber = resubscriber;
+        this.months = months;
+        this.plan = null;
+    }
+
+    /*
+     * Class constructor.
+     *
+     * @param {String} resubscriber
+     * @param {String} months
+     * @param {String} plan
+     */
+    public TwitchReSubscriberEvent(String resubscriber, String months, String plan) {
+        this.resubscriber = resubscriber;
+        this.months = months;
+        this.plan = plan;
+    }
+
+    /*
+     * Method that returns the resubscriber.
+     *
+     * @return {String} resubscriber
+     */
+    public String getReSubscriber() {
+        return this.resubscriber;
+    }
+
+    /*
+     * Method that returns the resub months.
+     *
+     * @return {String} months
+     */
+    public String getMonths() {
+        return this.months;
+    }
+
+    /*
+     * Method that returns the subcription plan. (1000, 2000, 3000 and Prime)
+     *
+     * @return {String} plan
+     */
+    public String getPlan() {
+        return this.plan;
+    }
+}
