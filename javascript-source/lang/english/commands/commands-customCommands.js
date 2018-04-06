77135e1c4eb155a0793071bf4088002f1cd7649b
Robert Vesturo Tiede <vesturomedia@gmail.com>
2018-04-06 03:19:39 +0200

    Inserted missing D in "customcommands.add.disable(d)" on line 12

--- javascript-source/lang/english/commands/commands-customCommands.js
+++ javascript-source/lang/english/commands/commands-customCommands.js
@@ -9,7 +9,7 @@ $.lang.register('customcommands.alias.delete.usage', 'Usage: !delalias (alias na
 $.lang.register('customcommands.alias.error', 'An alias already exists for !$1. Delete it first.');
 $.lang.register('customcommands.alias.error.target404', 'The target command does not exist!');
 $.lang.register('customcommands.alias.error.exists', 'The command you want to alias to already exists.');
-$.lang.register('customcommands.add.disable', 'That command is currently disabled. Re-enable the command or delete it to add a new command with that name.');
+$.lang.register('customcommands.add.disabled', 'That command is currently disabled. Re-enable the command or delete it to add a new command with that name.');
 $.lang.register('customcommands.alias.success', 'The command !$1 was successfully aliased to !$2');
 $.lang.register('customcommands.alias.usage', 'Usage: !aliascom (alias name) (existing command) [optional parameters]');
 $.lang.register('customcommands.delete.success', 'Command !$1 has been removed!');
