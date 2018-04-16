2d71e3001986e0027978b74a8bb0b1b720bf5f90
ScaniaTV <scaniatv@users.noreply.github.com>
2018-04-16 19:02:11 -0300

    - Moved FFZ to https since it no longer supports http.

--- source/com/illusionaryone/FrankerZAPIv1.java
+++ source/com/illusionaryone/FrankerZAPIv1.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.HttpURLConnection;
+import javax.net.ssl.HttpsURLConnection
 import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 import java.net.URL;
@@ -41,7 +41,7 @@ import org.json.JSONObject;
 public class FrankerZAPIv1 {
 
     private static final FrankerZAPIv1 instance = new FrankerZAPIv1();
-    private static final String sAPIURL = "http://api.frankerfacez.com/v1";
+    private static final String sAPIURL = "https://api.frankerfacez.com/v1";
     private static final int iHTTPTimeout = 2 * 1000;
 
     public static FrankerZAPIv1 instance() {
@@ -90,7 +90,7 @@ public class FrankerZAPIv1 {
 
         try {
             urlRaw = new URL(urlAddress);
-            urlConn = (HttpURLConnection) urlRaw.openConnection();
+            urlConn = (HttpsURLConnection) urlRaw.openConnection();
             urlConn.setDoInput(true);
             urlConn.setRequestMethod("GET");
             urlConn.addRequestProperty("Content-Type", "application/json");
@@ -127,16 +127,17 @@ public class FrankerZAPIv1 {
             fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "Exception", ex.getMessage(), "");
             com.gmt2001.Console.err.println("FrankerZAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
         } finally {
-            if (inputStream != null)
+            if (inputStream != null) {
                 try {
                     inputStream.close();
                 } catch (IOException ex) {
                     fillJSONObject(jsonResult, false, "GET", urlAddress, 0, "IOException", ex.getMessage(), "");
                     com.gmt2001.Console.err.println("FrankerZAPIv1::readJsonFromUrl::Exception: " + ex.getMessage());
                 }
+            }
         }
 
-        return(jsonResult);
+        return jsonResult;
     }
 
     /*
