cd403f104051088052a5f0704ad1f5682d6663ef
BotsWatcher <37457847+BotsWatcher@users.noreply.github.com>
2018-04-19 19:02:16 +0500

    Update handlers-streamlabsHandler.js

--- javascript-source/lang/english/discord/handlers/handlers-streamlabsHandler.js
+++ javascript-source/lang/english/discord/handlers/handlers-streamlabsHandler.js
@@ -1,7 +1,7 @@
 $.lang.register('discord.streamlabshandler.usage', 'Usage: !streamlabshandler [toggle / message / channel]');
-$.lang.register('discord.streamlabshandler.toggle', 'StreamLabs donation announcements have been $1.');
+$.lang.register('discord.streamlabshandler.toggle', 'Streamlabs donation announcements have been $1.');
 $.lang.register('discord.streamlabshandler.message.usage', 'Usage: !streamlabshandler message [message] - Tags: (name) (amount) (currency) (message)');
-$.lang.register('discord.streamlabshandler.message.set', 'StreamLabs donation message set to: $1');
+$.lang.register('discord.streamlabshandler.message.set', 'Streamlabs donation message set to: $1');
 $.lang.register('discord.streamlabshandler.channel.usage', 'Usage: !streamlabshandler channel [channel name]');
-$.lang.register('discord.streamlabshandler.channel.set', 'StreamLabs donation annoucements will now be made in channel #$1');
+$.lang.register('discord.streamlabshandler.channel.set', 'Streamlabs donation annoucements will now be made in channel #$1');
 $.lang.register('discord.streamlabshandler.embed.title', 'New Tip!');
