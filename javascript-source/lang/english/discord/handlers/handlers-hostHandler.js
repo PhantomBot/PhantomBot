f6390bfaf9a8fc443ddaa05e77fed6ebd5811d65
ScaniaTV <scaniatv@users.noreply.github.com>
2018-04-21 19:04:36 -0300

    - Fixed missing lang, fixed missing embed and more

--- javascript-source/lang/english/discord/handlers/handlers-hostHandler.js
+++ javascript-source/lang/english/discord/handlers/handlers-hostHandler.js
@@ -7,3 +7,4 @@ $.lang.register('discord.hosthandler.autohost.message.set', 'Auto-Host message s
 $.lang.register('discord.hosthandler.channel.usage', 'Usage: !hosthandler channel [channel name]');
 $.lang.register('discord.hosthandler.channel.set', 'Host announcements will now be made in channel #$1');
 $.lang.register('discord.hosthandler.host.embedtitle', 'New Host!');
+$.lang.register('discord.hosthandler.auto.host.embedtitle', 'New Auto-Host!');
