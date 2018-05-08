151421d15ea23f20f094392b3a876651f62b134f
ScaniaTV <scaniatv@users.noreply.github.com>
2018-05-08 13:49:52 -0300

    - Removed StreamTip from PhantomBot and more
    - Removed StreamTip.
    - Moved CODESTYLE.md and DEVSETUP.md to dev-resources.
    - Switch back version number to 2.4.0.3 to stop nightly update issue.
    - Added an OS check for the setup process to link the proper setup guide.

--- javascript-source/lang/english/discord/handlers/handlers-streamtipHandler.js
+++
@@ -1,7 +0,0 @@
-$.lang.register('discord.streamtiphandler.usage', 'Usage: !streamtiphandler [toggle / message / channel]');
-$.lang.register('discord.streamtiphandler.toggle', 'StreamTip donation announcements have been $1.');
-$.lang.register('discord.streamtiphandler.message.usage', 'Usage: !streamtiphandler message [message] - Tags: (name) (amount) (currency) (message)');
-$.lang.register('discord.streamtiphandler.message.set', 'StreamTip donation message set to: $1');
-$.lang.register('discord.streamtiphandler.channel.usage', 'Usage: !streamtiphandler channel [channel name]');
-$.lang.register('discord.streamtiphandler.channel.set', 'StreamTip donation announcements will now be made in channel #$1');
-$.lang.register('discord.streamtiphandler.embed.title', 'New Tip!');
