aa614999757bc0b4906942894dba86a0c0c82af3
IllusionaryOne <illusionaryone@illusionaryone.com>
2018-07-12 23:55:18 -0600

    Ensure that copyright is present on all JavaScript files.

--- javascript-source/lang/english/systems/systems-auctionSystem.js
+++ javascript-source/lang/english/systems/systems-auctionSystem.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016-2018 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 $.lang.register('auctionsystem.usage', 'Usage: !auction open (increments) (minimum bid) (auto close timer)');
 $.lang.register('auctionsystem.err.opened', 'An auction is already running.');
 $.lang.register('auctionsystem.opened', 'Auction is now opened! You can bid in increments of at least $1. Minimum bid allowed is $2! Start bidding with !bid (amount)');
--- javascript-source/lang/english/systems/systems-greetingSystem.js
+++ javascript-source/lang/english/systems/systems-greetingSystem.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016-2018 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 $.lang.register('greetingsystem.set.autogreet.enabled', 'Auto Greeting Enabled. $1 will now greet viewers with a greeting configured.');
 $.lang.register('greetingsystem.set.autogreet.disabled', 'Auto Greeting Disabled');
 $.lang.register('greetingsystem.set.autogreet.noname', 'Please use the (name) tag in the default greeting.');
