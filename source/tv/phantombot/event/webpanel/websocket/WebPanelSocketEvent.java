4f9376590875f728c75672bfbf4241bbc5a9ca35
Karlito <karlito@ldop.de>
2018-07-25 13:58:35 +0200

    corrected package names and references

--- source/tv/phantombot/event/webpanel/websocket/WebPanelSocketEvent.java
+++ source/tv/phantombot/event/webpanel/websocket/WebPanelSocketEvent.java
@@ -14,7 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package tv.phantombot.event.webpanel;
+package tv.phantombot.event.webpanel.websocket;
+
+import tv.phantombot.event.webpanel.WebPanelEvent;
 
 public abstract class WebPanelSocketEvent extends WebPanelEvent {
 
