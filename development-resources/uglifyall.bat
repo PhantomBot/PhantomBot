d6241a421db204fb31bbf72d0dd28aa0e2947836
ScaniaTV <scaniatv@users.noreply.github.com>
2018-08-10 16:10:58 -0300

    - Fixed a few things

--- development-resources/uglifyall.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-for /f "tokens=1* delims=\" %%A in (
-  'forfiles /s /m *.js /c "cmd /c echo @relpath"'
-) do for %%F in (^"%%B) do ( 
-echo Running uglifyjs  %%~F -c -m -o ..\resources\scripts\%%~F
-call uglifyjs  %%~F -c -m -o ..\resources\scripts\%%~F
-)
