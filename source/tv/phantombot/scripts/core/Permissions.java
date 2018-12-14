57a5b544a7129a24edf770286c6b08ced29a487a
ScaniaTV <scaniatv@users.noreply.github.com>
2018-12-14 17:54:40 -0400

    - Update to new permissions system

--- source/tv/phantombot/scripts/core/Permissions.java
+++ source/tv/phantombot/scripts/core/Permissions.java
@@ -17,12 +17,114 @@
 
 package tv.phantombot.scripts.core;
 
+import net.engio.mbassy.listener.Handler;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import tv.phantombot.event.Listener;
+import tv.phantombot.event.irc.channel.IrcChannelJoinEvent;
+import tv.phantombot.event.irc.channel.IrcChannelLeaveEvent;
+import tv.phantombot.event.irc.channel.IrcChannelUserModeEvent;
+import tv.phantombot.event.irc.channel.IrcChannelUsersUpdateEvent;
+import tv.phantombot.event.irc.message.IrcChannelMessageEvent;
+import tv.phantombot.event.irc.message.IrcPrivateMessageEvent;
+
 
 /**
  * Class that handles user permissions for PhantomBot.
  * 
  * @author ScaniaTV
  */
-public class Permissions {
+public class Permissions implements Listener {
+    public static final Permissions INSTANCE = new Permissions();
+    private final ConcurrentMap<String, User> users = new ConcurrentHashMap();
     
+    /**
+     * Method that returns this instance.
+     * 
+     * @return 
+     */
+    public static Permissions instance() {
+        return INSTANCE;
+    }
+    
+    /**
+     * Method that removes a user from the cache.
+     * 
+     * @param username
+     * @return 
+     */
+    public User getUser(String username) {
+        User user = users.get(username.toLowerCase());
+        
+        // If the user doesn't exists, create new one.
+        if (user == null) {
+            // Create the user object.
+            user = new User(username);
+            // Add it to the object,
+            users.put(user.getUsername(), user);
+        }
+        
+        return user;
+    }
+    
+    /**
+     * Method that checks if the user is in the cache.
+     * 
+     * @param username
+     * @return 
+     */
+    public boolean hasUser(String username) {
+        return users.containsKey(username.toLowerCase());
+    }
+    
+    /**
+     * Method that removes a user if he doesn't exist.
+     * 
+     * @param username 
+     */
+    public void removeUser(String username) {
+        username = username.toLowerCase();
+        
+        if (hasUser(username)) {
+            users.remove(username);
+        }
+    }
+    
+    // A handler event for IrcChannelUsersUpdateEvent.
+    @Handler
+    private void ircChannelUsersUpdateEvent(IrcChannelUsersUpdateEvent event) {
+        
+    }
+    
+    // A handler event for IrcChannelJoinEvent.
+    @Handler
+    private void ircChannelJoinEvent(IrcChannelJoinEvent event) {
+        
+    }
+    
+    // A handler event for IrcChannelLeaveEvent.
+    @Handler
+    private void ircChannelLeaveEvent(IrcChannelLeaveEvent event) {
+        
+    }
+    
+    // A handler event for IrcChannelUserModeEvent.
+    @Handler
+    private void ircChannelUserModeEvent(IrcChannelUserModeEvent event) {
+        
+    }
+    
+    // A handler event for IrcPrivateMessageEvent.
+    @Handler
+    private void ircPrivateMessageEvent(IrcPrivateMessageEvent event) {
+        
+    }
+    
+    // A handler event for IrcChannelMessageEvent.
+    @Handler
+    private void ircChannelMessageEvent(IrcChannelMessageEvent event) {
+        
+    }
 }
