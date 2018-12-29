c16c83b6bd85bf9fe9101e4edc44662bc303e254
Karlito <karlito@ldop.de>
2018-12-29 17:48:08 +0100

    Separated setup process from config loading and some refactoring
    ConfigurationManager.java:
    - added a private constructor to prevent the purely static class from
    being instantiated
    - formatted code
    - changed variable names according to the java code style convention
    - extracted setup process into a separate method

--- source/tv/phantombot/ConfigurationManager.java
+++ source/tv/phantombot/ConfigurationManager.java
@@ -11,21 +11,25 @@ import java.util.TreeSet;
 import java.util.Map.Entry;
 
 public class ConfigurationManager {
-    static Properties getConfiguration()
-    {
+
+    private ConfigurationManager() {
+        // private constructor to prevent users from instantiating a pure static class
+    }
+
+    static Properties getConfiguration() {
         /* List of properties that must exist. */
         String requiredProperties[] = new String[] { "oauth", "channel", "owner", "user" };
         String requiredPropertiesErrorMessage = "";
-        
+
         /* Properties configuration */
         Properties startProperties = new Properties();
-        
+
         /* Indicates that the botlogin.txt file should be overwritten/created. */
         Boolean changed = false;
-        
+
         // Indicates that this is a fresh setup
         Boolean newSetup = false;
-        
+
         /* Load up the bot info from the bot login file */
         try {
             if (new File("./config/botlogin.txt").exists()) {
@@ -33,8 +37,9 @@ public class ConfigurationManager {
                 startProperties.load(inputStream);
                 inputStream.close();
             } else {
-                /* Fill in the Properties object with some default values. Note that some values are left
-                 * unset to be caught in the upcoming logic to enforce settings.
+                /*
+                 * Fill in the Properties object with some default values. Note that some values
+                 * are left unset to be caught in the upcoming logic to enforce settings.
                  */
                 startProperties.setProperty("baseport", "25000");
                 startProperties.setProperty("usehttps", "false");
@@ -50,12 +55,12 @@ public class ConfigurationManager {
         }
         /* Load up the bot info from the environment */
         for (Entry<String, String> v : System.getenv().entrySet()) {
-            String Prefix = "PHANTOMBOT_";
-            String Key = v.getKey().toUpperCase();
-            String Value = v.getValue();
-            if (Key.startsWith(Prefix) && Prefix.length() < Key.length()) {
-                Key = Key.substring(Prefix.length()).toLowerCase();
-                startProperties.setProperty(Key, Value);
+            String prefix = "PHANTOMBOT_";
+            String key = v.getKey().toUpperCase();
+            String value = v.getValue();
+            if (key.startsWith(prefix) && prefix.length() < key.length()) {
+                key = key.substring(prefix.length()).toLowerCase();
+                startProperties.setProperty(key, value);
             }
         }
         /* Check to enable debug mode */
@@ -118,109 +123,9 @@ public class ConfigurationManager {
 
         /* Make a new botlogin with the botName, oauth or channel is not found */
         if (startProperties.getProperty("user") == null || startProperties.getProperty("oauth") == null || startProperties.getProperty("channel") == null) {
-            try {
-
-                com.gmt2001.Console.out.print("\r\n");
-                com.gmt2001.Console.out.print("Welcome to the PhantomBot setup process!\r\n");
-                com.gmt2001.Console.out.print("If you have any issues please report them on our forum, Tweet at us, or join our Discord!\r\n");
-                com.gmt2001.Console.out.print("Forum: https://community.phantombot.tv/\r\n");
-                com.gmt2001.Console.out.print("Documentation: https://docs.phantombot.tv/\r\n");
-                com.gmt2001.Console.out.print("Twitter: https://twitter.com/PhantomBot/\r\n");
-                com.gmt2001.Console.out.print("Discord: https://discord.gg/rkPqDuK/\r\n");
-                com.gmt2001.Console.out.print("Support PhantomBot on Patreon: https://phantombot.tv/support/\r\n");
-                com.gmt2001.Console.out.print("\r\n");                
-
-                final String os = System.getProperty("os.name").toLowerCase();
-
-                // Detect Windows, MacOS, Linux or any other operating system.
-                if (os.startsWith("win")) {
-                    com.gmt2001.Console.out.print("PhantomBot has detected that your device is running Windows.\r\n");
-                    com.gmt2001.Console.out.print("Here's the setup guide for Windows: https://community.phantombot.tv/t/windows-setup-guide/");
-                } else if (os.startsWith("mac")) {
-                    com.gmt2001.Console.out.print("PhantomBot has detected that your device is running macOS.\r\n");
-                    com.gmt2001.Console.out.print("Here's the setup guide for macOS: https://community.phantombot.tv/t/macos-setup-guide/");
-                } else {
-                    com.gmt2001.Console.out.print("PhantomBot has detected that your device is running Linux.\r\n");
-                    com.gmt2001.Console.out.print("Here's the setup guide for Ubuntu: https://community.phantombot.tv/t/ubuntu-16-04-lts-setup-guide/\r\n");
-                    com.gmt2001.Console.out.print("Here's the setup guide for CentOS: https://community.phantombot.tv/t/centos-7-setup-guide/");
-                }
-
-                com.gmt2001.Console.out.print("\r\n\r\n\r\n");
-
-                // Bot name.
-                do {
-                    com.gmt2001.Console.out.print("1. Please enter the bot's Twitch username: ");
-
-                    startProperties.setProperty("user", System.console().readLine().trim().toLowerCase());
-                } while (startProperties.getProperty("user", "").length() <= 0);
-
-                // Twitch oauth.
-                do {
-                    com.gmt2001.Console.out.print("\r\n");
-                    com.gmt2001.Console.out.print("2. You will now need a OAuth token for the bot to be able to chat.\r\n");
-                    com.gmt2001.Console.out.print("Please note, this OAuth token needs to be generated while you're logged in into the bot's Twitch account.\r\n");
-                    com.gmt2001.Console.out.print("If you're not logged in as the bot, please go to https://twitch.tv/ and login as the bot.\r\n");
-                    com.gmt2001.Console.out.print("Get the bot's OAuth token here: https://twitchapps.com/tmi/\r\n");
-                    com.gmt2001.Console.out.print("Please enter the bot's OAuth token: ");
-
-                    startProperties.setProperty("oauth", System.console().readLine().trim());
-                } while (startProperties.getProperty("oauth", "").length() <= 0);
-
-                // api oauth.
-                do {
-                    com.gmt2001.Console.out.print("\r\n");
-                    com.gmt2001.Console.out.print("3. You will now need your channel OAuth token for the bot to be able to change your title and game.\r\n");
-                    com.gmt2001.Console.out.print("Please note, this OAuth token needs to be generated while you're logged in into your caster account.\r\n");
-                    com.gmt2001.Console.out.print("If you're not logged in as the caster, please go to https://twitch.tv/ and login as the caster.\r\n");
-                    com.gmt2001.Console.out.print("Get the your OAuth token here: https://phantombot.tv/oauth/\r\n");
-                    com.gmt2001.Console.out.print("Please enter your OAuth token: ");
-
-                    startProperties.setProperty("apioauth", System.console().readLine().trim());
-                } while (startProperties.getProperty("apioauth", "").length() <= 0);
-
-                // Channel name.
-                do {
-                    com.gmt2001.Console.out.print("\r\n");
-                    com.gmt2001.Console.out.print("4. Please enter the name of the Twitch channel the bot should join: ");
-
-                    startProperties.setProperty("channel", System.console().readLine().trim());
-                } while (startProperties.getProperty("channel", "").length() <= 0);
-
-                // Panel username.
-                do {
-                    com.gmt2001.Console.out.print("\r\n");
-                    com.gmt2001.Console.out.print("5. Please enter a custom username for the web panel: ");
-
-                    startProperties.setProperty("paneluser", System.console().readLine().trim());
-                } while (startProperties.getProperty("paneluser", "").length() <= 0);
-
-                // Panel password.
-                do {
-                    com.gmt2001.Console.out.print("\r\n");
-                    com.gmt2001.Console.out.print("6. Please enter a custom password for the web panel: ");
-
-                    startProperties.setProperty("panelpassword", System.console().readLine().trim());
-                } while (startProperties.getProperty("panelpassword", "").length() <= 0);
-
-                com.gmt2001.Console.out.print("\r\n");
-                com.gmt2001.Console.out.print("PhantomBot will launch in 10 seconds.\r\n");
-                com.gmt2001.Console.out.print("If you're hosting the bot locally you can access the control panel here: http://localhost:25000/panel \r\n");
-                com.gmt2001.Console.out.print("If you're running the bot on a server, make sure to open the following ports: \r\n");
-                com.gmt2001.Console.out.print("25000, 25003, and 25004. You have to change 'localhost' to your server ip to access the panel. \r\n");
-
-                try {
-                    Thread.sleep(10000);
-                } catch (InterruptedException ex) {
-                    com.gmt2001.Console.debug.println("Failed to sleep in setup: " + ex.getMessage());
-                }
-
-                changed = true;
-                newSetup = true;
-            } catch (NullPointerException ex) {
-                com.gmt2001.Console.err.printStackTrace(ex);
-                com.gmt2001.Console.out.println("[ERROR] Failed to setup PhantomBot. Now exiting...");
-                PhantomBot.exitError();
-            }
+            doSetup(startProperties);
+            changed = true;
+            newSetup = true;
         }
 
         /* Make sure the oauth has been set correctly */
@@ -258,7 +163,8 @@ public class ConfigurationManager {
             }
         }
 
-        /* Iterate the properties and delete entries for anything that does not have a
+        /*
+         * Iterate the properties and delete entries for anything that does not have a
          * value.
          */
         for (String propertyKey : startProperties.stringPropertyNames()) {
@@ -302,13 +208,117 @@ public class ConfigurationManager {
                 com.gmt2001.Console.err.printStackTrace(ex);
             }
         }
-        
+
         // fresh setup indicator should not be saved
         startProperties.put("newSetup", newSetup);
-        
+
         return startProperties;
     }
-    
+
+    private static void doSetup(Properties startProperties) {
+        try {
+
+            com.gmt2001.Console.out.print("\r\n");
+            com.gmt2001.Console.out.print("Welcome to the PhantomBot setup process!\r\n");
+            com.gmt2001.Console.out.print("If you have any issues please report them on our forum, Tweet at us, or join our Discord!\r\n");
+            com.gmt2001.Console.out.print("Forum: https://community.phantombot.tv/\r\n");
+            com.gmt2001.Console.out.print("Documentation: https://docs.phantombot.tv/\r\n");
+            com.gmt2001.Console.out.print("Twitter: https://twitter.com/PhantomBot/\r\n");
+            com.gmt2001.Console.out.print("Discord: https://discord.gg/rkPqDuK/\r\n");
+            com.gmt2001.Console.out.print("Support PhantomBot on Patreon: https://phantombot.tv/support/\r\n");
+            com.gmt2001.Console.out.print("\r\n");
+
+            final String os = System.getProperty("os.name").toLowerCase();
+
+            // Detect Windows, MacOS, Linux or any other operating system.
+            if (os.startsWith("win")) {
+                com.gmt2001.Console.out.print("PhantomBot has detected that your device is running Windows.\r\n");
+                com.gmt2001.Console.out.print("Here's the setup guide for Windows: https://community.phantombot.tv/t/windows-setup-guide/");
+            } else if (os.startsWith("mac")) {
+                com.gmt2001.Console.out.print("PhantomBot has detected that your device is running macOS.\r\n");
+                com.gmt2001.Console.out.print("Here's the setup guide for macOS: https://community.phantombot.tv/t/macos-setup-guide/");
+            } else {
+                com.gmt2001.Console.out.print("PhantomBot has detected that your device is running Linux.\r\n");
+                com.gmt2001.Console.out.print("Here's the setup guide for Ubuntu: https://community.phantombot.tv/t/ubuntu-16-04-lts-setup-guide/\r\n");
+                com.gmt2001.Console.out.print("Here's the setup guide for CentOS: https://community.phantombot.tv/t/centos-7-setup-guide/");
+            }
+
+            com.gmt2001.Console.out.print("\r\n\r\n\r\n");
+
+            // Bot name.
+            do {
+                com.gmt2001.Console.out.print("1. Please enter the bot's Twitch username: ");
+
+                startProperties.setProperty("user", System.console().readLine().trim().toLowerCase());
+            } while (startProperties.getProperty("user", "").length() <= 0);
+
+            // Twitch oauth.
+            do {
+                com.gmt2001.Console.out.print("\r\n");
+                com.gmt2001.Console.out.print("2. You will now need a OAuth token for the bot to be able to chat.\r\n");
+                com.gmt2001.Console.out.print("Please note, this OAuth token needs to be generated while you're logged in into the bot's Twitch account.\r\n");
+                com.gmt2001.Console.out.print("If you're not logged in as the bot, please go to https://twitch.tv/ and login as the bot.\r\n");
+                com.gmt2001.Console.out.print("Get the bot's OAuth token here: https://twitchapps.com/tmi/\r\n");
+                com.gmt2001.Console.out.print("Please enter the bot's OAuth token: ");
+
+                startProperties.setProperty("oauth", System.console().readLine().trim());
+            } while (startProperties.getProperty("oauth", "").length() <= 0);
+
+            // api oauth.
+            do {
+                com.gmt2001.Console.out.print("\r\n");
+                com.gmt2001.Console.out.print("3. You will now need your channel OAuth token for the bot to be able to change your title and game.\r\n");
+                com.gmt2001.Console.out.print("Please note, this OAuth token needs to be generated while you're logged in into your caster account.\r\n");
+                com.gmt2001.Console.out.print("If you're not logged in as the caster, please go to https://twitch.tv/ and login as the caster.\r\n");
+                com.gmt2001.Console.out.print("Get the your OAuth token here: https://phantombot.tv/oauth/\r\n");
+                com.gmt2001.Console.out.print("Please enter your OAuth token: ");
+
+                startProperties.setProperty("apioauth", System.console().readLine().trim());
+            } while (startProperties.getProperty("apioauth", "").length() <= 0);
+
+            // Channel name.
+            do {
+                com.gmt2001.Console.out.print("\r\n");
+                com.gmt2001.Console.out.print("4. Please enter the name of the Twitch channel the bot should join: ");
+
+                startProperties.setProperty("channel", System.console().readLine().trim());
+            } while (startProperties.getProperty("channel", "").length() <= 0);
+
+            // Panel username.
+            do {
+                com.gmt2001.Console.out.print("\r\n");
+                com.gmt2001.Console.out.print("5. Please enter a custom username for the web panel: ");
+
+                startProperties.setProperty("paneluser", System.console().readLine().trim());
+            } while (startProperties.getProperty("paneluser", "").length() <= 0);
+
+            // Panel password.
+            do {
+                com.gmt2001.Console.out.print("\r\n");
+                com.gmt2001.Console.out.print("6. Please enter a custom password for the web panel: ");
+
+                startProperties.setProperty("panelpassword", System.console().readLine().trim());
+            } while (startProperties.getProperty("panelpassword", "").length() <= 0);
+
+            com.gmt2001.Console.out.print("\r\n");
+            com.gmt2001.Console.out.print("PhantomBot will launch in 10 seconds.\r\n");
+            com.gmt2001.Console.out.print("If you're hosting the bot locally you can access the control panel here: http://localhost:25000/panel \r\n");
+            com.gmt2001.Console.out.print("If you're running the bot on a server, make sure to open the following ports: \r\n");
+            com.gmt2001.Console.out.print("25000, 25003, and 25004. You have to change 'localhost' to your server ip to access the panel. \r\n");
+
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ex) {
+                com.gmt2001.Console.debug.println("Failed to sleep in setup: " + ex.getMessage());
+            }
+
+        } catch (NullPointerException ex) {
+            com.gmt2001.Console.err.printStackTrace(ex);
+            com.gmt2001.Console.out.println("[ERROR] Failed to setup PhantomBot. Now exiting...");
+            PhantomBot.exitError();
+        }
+    }
+
     /* gen a oauth */
     private static String generateWebAuth() {
         return PhantomBot.generateRandomString(30);
