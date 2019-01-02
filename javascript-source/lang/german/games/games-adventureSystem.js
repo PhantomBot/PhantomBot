ecec07dac07bc75669079fec39e8b1674293a7f6
Locxion <locxion.m@googlemail.com>
2019-01-02 18:09:34 +0100

    Added more Germand Adventures (#3)
    Added Adventures from @Locxion

--- javascript-source/lang/german/games/games-adventureSystem.js
+++ javascript-source/lang/german/games/games-adventureSystem.js
@@ -34,7 +34,7 @@ $.lang.register('adventuresystem.start.success', '$1 versucht ein Team für ein 
 $.lang.register('adventuresystem.tamagotchijoined', '$1 tritt auch dem Abenteuer bei.');
 $.lang.register('adventuresystem.top5', 'Die Top 5 Abenteurer sind: $1.');
 $.lang.register('adventuresystem.top5.empty', 'Es wurden noch keine Abenteuer Gewinner verzeichnet.');
-$.lang.register('adventuresystem.reset', 'Es wird wieder Zeit für ein neues Abenteuer! Schreibe "!adventure [$1]" um ein neues Abenteuer zu starten!');
+$.lang.register('adventuresystem.reset', 'Es wird wieder Zeit für ein neues Abenteuer! Schreibe "!adventure [$1]" um ein neues Abenteuer zu starten!!');
 
 
 $.lang.register('adventuresystem.stories.1.title', 'Schatz im Schwarzsee');
@@ -58,6 +58,34 @@ $.lang.register('adventuresystem.stories.4.chapter.2', '/me Als sich der/die Abe
 $.lang.register('adventuresystem.stories.4.chapter.3', 'Achtung! Es ist Mannbärschwein! (caught) wurde(n) in die Dunkelheit gezogen und verschwand(en) auf nimmer Wiedersehen.');
 $.lang.register('adventuresystem.stories.4.chapter.4', '(survivors), lasst uns hier verschwinden! Wir können es nicht mit ihm aufnehmen!');
 
+$.lang.register('adventuresystem.stories.5.title', 'Des Buttlers Geheimnis');
+$.lang.register('adventuresystem.stories.5.chapter.1', 'Es gibt da diesen einen Moderator bei dieser Streamerin Shurjoka. Man sagt er habe irgendwo unvorstellbare Schätze versteckt. Leider ist Er wohl auch ein mächtiger Feind!');
+$.lang.register('adventuresystem.stories.5.chapter.2', '/me Vorsichtig schlichen sich der/die Abenteuer durch die gruselige Villa und versuchten dabei keinen Lärm zu machen.');
+$.lang.register('adventuresystem.stories.5.chapter.3', 'Plötlich erstarrt (caught) auf der Stelle und kann sich nicht mehr bewegen. "Lauft um euer leben! Lasst mich/uns hier zurück! Der Buttler Locxion hat uns entdeckt!"');
+$.lang.register('adventuresystem.stories.5.chapter.4', 'Haarscharf konnte/n (survivors) den grausamen Fängen des Buttlers entfliehen! Ob es wohl nochmal jemand waagen sollte Locxion herraus zu fordern?');
+
+$.lang.register('adventuresystem.stories.6.title', 'Das wilde Reh aus dem Wald');
+$.lang.register('adventuresystem.stories.6.chapter.1', 'HERGEHÖRT! Du/Ihr hast/habt doch bestimmt schonmal den Film "Bambi" gesehen oder? Das alles ist REAL!');
+$.lang.register('adventuresystem.stories.6.chapter.2', 'Los! Greif/t dir/euch deine/eure Smartphones mit Snapchat Filter und Youtube VLog Kameras! Wir machen uns auf in den Wald!');
+$.lang.register('adventuresystem.stories.6.chapter.3', 'Reiß/t dich/euch zusammen! Kann jemand Spuren lesen? Irgendwo muss dieses Reh doch zu finden sein!?');
+$.lang.register('adventuresystem.stories.6.chapter.4', 'Passt auf wo ihr hin tretet! Sonst fallt ihr noch .... och nö ... (caught) hat es voll auf die Fresse gelegt.');
+$.lang.register('adventuresystem.stories.6.chapter.5', 'Na wenigstens hat/haben (survivors) ein paar Pilze gesammelt. Da können wir immerhin Food-Fotos auf Instagram hochladen.');
+
+$.lang.register('adventuresystem.stories.7.title', 'Abfluss FREI');
+$.lang.register('adventuresystem.stories.7.chapter.1', 'Los gehts! Haben alle ihre Siebe und Atemmasken dabei? Dann rein in die Wassermassen, auf die besten Feenstaubheraussieber!');
+$.lang.register('adventuresystem.stories.7.chapter.2', 'Leider waren diesmal nicht alle richtig bei der Sache. Nur wenige haben etwas Feenstaub aus dem Wasser sieben können! (caught) sind ausgerutscht, kopfüber im Abfluss gelandet und wurd(en) durch hinterlistige Strömungen mitgerissen.');
+$.lang.register('adventuresystem.stories.7.chapter.3', '(survivors) schaffte(n) es zurück aus dem Abfluss, beladen mit gesiebtem Feenstaub.');
+
+$.lang.register('adventuresystem.stories.8.title', 'Der Feenschatz');
+$.lang.register('adventuresystem.stories.8.chapter.1', 'Freunde! Ich habe die Koordinaten für ein geheimes Staubversteck der abtrünnigen Feen, tief im Fairy Forest, bekommen. Wir sollten uns das unbedingt ansehen!');
+$.lang.register('adventuresystem.stories.8.chapter.2', 'Vorsicht! Die bösen Feen haben überall tödliche Fallen versteckt! (caught) hatte(n) Pech und fiel(en) diesen zum Opfer.');
+$.lang.register('adventuresystem.stories.8.chapter.3', 'Verdammt, das war knapp! Aber es ist geschafft! (survivors) kehrte(n) mit vollen Taschen zurück!');
+
+$.lang.register('adventuresystem.stories.9.title', 'Mutation');
+$.lang.register('adventuresystem.stories.9.chapter.1', 'Ich denke, wir haben sehr gute Chancen, eine sagenumwobene Kreatur in dieser Höhle zu finden... Sie ist das Resultat jahrelanger Forschung nach ewigem Leben. Lacht nicht, ICH MEINE ES TOT ERNST!');
+$.lang.register('adventuresystem.stories.9.chapter.2', '/me Als sich der mutige Feentrupp seinen Weg durch die Tunnel bahnt, hören sie ein leises Flattern hinter sich...');
+$.lang.register('adventuresystem.stories.9.chapter.3', 'Achtung! Es ist Fee Hindert, das lang verschollene Experiment! (caught) wurde(n) in die Dunkelheit gezogen und verschwand(en) auf nimmer Wiedersehen.');
+$.lang.register('adventuresystem.stories.9.chapter.4', '(survivors), lasst uns hier verschwinden! Wir können es nicht mit ihr aufnehmen!');
 
 /*
  * Rules on writing your own adventure story:
