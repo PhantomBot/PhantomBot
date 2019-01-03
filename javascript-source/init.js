81c1901bc07933a72262f31761bec7c509ba77ee
IllusionaryOne <illusionaryone@illusionaryone.com>
2019-01-03 12:29:28 -0700

    Update init.js to Bring in Parity with Previous PR
    **init.js**
    - Updated Packages.tv.phantombot.PhantomBot.enableDebugging to Packages.tv.phantombot.PhantomBot.enableDebugging()

--- javascript-source/init.js
+++ javascript-source/init.js
@@ -79,7 +79,7 @@
      * @param {String} message
      */
     function consoleDebug(message) {
-        if (Packages.tv.phantombot.PhantomBot.enableDebugging) {
+        if (Packages.tv.phantombot.PhantomBot.enableDebugging()) {
             try {
                 throw new Error();
             } catch (ex) {
