44db9665fd5648c38371153b1205d1b3e8276a9f
ScaniaTV <scaniatv@users.noreply.github.com>
2019-03-04 14:50:49 -0400

    - Handle not connected error from our WS

--- source/tv/phantombot/twitch/irc/chat/utils/MessageQueue.java
+++ source/tv/phantombot/twitch/irc/chat/utils/MessageQueue.java
@@ -19,6 +19,7 @@ package tv.phantombot.twitch.irc.chat.utils;
 import java.util.HashMap;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
+import org.java_websocket.exceptions.WebsocketNotConnectedException;
 
 import tv.phantombot.twitch.irc.TwitchSession;
 import tv.phantombot.PhantomBot;
@@ -141,6 +142,9 @@ public class MessageQueue implements Runnable {
                     session.sendRaw("PRIVMSG #" + this.channelName + " :" + message.getMessage());
                     com.gmt2001.Console.out.println("[CHAT] " + message.getMessage());
                 }
+            } catch (WebsocketNotConnectedException ex) {
+                com.gmt2001.Console.err.println("Failed to send message due to being disconnected from Twitch IRC.");
+                session.reconnect();
             } catch (InterruptedException ex) {
                 com.gmt2001.Console.err.printStackTrace(ex);
             }
