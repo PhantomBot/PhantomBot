102e667f3d459fdcf2dd63d90f7d5469fbc836f3
jsnphil <jsnphil@users.noreply.github.com>
2019-03-13 18:29:10 -0500

    Initial commit

---
+++ javascript-source/lang/english/systems/custom/systems-stickSystem.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2016-2018 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+$.lang.register('sticks.drop.message', 'Kent has dropped $1 sticks on stream so far. :LUL');
+$.lang.register('sticks.breaks.message', 'Kent has broken $1 sticks on stream so far. rip');
+
+$.lang.register('sticks.set.usage', '$1 Usage: !setsticks [drops|breaks] <count>');
+$.lang.register('sticks.set.drops', 'Stick drop count set to $1');
+$.lang.register('sticks.set.breaks', 'Stick break count set to $1');
