f4688c00dd79d26e676534714a5bbbf1127be54b
Dakoda Jackson <wayne6324@gmail.com>
2019-03-28 16:59:57 +0000

    Add files via upload

---
+++ javascript-source/lang/english/systems/systems-speakSystem.js
@@ -0,0 +1,8 @@
+$.lang.register('speak.ttsvoice.usage', 'Usage: !ttsvoice [voice].');
+$.lang.register('speak.ttsvoice.set', 'ttsvoice has been set to $1.');
+$.lang.register('speak.ttsvolume.usage', 'Usage: !ttsvolume [volume].');
+$.lang.register('speak.ttsvolume.set', 'ttsvolume has been set to $1.');
+$.lang.register('speak.ttsrate.usage', 'Usage: !ttsrate [rate].');
+$.lang.register('speak.ttsrate.set', 'ttsrate has been set to $1.');
+$.lang.register('speak.ttspitch.usage', 'Usage: !ttspitch [pitch].');
+$.lang.register('speak.ttspitch.set', 'ttspitch has been set to $1.');
