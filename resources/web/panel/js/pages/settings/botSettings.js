7186664a7c3328bfe2f8e9db277269e48ecba6b7
Dakoda Jackson <wayne6324@gmail.com>
2019-03-28 17:52:30 +0000

    Update botSettings.js

--- resources/web/panel/js/pages/settings/botSettings.js
+++ resources/web/panel/js/pages/settings/botSettings.js
@@ -17,129 +17,128 @@
 
 // Function that querys all of the data we need.
 $(function() {
-	// Get logging settings.
-	socket.getDBValues('get_logging_settings', {
-		tables: ['settings', 'settings', 'settings', 'settings', 'settings',
-			'settings', 'settings'],
-		keys: ['log.file', 'log.event', 'log.error', 'log_rotate_days',
-			'response_@chat', 'response_action', 'whisperMode']
-	}, true, function(e) {
-		// Update log event toggle.
-		$('#logging-events').val((e['log.event'] === 'true' ? 'Yes' : 'No'));
-		// Update log error toggle.
-		$('#logging-errors').val((e['log.error'] === 'true' ? 'Yes' : 'No'));
-		// Update log chat toggle.
-		$('#logging-chat').val((e['log.file'] === 'true' ? 'Yes' : 'No'));
-		// Update log keep days.
-		$('#log-days').val(e.log_rotate_days);
-		// Set mute mode.
-		$('#bot-mute-mode').val((e['response_@chat'] === 'true' ? 'No' : 'Yes'));
-		// Set action mode.
-		$('#bot-action-mode').val((e['response_action'] === 'true' ? 'Yes' : 'No'));
-		// Set whisper mode.
-		$('#bot-whisper-mode').val((e['whisperMode'] === 'true' ? 'Yes' : 'No'));
-	});
+    // Get logging settings.
+    socket.getDBValues('get_logging_settings', {
+        tables: ['settings', 'settings', 'settings', 'settings', 'settings',
+            'settings', 'settings'],
+        keys: ['log.file', 'log.event', 'log.error', 'log_rotate_days',
+            'response_@chat', 'response_action', 'whisperMode']
+    }, true, function(e) {
+        // Update log event toggle.
+        $('#logging-events').val((e['log.event'] === 'true' ? 'Yes' : 'No'));
+        // Update log error toggle.
+        $('#logging-errors').val((e['log.error'] === 'true' ? 'Yes' : 'No'));
+        // Update log chat toggle.
+        $('#logging-chat').val((e['log.file'] === 'true' ? 'Yes' : 'No'));
+        // Update log keep days.
+        $('#log-days').val(e.log_rotate_days);
+        // Set mute mode.
+        $('#bot-mute-mode').val((e['response_@chat'] === 'true' ? 'No' : 'Yes'));
+        // Set action mode.
+        $('#bot-action-mode').val((e['response_action'] === 'true' ? 'Yes' : 'No'));
+        // Set whisper mode.
+        $('#bot-whisper-mode').val((e['whisperMode'] === 'true' ? 'Yes' : 'No'));
+    });
 });
 
 // Function that handles events.
 $(function() {
-	// Save button
-	$('#bot-logging-save').on('click', function() {
-		let logEvents = $('#logging-events').find(':selected').text() === 'Yes',
-			logErrors = $('#logging-errors').find(':selected').text() === 'Yes',
-			logChat = $('#logging-chat').find(':selected').text() === 'Yes',
-			muteMode = $('#bot-mute-mode').find(':selected').text() !== 'Yes',
-			actionMode = $('#bot-action-mode').find(':selected').text() === 'Yes',
-			whisperMode = $('#bot-whisper-mode').find(':selected').text() === 'Yes',
-			logDays = $('#log-days');
+    // Save button
+    $('#bot-logging-save').on('click', function() {
+        let logEvents = $('#logging-events').find(':selected').text() === 'Yes',
+            logErrors = $('#logging-errors').find(':selected').text() === 'Yes',
+            logChat = $('#logging-chat').find(':selected').text() === 'Yes',
+            muteMode = $('#bot-mute-mode').find(':selected').text() !== 'Yes',
+            actionMode = $('#bot-action-mode').find(':selected').text() === 'Yes',
+            whisperMode = $('#bot-whisper-mode').find(':selected').text() === 'Yes',
+            logDays = $('#log-days');
 
-		switch (false) {
-			case helpers.handleInputNumber(logDays):
-				break;
-			default:
-				socket.updateDBValues('update_logging_settings', {
-					tables: ['settings', 'settings', 'settings', 'settings', 'settings',
-						'settings', 'settings'],
-					keys: ['log.file', 'log.event', 'log.error', 'log_rotate_days',
-						'response_@chat', 'response_action', 'whisperMode'],
-					values: [logChat, logEvents, logErrors, logDays.val(),
-						muteMode, actionMode, whisperMode]
-				}, function() {
-					socket.sendCommand('update_logging_settings_cmd', 'reloadlogs', function() {
-						socket.sendCommand('update_misc_settings_cmd', 'reloadmisc', function() {
-							toastr.success('Successfully updated log settings!');
-						});
-					});
-				});
-		}
-	});
+        switch (false) {
+            case helpers.handleInputNumber(logDays):
+                break;
+            default:
+                socket.updateDBValues('update_logging_settings', {
+                    tables: ['settings', 'settings', 'settings', 'settings', 'settings',
+                        'settings', 'settings'],
+                    keys: ['log.file', 'log.event', 'log.error', 'log_rotate_days',
+                        'response_@chat', 'response_action', 'whisperMode'],
+                    values: [logChat, logEvents, logErrors, logDays.val(),
+                        muteMode, actionMode, whisperMode]
+                }, function() {
+                    socket.sendCommand('update_logging_settings_cmd', 'reloadlogs', function() {
+                        socket.sendCommand('update_misc_settings_cmd', 'reloadmisc', function() {
+                            toastr.success('Successfully updated log settings!');
+                        });
+                    });
+                });
+        }
+    });
 });
-
 
 // Function that handles text to speak.
 $(function() {
-	socket.getDBValues('get_text_speak_settings', {
-		tables: ['speakSettings', 'speakSettings', 'speakSettings', 'speakSettings'],
-		keys: ['textVoice', 'textVolume', 'textRate', 'textPitch']
-	}, true, function(e) {
-		// Update textVoice.
-		var voicelist = responsiveVoice.getVoices();
-	
-		var group = $('<optgroup label="Select Voice"></optgroup>');
-		group.append('<option value="' + e.textVoice + '">' + e.textVoice + '</option>');
-		voicelist.forEach(function(obj) {
-			group.append('<option value="' + obj.name + '">' + obj.name + '</option>');
-	
-		$('#text-speak-voices').append(group);
-		});
-		// Update textVolume.
-		$('#text-speak-volume').val(e.textVolume);
-		// Update textRate.
-		$('#text-speak-rate').val(e.textRate);
-		// Update textPitch.
-		$('#text-speak-pitch').val(e.textPitch);
-	});
+    socket.getDBValues('get_text_speak_settings', {
+        tables: ['speakSettings', 'speakSettings', 'speakSettings', 'speakSettings'],
+        keys: ['textVoice', 'textVolume', 'textRate', 'textPitch']
+    }, true, function(e) {
+        // Update textVoice.
+        var voicelist = responsiveVoice.getVoices();
+    
+        var group = $('<optgroup label="Select Voice"></optgroup>');
+        group.append('<option value="' + e.textVoice + '">' + e.textVoice + '</option>');
+        voicelist.forEach(function(obj) {
+            group.append('<option value="' + obj.name + '">' + obj.name + '</option>');
+    
+        $('#text-speak-voices').append(group);
+        });
+        // Update textVolume.
+        $('#text-speak-volume').val(e.textVolume);
+        // Update textRate.
+        $('#text-speak-rate').val(e.textRate);
+        // Update textPitch.
+        $('#text-speak-pitch').val(e.textPitch);
+    });
 
-	$('#text-speak-save').on('click', function() {
-		let textVoice = $('#text-speak-voices').find(':selected').text(),
-			textVolume = $('#text-speak-volume'),
-			textRate = $('#text-speak-rate'),
-			textPitch = $('#text-speak-pitch');
-		
-		switch (false) {
-			case helpers.handleInputNumber(textVolume):
-			case helpers.handleInputNumber(textRate):
-			case helpers.handleInputNumber(textPitch):
-				break;
-			default:
-				socket.updateDBValues('update_text_speak_settings', {
-					tables: ['speakSettings', 'speakSettings', 'speakSettings', 'speakSettings'],
-		keys: ['textVoice', 'textVolume', 'textRate', 'textPitch'],
-					values: [textVoice, textVolume.val(), textRate.val(), textPitch.val()]
-				}, function() {
-					socket.sendCommand('update_text_speak_settings_cmd', 'reloadspeak', function() {
-						toastr.success('Successfully updated text to speak settings!');
-					});
-				});
-		}
-	});
+    $('#text-speak-save').on('click', function() {
+        let textVoice = $('#text-speak-voices').find(':selected').text(),
+            textVolume = $('#text-speak-volume'),
+            textRate = $('#text-speak-rate'),
+            textPitch = $('#text-speak-pitch');
+        
+        switch (false) {
+            case helpers.handleInputNumber(textVolume):
+            case helpers.handleInputNumber(textRate):
+            case helpers.handleInputNumber(textPitch):
+                break;
+            default:
+                socket.updateDBValues('update_text_speak_settings', {
+                    tables: ['speakSettings', 'speakSettings', 'speakSettings', 'speakSettings'],
+        keys: ['textVoice', 'textVolume', 'textRate', 'textPitch'],
+                    values: [textVoice, textVolume.val(), textRate.val(), textPitch.val()]
+                }, function() {
+                    socket.sendCommand('update_text_speak_settings_cmd', 'reloadspeak', function() {
+                        toastr.success('Successfully updated text to speak settings!');
+                    });
+                });
+        }
+    });
 
-	$('#text-speak-test').on('click', function() {
-		let textVoice = $('#text-speak-voices').find(':selected').text(),
-			textVolume = $('#text-speak-volume'),
-			textRate = $('#text-speak-rate'),
-			textPitch = $('#text-speak-pitch'),
-			textTest = $('#text-speak-testmessage');
-		
-		switch (false) {
-			case helpers.handleInputNumber(textVolume):
-			case helpers.handleInputNumber(textRate):
-			case helpers.handleInputNumber(textPitch):
-			case helpers.handleInputString(textTest):
-				break;
-			default:
-			responsiveVoice.speak(textTest.val(), textVoice, {volume: textVolume.val(), rate: textRate.val(), pitch: textPitch.val()});
-			toastr.success('Successfully tested current speak settings!');
-		}
-	});
+    $('#text-speak-test').on('click', function() {
+        let textVoice = $('#text-speak-voices').find(':selected').text(),
+            textVolume = $('#text-speak-volume'),
+            textRate = $('#text-speak-rate'),
+            textPitch = $('#text-speak-pitch'),
+            textTest = $('#text-speak-testmessage');
+        
+        switch (false) {
+            case helpers.handleInputNumber(textVolume):
+            case helpers.handleInputNumber(textRate):
+            case helpers.handleInputNumber(textPitch):
+            case helpers.handleInputString(textTest):
+                break;
+            default:
+            responsiveVoice.speak(textTest.val(), textVoice, {volume: textVolume.val(), rate: textRate.val(), pitch: textPitch.val()});
+            toastr.success('Successfully updated text to speak settings!');
+        }
+    });
 });
