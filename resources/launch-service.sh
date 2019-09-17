eb34f03d6c38ed92dc5a5a182218a148e32309c1
gmt2001 <garymt2001@hotmail.com>
2019-09-17 11:09:58 -0400

    Updated launcher scripts to use included JRE 11 Removed no longer needed launch-jlink script

--- resources/launch-service.sh
+++ resources/launch-service.sh
@@ -26,18 +26,7 @@
 
 unset DISPLAY
 
-if [[ $(uname -m) == "arm"* ]] && [[ $(java -version 2>&1) == *"OpenJDK"* ]]; then
-    echo "It appears you are using a Raspberry Pi with OpenJDK."
-    echo "OpenJDK causes fatal errors and unexpected behavior on Raspberry Pi devices."
-    echo "Please switch to Oracle JDK".
-    echo
-    echo "Commands for Raspbian:"
-    echo "  apt-get install oracle-java8-jdk"
-    echo "  update-alternatives --config java"
-    echo
-    echo "When you issue the update-alternatives command, select Oracle JDK"
-    exit 1
-elif [[ $(uname) -eq "Darwin" ]]; then
+if [[ $(uname)=="Darwin" ]]; then
     SOURCE="${BASH_SOURCE[0]}"
     while [ -h "$SOURCE" ]; do
         DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
@@ -46,17 +35,10 @@ elif [[ $(uname) -eq "Darwin" ]]; then
     done
     DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
     cd "$DIR"
+	JAVA="./java-runtime-macos/bin/java"
 else
     cd $(dirname $(readlink -f $0))
+	JAVA="./java-runtime-linux/bin/java"
 fi
 
-if type -p java 1>/dev/null 2>/dev/null; then
-    _java=java
-elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    _java="$JAVA_HOME/bin/java"
-else
-    echo "You don't have Java installed! Download it from https://www.java.com/en/download/"
-    exit 1
-fi
-
-java -Xms1m -Dfile.encoding=UTF-8 -jar PhantomBot.jar
+${JAVA} -Xms1m -Dfile.encoding=UTF-8 -jar PhantomBot.jar ${1}
