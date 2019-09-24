4d2f6d668d8f17856135daed56d3bd22cf22bc8f
gmt2001 <garymt2001@hotmail.com>
2019-09-23 21:34:25 -0400

    Added jlink-jdk11 script for macOS Removed unneccessary jlink-jdk12 script

---
+++ development-resources/jlink-jdk11-macos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#  
+# Copyright (C) 2016-2018 phantombot.tv
+#  
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#  
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    SOURCE="$(readlink "$SOURCE")"
+    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd "$DIR"
+
+JDK="/Library/Java/JavaVirtualMachines/jdk-11.0.4.jdk/Contents/Home/bin"
+${JDK}/jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.desktop,java.management,java.sql,java.naming,jdk.httpserver,jdk.jdwp.agent --output ..\resources\java-runtime-macos
