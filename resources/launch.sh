abae0c0f8fb180aec6b99171dee558ca1ccd722c
gmt2001 <garymt2001@hotmail.com>
2019-09-30 16:06:03 -0400

    Updated linux launchers to inform users when their OS may not have Java 11 available Also added link to instructions for Debian 9 to use backports instead of upgrading closes #99

--- resources/launch.sh
+++ resources/launch.sh
@@ -38,28 +38,69 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     JAVA="./java-runtime-macos/bin/java"
 elif [[ "$MACHTYPE" != "x86_64"* ]]; then
     cd $(dirname $(readlink -f $0))
+    osdist=$(cat /etc/os-release | awk '/^ID(_LIKE)?=/' | sed 's/"//g' | sort --field-separator== --key=1,1 --dictionary-order --reverse | cut -d = -f 2 | awk 'FNR == 1')
+    osdist2=$(cat /etc/os-release | awk '/^ID(_LIKE)?=/' | sed 's/"//g' | sort --field-separator== --key=1,1 --dictionary-order --reverse | cut -d = -f 2 | awk 'FNR == 2')
+    osver=$(cat /etc/os-release | awk '/^VERSION_ID=/' | sed 's/"//g' | cut -d = -f 2)
     JAVA=$(which java)
-    
+
     if (( $? > 0 )); then
         jvermaj=0
     else
         jvermaj=$(java --version | awk 'FNR == 1 { print $2 }' | cut -d . -f 1)
     fi
-    
+
     if (( jvermaj < 11 )); then
         echo "PhantomBot requires Java 11 or later to run."
         echo
-        echo "Please install the package openjdk-11-jre-headless (Debian/rPi) or java-11-openjdk (RHEL/CentOS)"
-        echo
-        echo "The commands to do this are (Debian/rPi):"
-        echo "   sudo apt-get install openjdk-11-jre-headless"
-        echo "   sudo update-alternatives --config java"
-        echo
-		echo "The commands to do this are (RHEL/CentOS):"
-        echo "   sudo yum install java-11-openjdk"
-        echo "   sudo alternatives --config java"
-        echo
-        echo "When you issue the update-alternatives or alternatives command, select the option for java-11-openjdk"
+
+        if  [[ "$osdist" == *"debian"* ]]; then
+            echo "Please install the package openjdk-11-jre-headless"
+            echo
+
+            if (( osver < 10 )); then
+                echo "WARNING: You are running a Debian derivative lower than version 10 (buster)"
+                echo "Java 11 may not be available on this version"
+                echo "It is recommended to upgrade to at least Debian 10 (buster)"
+                echo "NOTE: Upgrading the major version of the OS usually means a clean install (wipe)"
+                echo
+
+                if (( osver == 9 )); then
+                    echo "Alternatively, you can add the stretch-backports repository to apt and then you should be able to install openjdk-11-jre-headless"
+                    echo "You can find instructions at https://github.com/superjamie/lazyweb/wiki/Raspberry-Pi-Debian-Backports#installation"
+                    echo
+                fi
+            fi
+
+            echo "The commands to do this are:"
+            echo "   sudo apt-get install openjdk-11-jre-headless"
+            echo "   sudo update-alternatives --config java"
+            echo
+            echo "When you issue the update-alternatives command, select the option for java-11-openjdk"
+        elif  [[ "$osdist" == *"fedora"* || "$osdist" == *"rhel"* ]]; then
+            echo "Please install the package java-11-openjdk-headless"
+            echo
+
+            if [[ "$osdist" == *"rhel"* || "$osdist2" == *"rhel"* ]] && (( osver < 7 )); then
+                echo "WARNING: You are running a RHEL derivative lower than version 7"
+                echo "Java 11 may not be available on this version"
+                echo "It is recommended to upgrade to at least RHEL 7"
+                echo "NOTE: Upgrading the major version of the OS usually means a clean install (wipe)"
+                echo
+            elif (( osver < 29 )); then
+                echo "WARNING: You are running a Fedora derivative lower than version 29"
+                echo "Java 11 may not be available on this version"
+                echo "It is recommended to upgrade to at least Fedora 29"
+                echo "NOTE: Upgrading the major version of the OS usually means a clean install (wipe)"
+                echo
+            fi
+
+            echo "The commands to do this are:"
+            echo "   sudo yum install java-11-openjdk-headless"
+            echo "   sudo alternatives --config java"
+            echo
+            echo "When you issue the alternatives command, select the option for java-11-openjdk"
+        fi
+
         exit 1
     fi
 else
