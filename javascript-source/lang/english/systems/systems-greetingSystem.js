171a4ae96775fcc50e497944de0bfde5d9f64e5d
AevumDecessus <aevum@decess.us>
2020-02-07 19:02:17 +0000

    Added "setdefault" handler to greeting system

--- javascript-source/lang/english/systems/systems-greetingSystem.js
+++ javascript-source/lang/english/systems/systems-greetingSystem.js
@@ -18,9 +18,10 @@
 $.lang.register('greetingsystem.set.autogreet.enabled', 'Auto Greeting Enabled. $1 will now greet viewers with a greeting configured.');
 $.lang.register('greetingsystem.set.autogreet.disabled', 'Auto Greeting Disabled');
 $.lang.register('greetingsystem.set.autogreet.noname', 'Please use the (name) tag in the default greeting.');
+$.lang.register('greetingsystem.set.default.success', 'Changed the default greeting to "$1".');
 $.lang.register('greetingsystem.set.personal.success', 'Changed your personal greeting to "$1".');
 $.lang.register('greetingsystem.remove.personal.success', 'Successfully removed your personal greeting message.');
-$.lang.register('greetingsystem.generalusage.admin', 'Usage: !greeting [toggle | enable [default | message text] | disable ]. Tags for message text: (name)');
+$.lang.register('greetingsystem.generalusage.admin', 'Usage: !greeting [toggle | enable [default | message text] | disable | setdefault ]. Tags for message text: (name)');
 $.lang.register('greetingsystem.generalusage.other', 'Usage: !greeting [ enable [default | message text] | disable ]. Tags for message text: (name)');
 $.lang.register('greetingsystem.cooldown.usage', 'Usage: !greeting cooldown [hours]');
 $.lang.register('greetingsystem.cooldown.success', 'Greetings cooldown set to $1 hours.');
