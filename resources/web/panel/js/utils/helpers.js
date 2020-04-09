9d038a71aa1556a59382793b957a178b3884747d
BrandenB <18152900+BrandenB@users.noreply.github.com>
2020-04-09 18:26:50 -0300

    - Removed version

--- resources/web/panel/js/utils/helpers.js
+++ resources/web/panel/js/utils/helpers.js
@@ -33,7 +33,7 @@ $(function() {
     helpers.LOG_TYPE = helpers.DEBUG_STATES;
     // Panel version. SEE: https://semver.org/
     // Example: MAJOR.MINOR.PATCH
-    helpers.PANEL_VERSION = "1.0.0";
+    helpers.PANEL_VERSION = "NONE";
 
     /*
      * @function adds commas to thousands.
