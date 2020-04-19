b35e3c93ef355c3d7d6557714bd45eca5109fd24
Steve Forster <superfraggle@superfraggle.com>
2020-04-19 11:23:30 +0100

    Fixing  "[init.js:317] Error with Event Handler [command] Script [./commands/customCommands.js] Stacktrace [customCommands.js:1025 > init.js:315 > init.js:523] Exception [TypeError: Cannot call property length in object" Error when adding token.

--- javascript-source/commands/customCommands.js
+++ javascript-source/commands/customCommands.js
@@ -1021,7 +1021,7 @@
                 $.say($.whisperPrefix(sender) + $.lang.get('customcommands.token.success', action));
             }
 
-            if (argsString.length() === 0) {
+            if (argsString.length === 0) {
                 $.inidb.RemoveKey('commandtoken', '', action);
             } else {
                 $.inidb.SetString('commandtoken', '', action, argsString);
