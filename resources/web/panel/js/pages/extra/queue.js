34d8669d95195b54c63a672b7d0fa86a51575063
gmt2001 <garymt2001@hotmail.com>
2020-06-06 23:19:36 -0400

    Prepped the panel for the switch to Content Security Policy: Frame Ancestors closes gmt2001/PhantomBot-1#132

--- resources/web/panel/js/pages/extra/queue.js
+++ resources/web/panel/js/pages/extra/queue.js
@@ -16,25 +16,29 @@
  */
 
 // Function that querys all of the data we need.
-$(run = function() {
+$(run = function () {
     // Check if the module is enabled.
     socket.getDBValues('queue_module', {
         tables: ['modules', 'queueSettings'],
         keys: ['./systems/queueSystem.js', 'isActive']
-    }, true, function(e) {
+    }, true, function (e) {
         // If the module is off, don't load any data.
         if (!helpers.handleModuleLoadUp(['queueModule', 'queueListModule'], e['./systems/queueSystem.js'], 'queueModuleToggle')) {
             // Remove the chat.
             $('#queue-chat').find('iframe').remove();
             return;
         } else {
-            // Add Twitch chat.
-            $('#queue-chat').html($('<iframe/>', {
-                'frameborder': '0',
-                'scrolling': 'no',
-                'style': 'width: 100%; height: 532px;',
-                'src': 'https://www.twitch.tv/embed/' + getChannelName() + '/chat' + (helpers.isDark ? '?darkpopout' : '')
-            }));
+            if (location.protocol.toLowerCase().startsWith('https')) {
+                // Add Twitch chat.
+                $('#queue-chat').html($('<iframe/>', {
+                    'frameborder': '0',
+                    'scrolling': 'no',
+                    'style': 'width: 100%; height: 532px;',
+                    'src': 'https://www.twitch.tv/embed/' + getChannelName() + '/chat' + (helpers.isDark ? '?darkpopout' : '') + '&parent=' + location.hostname
+                }));
+            } else {
+                $('#queue-chat').html('Due to changes by Twitch, the chat panel can no longer be displayed unless you enable SSL on the PhantomBot Panel');
+            }
         }
 
         // Update the open button to close if the queue is active.
@@ -45,12 +49,12 @@ $(run = function() {
         }
 
         // Function that updates the queue list.
-        helpers.temp.updateQueueList = function() {
+        helpers.temp.updateQueueList = function () {
             // Get queue list.
-            socket.getDBTableValues('get_queue_list', 'queue', function(results) {
+            socket.getDBTableValues('get_queue_list', 'queue', function (results) {
                 const table = $('#queue-table');
 
-                const trim = function(username) {
+                const trim = function (username) {
                     if (username.length > 15) {
                         return username.substr(0, 15) + '...';
                     } else {
@@ -59,7 +63,7 @@ $(run = function() {
                 };
 
                 // Sort.
-                results.sort(function(a, b) {
+                results.sort(function (a, b) {
                     return parseInt(JSON.parse(a.value).position) - parseInt(JSON.parse(b.value).position);
                 });
 
@@ -68,7 +72,7 @@ $(run = function() {
 
                 for (let i = 0; i < results.length; i++) {
                     const json = JSON.parse(results[i].value),
-                        tr = $('<tr/>');
+                            tr = $('<tr/>');
 
                     // Add position.
                     tr.append($('<td/>', {
@@ -98,9 +102,9 @@ $(run = function() {
                             'html': $('<i/>', {
                                 'class': 'fa fa-trash'
                             }),
-                            'click': function() {
+                            'click': function () {
                                 socket.wsEvent('rm_queue_user', './systems/queueSystem.js', null,
-                                    ['remove', results[i].key], helpers.temp.updateQueueList);
+                                        ['remove', results[i].key], helpers.temp.updateQueueList);
                             }
                         })
                     }));
@@ -116,33 +120,33 @@ $(run = function() {
 });
 
 // Function that handlers the loading of events.
-$(function() {
+$(function () {
     const QUEUE_SCRIPT = './systems/queueSystem.js';
     let canUpdate = true;
 
     /*
      * @function Clears the input boxes of the queue.
      */
-    const clearQueueInput = function() {
+    const clearQueueInput = function () {
         $('#queue-title').val('');
         $('#queue-cost, #queue-size').val('0');
         $('#queue-permission').val('Viewers');
     };
 
     // Toggle for the module.
-    $('#queueModuleToggle').on('change', function() {
+    $('#queueModuleToggle').on('change', function () {
         // Enable the module then query the data.
         socket.sendCommandSync('queue_module_toggle_cmd',
-            'module ' + ($(this).is(':checked') ? 'enablesilent' : 'disablesilent') + ' ./systems/queueSystem.js', run);
+                'module ' + ($(this).is(':checked') ? 'enablesilent' : 'disablesilent') + ' ./systems/queueSystem.js', run);
     });
 
     // Queue open/close button.
-    $('#open-or-close-queue').on('click', function() {
+    $('#open-or-close-queue').on('click', function () {
         if ($(this)[0].innerText.trim() === 'Open') {
             let title = $('#queue-title'),
-                cost = $('#queue-cost'),
-                size = $('#queue-size'),
-                permission = $('#queue-permission').find(':selected').text();
+                    cost = $('#queue-cost'),
+                    size = $('#queue-size'),
+                    permission = $('#queue-permission').find(':selected').text();
 
             switch (false) {
                 case helpers.handleInputString(title):
@@ -150,9 +154,9 @@ $(function() {
                 case helpers.handleInputNumber(size, 0):
                     break;
                 default:
-                    socket.sendCommand('queue_permisison_update', 'permcomsilent joinqueue ' + helpers.getGroupIdByName(permission, true), function() {
-                        socket.updateDBValue('queue_command_cost', 'pricecom', 'joinqueue', cost.val(), function() {
-                            socket.wsEvent('queue_open_ws', QUEUE_SCRIPT, null, ['open', size.val(), title.val()], function() {
+                    socket.sendCommand('queue_permisison_update', 'permcomsilent joinqueue ' + helpers.getGroupIdByName(permission, true), function () {
+                        socket.updateDBValue('queue_command_cost', 'pricecom', 'joinqueue', cost.val(), function () {
+                            socket.wsEvent('queue_open_ws', QUEUE_SCRIPT, null, ['open', size.val(), title.val()], function () {
                                 toastr.success('Successfully opened the queue!');
                                 // Update the button.
                                 $('#open-or-close-queue').html($('<i/>', {
@@ -163,7 +167,7 @@ $(function() {
                     });
             }
         } else {
-            socket.wsEvent('close_queue_ws', QUEUE_SCRIPT, null, ['close'], function() {
+            socket.wsEvent('close_queue_ws', QUEUE_SCRIPT, null, ['close'], function () {
                 toastr.success('Successfully closed the queue!');
                 clearQueueInput();
                 // Update the button.
@@ -175,8 +179,8 @@ $(function() {
     });
 
     // Clear queue command.
-    $('#reset-queue').on('click', function() {
-        socket.wsEvent('clear_queue_ws', QUEUE_SCRIPT, null, ['clear'], function() {
+    $('#reset-queue').on('click', function () {
+        socket.wsEvent('clear_queue_ws', QUEUE_SCRIPT, null, ['clear'], function () {
             toastr.success('Successfully cleared the queue!');
             clearQueueInput();
             helpers.temp.updateQueueList();
@@ -184,39 +188,39 @@ $(function() {
     });
 
     // Draw users command.
-    $('#draw-queue').on('click', function() {
+    $('#draw-queue').on('click', function () {
         helpers.getModal('queue-draw-users', 'Draw Users', 'Draw', $('<form/>', {
             'role': 'form'
         })
-        // Append amount to draw
-        .append(helpers.getInputGroup('draw-amount', 'number', 'Number of Users to Pick', '', '1', 'The amount of users to be drawn from the queue.')),
-        // Callback.
-        function() {
-            let amount = $('#draw-amount');
+                // Append amount to draw
+                .append(helpers.getInputGroup('draw-amount', 'number', 'Number of Users to Pick', '', '1', 'The amount of users to be drawn from the queue.')),
+                // Callback.
+                        function () {
+                            let amount = $('#draw-amount');
 
-            switch (false) {
-                case helpers.handleInputNumber(amount, 1, 5):
-                    break;
-                default:
-                    socket.wsEvent('draw_queue_users', QUEUE_SCRIPT, null, ['pick', amount.val()], function() {
-                        // Alert the user.
-                        toastr.success('Drew ' + amount.val() + ' users from the queue!');
-                        // Update the list.
-                        helpers.temp.updateQueueList();
-                        // Close the modal.
-                        $('#queue-draw-users').modal('toggle');
-                    });
-            }
-        }).modal('toggle');
-    });
+                            switch (false) {
+                                case helpers.handleInputNumber(amount, 1, 5):
+                                    break;
+                                default:
+                                    socket.wsEvent('draw_queue_users', QUEUE_SCRIPT, null, ['pick', amount.val()], function () {
+                                        // Alert the user.
+                                        toastr.success('Drew ' + amount.val() + ' users from the queue!');
+                                        // Update the list.
+                                        helpers.temp.updateQueueList();
+                                        // Close the modal.
+                                        $('#queue-draw-users').modal('toggle');
+                                    });
+                            }
+                        }).modal('toggle');
+            });
 
     // Handle mouse over on queue list.
-    $('#queueTable').on('mouseenter mouseleave', function(event) {
+    $('#queueTable').on('mouseenter mouseleave', function (event) {
         canUpdate = event.type === 'mouseleave';
     });
 
     // Update every 5 seconds.
-    helpers.setInterval(function() {
+    helpers.setInterval(function () {
         if (canUpdate) {
             helpers.temp.updateQueueList();
         }
