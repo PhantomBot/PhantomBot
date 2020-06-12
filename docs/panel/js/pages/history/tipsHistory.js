9f56f7731d9142eb21b2b62a7749f44b5d807414
gmt2001 <garymt2001@hotmail.com>
2020-06-12 18:14:39 -0400

    GH-Pages version of panel

---
+++ docs/panel/js/pages/history/tipsHistory.js
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2016-2019 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Function that querys all of the data we need.
+$(function() {
+	socket.getDBTableValues('get_all_tips', 'donations', function(results) {
+		let tips = [];
+
+		for (let i = 0; i < results.length; i++) {
+			let value = results[i].value;
+
+			// Make sure that it's a json object.
+			if (value.startsWith('{') && value.endsWith('}')) {
+				// Just to be safe, we'll add a try catch.
+				try {
+					let json = JSON.parse(value);
+
+					// Since we don't store what service the tip came from.
+					// We have to guess.
+					switch (true) {
+						case json.currency !== undefined && json.name !== undefined:
+							tips.push([
+								json.name,
+								helpers.getPaddedDateString(new Date(parseInt(json.created_at) * 1e3).toLocaleString()),
+								json.currency + ' ' + parseFloat(json.amount).toFixed(2),
+								'StreamLabs',
+								parseInt(json.created_at) * 1e3
+							]);
+							break;
+						case json.parameters !== undefined:
+							tips.push([
+								json.parameters.username,
+								helpers.getPaddedDateString(new Date(json.created_at.substring(0, json.created_at.indexOf('+'))).toLocaleString()), // We remove the +0200 because Safari doesn't like it.
+								json.parameters.currency + ' ' + parseFloat(json.parameters.amount).toFixed(2),
+								'TipeeeStream',
+								new Date(json.created_at.substring(0, json.created_at.indexOf('+'))).getTime()
+							]);
+							break;
+						case json.createdAt !== undefined && json.donation !== undefined:
+							tips.push([
+								json.donation.user.username,
+								helpers.getPaddedDateString(new Date(json.createdAt).toLocaleString()),
+								json.donation.currency + ' ' + parseFloat(json.donation.amount).toFixed(2),
+								'StreamElements',
+								new Date(json.createdAt).getTime()
+							]);
+							break;
+						case json.user !== undefined && json.user.name !== undefined:
+							tips.push([
+								json.user.name,
+								helpers.getPaddedDateString(new Date(json.date).toLocaleString()),
+								json.currencyCode + ' ' + parseFloat(json.amount).toFixed(2),
+								'StreamTip',
+								new Date(json.date).getTime()
+							]);
+							break;
+					}
+				} catch (ex) {
+					helpers.logError('Failed to parse tip [' + value + ']: ' + ex.stack, helpers.LOG_TYPE.FORCE);
+				}
+			}
+		}
+
+		// Create table.
+		$('#tipsHistoryTable').DataTable({
+			'searching': true,
+			'autoWidth': false,
+			'data': tips,
+			'columnDefs': [
+    			{ 'width': '30%', 'targets': 0 }
+    		],
+			'columns': [
+				{ 'title': 'Username' },
+				{ 'title': 'Date', 'orderData': [4] },
+				{ 'title': 'Amount' },
+				{ 'title': 'Tipping Service' },
+				{ 'visible': false }
+			]
+		});
+	});
+});
---
+++ docs/panel/js/utils/ajaxLoader.js
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2016-2019 phantombot.tv
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$(function() {
+    /*
+     * @function removes the page loader.
+     */
+    function showPage() {
+        helpers.log('Showing dashboard page', helpers.LOG_TYPE.DEBUG);
+        if ($('.loader').length > 0) {
+            // Stop pace since we already have one loader.
+            Pace.stop();
+            $('.loader').fadeOut(3e2, function() {
+                $('.loader').remove();
+            });
+
+            $('.main').fadeIn(2e2, function() {
+                // Fix the window height.
+                $.fn.layout.Constuctor.prototype.fix();
+            });
+
+            // Add the load for the master script.
+            $('body').append($('<script>', {
+                'async': true,
+                'src': '/panel/js/pages/global.js'
+            }));
+            helpers.log('Page shown', helpers.LOG_TYPE.DEBUG);
+        }
+    }
+
+    /*
+     * @function handles the loading of pages.
+     *
+     * @param {String} folder
+     * @param {String} page
+     */
+    function loadPage(folder, page, href) {
+        // Make sure the href isn't blank, then load the page.
+        if (page !== '') {
+            helpers.log('Starting ajax request for page: ' + folder + '/' + page, helpers.LOG_TYPE.DEBUG);
+            // Start pace loading.
+            if (href === undefined) {
+                Pace.stop();
+            } else {
+                Pace.restart();
+            }
+
+            // Clear all timers. This doesn't clear any global timers (intervals).
+            helpers.clearTimers();
+            // Remove all temp global functions.
+            helpers.temp = {};
+
+            // Load the page.
+            $.ajax({
+                cache: false,
+                dataType: 'html',
+                url: '/panel/pages/' + folder + '/' + page,
+                success: function(data) {
+                    // Set the new page.
+                    $('#page-content').html(data);
+                    // Scroll to top.
+                    $(window).scrollTop(0);
+                    if (href !== undefined) {
+                        // Set the current tab as active.
+                        $.fn.dinamicMenu(href);
+                        // Update URL.
+                        window.history.pushState(null, '', '/panel');
+                    }
+                    helpers.log('Completed ajax request for page: ' + folder + '/' + page, helpers.LOG_TYPE.DEBUG);
+                },
+                error: function(err) {
+                    helpers.logError('Failed to load page (' + page + ') => ' + err.statusText, helpers.LOG_TYPE.FORCE);
+                }
+            });
+        }
+    }
+
+    // Handles loading of tabs.
+    $('[data-folder]').on('click', function(e) {
+        e.preventDefault();
+        // Load the page.
+        loadPage($(this).data('folder'), $(this).attr('href').substring(1), this.href);
+    });
+
+    // Export to API.
+    $.showPage = showPage;
+    $.loadPage = loadPage;
+});
