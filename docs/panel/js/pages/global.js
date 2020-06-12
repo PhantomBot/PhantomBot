1d424d80a2e83faa299d98c21fc165a8509f967b
gmt2001 <garymt2001@hotmail.com>
2020-06-12 19:33:02 -0400

    Fix paths and improve errors

--- docs/panel/js/pages/global.js
+++ docs/panel/js/pages/global.js
@@ -34,11 +34,10 @@ $(function () {
                     url: 'https://' + helpers.getBotHost() + '/panel/login/remote?logout=true',
                     crossDomain: true,
                     success: function () {
-                        document.cookie = 'selectedbot=null';
-                        window.location = window.location.href + 'login';
+                        window.location = window.location.origin + window.location.pathname + 'login';
                     },
                     error: function (xhr, status, thrown) {
-                        toastr.error('Logout request failed: ' + status + " > " + thrown, '', { timeOut: 0 });
+                        toastr.error('Logout request failed: [' + xhr.status + ']' + status + " > " + thrown, '', { timeOut: 0 });
                     }
                 }
         );
