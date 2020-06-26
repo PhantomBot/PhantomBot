e75224cf2cdb5a921fe38962d0db26c821f9dc23
gmt2001 <garymt2001@hotmail.com>
2020-06-25 20:40:32 -0400

    Update skins

--- docs/panel/js/utils/helpers.js
+++ docs/panel/js/utils/helpers.js
@@ -33,7 +33,7 @@ $(function() {
     helpers.LOG_TYPE = helpers.DEBUG_STATES;
     // Panel version. SEE: https://semver.org/
     // Example: MAJOR.MINOR.PATCH
-    helpers.PANEL_VERSION = "NONE";
+    helpers.PANEL_VERSION = "1.1.0-remote";
 
     helpers.hashmap = [];
 
