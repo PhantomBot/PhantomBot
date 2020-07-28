adc165a2b83351ee15d0caa1ae380976bd7b3a5a
gmt2001 <garymt2001@hotmail.com>
2020-07-28 00:46:29 -0400

    Update copyright

--- development-resources/parse_discordcommandpath.pl
+++ development-resources/parse_discordcommandpath.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # 
-# Copyright (C) 2016-2020 phantombot.tv
+# Copyright (C) 2016-2020 phantom.bot
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
