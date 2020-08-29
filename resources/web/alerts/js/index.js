e42ab7f4e4901fdd1d4ba31052873d11cb93c1ef
gmt2001 <garymt2001@hotmail.com>
2020-08-28 21:30:40 -0400

    Fixed browser audio compatibility check closes PhantomBot/PhantomBot#2364

--- resources/web/alerts/js/index.js
+++ resources/web/alerts/js/index.js
@@ -216,14 +216,14 @@ $(function () {
             if (fileName.length > 0) {
                 break;
             }
-            if (supportsAudioType(x) !== '') {
+            if (supportsAudioType(extensions[x]) !== '') {
                 found = true;
                 $.ajax({
                     async: false,
                     method: 'HEAD',
-                    url: defaultPath + name + '.' + x,
+                    url: defaultPath + name + '.' + extensions[x],
                     success: function () {
-                        fileName = (defaultPath + name + '.' + x);
+                        fileName = (defaultPath + name + '.' + extensions[x]);
                     }
                 });
             }
