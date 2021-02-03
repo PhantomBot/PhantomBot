65b119aad5906536d4321006c5b1966622174201
Thomas Saquet <tsaquet@gmail.com>
2021-02-03 18:49:53 +0100

    load follows step by step

--- resources/web/panel/js/pages/history/followsHistory.js
+++ resources/web/panel/js/pages/history/followsHistory.js
@@ -18,6 +18,10 @@
 // Function that querys all of the data we need.
 $(function() {
 
+	const FOLLOW_STEP = 100;
+
+	let followingOffset = 0
+
 	/**
 	 *
 	 * @param results
@@ -26,6 +30,7 @@ $(function() {
 		for (let i = 0; i < results.length; i++) {
 			let follow = results[i];
 			let name = follow.key;
+			$('#follows-btn-replay-' + name).off();
 			if ($('#follows-btn-replay-' + name).length) {
 				$('#follows-btn-replay-' + name).on('click', function () {
 					socket.sendCommand('replay_follow', 'replayfollow ' + name, function () {
@@ -33,34 +38,46 @@ $(function() {
 					});
 				});
 			}
+
 		}
 	}
 
-	socket.getDBTableValues('get_all_follows_by_date', 'followedDate', function(results) {
-
-		socket.wsEvent()
+	function pushDataIntoTable(results) {
+		followingOffset = followingOffset + FOLLOW_STEP;
 		let follows = [];
 
 		for (let i = 0; i < results.length; i++) {
 			let follow = results[i];
 			let name = follow.key;
 			let date = new Date(follow.value);
-			let month = ((date.getMonth() + 1)<10? '0'+ (date.getMonth() + 1):''+(date.getMonth() + 1));
-			let day = ((date.getDate() + 1)<10? '0'+ (date.getDate() + 1):''+(date.getDate() + 1));
+			let month = ((date.getMonth() + 1) < 10 ? '0' + (date.getMonth() + 1) : '' + (date.getMonth() + 1));
+			let day = ((date.getDate()) < 10 ? '0' + (date.getDate()) : '' + (date.getDate()));
 
 			follows.push([
 				name,
-				date.getFullYear()+'-'+month+'-'+day,
-				'<button type="button" className="btn btn-primary pull-right" id="follows-btn-replay-'+name+'" data="'+name+'">Replay</button>'
+				date.getFullYear() + '-' + month + '-' + day,
+				'<button type="button" className="btn btn-primary pull-right" id="follows-btn-replay-' + name + '" data="' + name + '">Replay</button>'
 			]);
 
 		}
+
+		$('#followsHistoryTable').on( 'draw.dt', function () {
+			createClickButtonEvents(results);
+		} );
+
+		return follows;
+	}
+
+	socket.getDBTableValuesByOrder('get_all_follows_by_date', 'followedDate', FOLLOW_STEP, followingOffset, 'DESC', true, function(results) {
+
+		let follows = pushDataIntoTable(results);
 
 		// Create table.
 		$('#followsHistoryTable').DataTable({
 			'searching': true,
 			'autoWidth': false,
 			'data': follows,
+			'pageLength': 10,
 			'columnDefs': [
 				{ 'width': '30%', 'targets': 0 }
 			],
@@ -71,13 +88,29 @@ $(function() {
 			]
 		});
 
-		// First time table created
 		createClickButtonEvents(results);
-
-		// each time the table is drawn after page change
-		$('#followsHistoryTable').on( 'draw.dt', function () {
-			createClickButtonEvents(results);
-		} );
-
 	});
+
+	// On load more time button.
+	$('#follows-history-load-more').on('click', function() {
+		let table = $('#followsHistoryTable').DataTable(),
+			dataCount = table.rows().count(),
+			follows = [];
+		// Only allow more data to be loaded once the last click was fully loaded.
+		if (followingOffset === dataCount) {
+			toastr.success('Loading more users into the follows table.');
+			// Get the next 100 users.
+			socket.getDBTableValuesByOrder('get_all_follows_by_date', 'followedDate', FOLLOW_STEP, followingOffset, 'DESC', true, function(results) {
+				follows = pushDataIntoTable(results);
+
+				// Add the rows.
+				table.rows.add(follows).draw(false);
+
+				createClickButtonEvents(results);
+			});
+		} else {
+			toastr.error('Cannot load more time since there are currently some being loaded.');
+		}
+	});
+
 });
