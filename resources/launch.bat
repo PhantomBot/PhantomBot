6481e4ee72d8081f201997bdd035813842c7b583
gmt2001 <garymt2001@hotmail.com>
2021-09-19 02:12:23 -0400

    Added support for Windows Terminal as the default option

--- resources/launch.bat
+++ resources/launch.bat
@@ -17,8 +17,36 @@ REM You should have received a copy of the GNU General Public License
 REM along with this program.  If not, see <http://www.gnu.org/licenses/>.
 REM
 
+IF /I "%1" == "--nowt" GOTO :LAUNCH
+
+WHERE powershell >nul 2>nul
+IF %ERRORLEVEL% NEQ 0 GOTO :LAUNCH
+
+setlocal
+CALL :GETPARENT PARENT
+IF /I "%PARENT%" == "wt" GOTO :LAUNCH
+endlocal
+
+WHERE wt >nul 2>nul
+IF %ERRORLEVEL% EQU 0 GOTO :SWITCHTOWT
+
+GOTO :LAUNCH
+
+:GETPARENT
+SET "PSCMD=$ppid=$pid;while($i++ -lt 3 -and ($ppid=(Get-CimInstance Win32_Process -Filter ('ProcessID='+$ppid)).ParentProcessId)) {}; (Get-Process -EA Ignore -ID $ppid).Name"
+
+for /f "tokens=*" %%i in ('powershell -noprofile -command "%PSCMD%"') do SET %1=%%i
+
+GOTO :EOF
+
+:LAUNCH
 setlocal enableextensions enabledelayedexpansion
 cd %~dp0
 ".\java-runtime\bin\java" --add-exports java.base/sun.security.x509=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED -Djava.security.policy=config/security -Dinteractive -Xms1m -Dfile.encoding=UTF-8 -jar "PhantomBot.jar" %1
 endlocal
 pause
+
+GOTO :EOF
+
+:SWITCHTOWT
+wt nt --profile "Command Prompt" --startingDirectory %~dp0 --title PhantomBot launch.bat
