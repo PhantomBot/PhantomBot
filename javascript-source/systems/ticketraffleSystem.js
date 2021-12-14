26c0d2d007bf75f24c41b750d23447b0b19b5762
gmt2001 <garymt2001@hotmail.com>
2021-12-13 19:24:12 -0500

    Fix calcBonus erasing entries for regular viewers

--- javascript-source/systems/ticketraffleSystem.js
+++ javascript-source/systems/ticketraffleSystem.js
@@ -273,7 +273,7 @@
     }
 
     function calcBonus(user, event, tickets) {
-        var bonus = 0;
+        var bonus = 1;
 
         if (event.getTags().containsKey('subscriber') && event.getTags().get('subscriber').equals('1')) {
             bonus = tickets * subTMulti;
