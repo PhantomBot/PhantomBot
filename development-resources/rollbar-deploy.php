67f8e19d06693656c07dc5f79dcc4dfffd54416b
gmt2001 <garymt2001@hotmail.com>
2022-01-02 15:27:27 -0500

    Add version filtering

---
+++ development-resources/rollbar-deploy.php
@@ -0,0 +1,57 @@
+<?php
+
+$token = ''; // Authorization token for this endpoint
+$maxver = array();
+$maxver['stable'] = 1; // Maximum number of stable versions
+$maxver['stable_docker'] = 1; // Maximum number of stable docker versions
+$maxver['nightly'] = 2; // Maximum number of nightly verisons
+$maxver['nightly_docker'] = 2; // Maximum number of nightly docker verisons
+
+
+$headers = apache_request_headers();
+
+function dofilter($data) {
+    return filter_var($data, FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_BACKTICK);
+}
+
+function doexit($code, $message, $filternm) {
+    http_response_code($code);
+    echo json_encode(array('err' => 1, 'message' => $message));
+    apache_note('RBResp', $filternm);
+    exit();
+}
+
+if (!array_key_exists('x-access-token', $headers) || dofilter($headers['x-access-token']) != $token) {
+    doexit(403, 'access denied', 'token');
+}
+
+$data = file_get_contents('php://input');
+$item = json_decode($data, true);
+
+if (file_exists('rollbar-allowed-versions.json')) {
+    $allowed_versions = json_decode(file_get_contents('rollbar-allowed-versions.json'));
+} else {
+    $allowed_versions = array();
+}
+
+foreach($maxver as $v) {
+    if (!array_key_exists($v, $allowed_versions)) {
+        $allowed_versions[$v] = array();
+    }
+}
+
+if (!array_key_exists($item['type'], $allowed_versions)) {
+    doexit(400, 'environment not allowed', 'env');
+}
+
+if (!in_array($item['version'], $allowed_versions[$item['type']])) {
+    array_push($allowed_versions[$item['type']], $item['version']);
+    while (count($allowed_versions[$item['type']]) > $maxver[$item['type']]) {
+        array_shift($allowed_versions[$item['type']]);
+    }
+}
+
+file_put_contents('rollbar-allowed-versions.json', json_encode($allowed_versions));
+apache_note('RBResp', 'save');
+
+?>
