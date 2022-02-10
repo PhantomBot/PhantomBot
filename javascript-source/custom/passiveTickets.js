22a9028821994bde9d098ade28875735e7251ffe
Aaron Beauvais <psycoboy@gmail.com>
2022-02-10 09:28:35 -0700

    Changed passive tickets to use event logger

--- javascript-source/custom/passiveTickets.js
+++ javascript-source/custom/passiveTickets.js
@@ -3,22 +3,23 @@
         delay = 300000;
 
     function runTicketsPayout() {
-        $.log.error("Starting payout");
+        $.log.event("Starting payout");
         if ($.twitchcache.isStreamOnline()) {
             var activeUsers = $.getActiveUsers();
             $.streamelements.AddTicketsToUsers(activeUsers,pointsToAdd);
             var subUsers = [];
-
+            $.log.event("Sent tickets to active users");
             for (user in activeUsers) {
                 if($.isSub(user)) {
                     subUsers.push(user);
                 }
             }
             if(subUsers.length > 0) {
+                $.log.event("Sent tickets to active subscribers");
                 $.streamelements.AddTicketsToUsers(subUsers,pointsToAdd);
             }
         }
-        $.log.error("Ending payout");
+        $.log.event("Ending payout");
     }
 
     var interval = setInterval(function() {
