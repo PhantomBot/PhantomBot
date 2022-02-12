d79d37ec649b0ef15a086da950926e64f31ec36a
Aaron Beauvais <psycoboy@gmail.com>
2022-02-11 17:40:30 -0700

    Added Pancake & waffle to special

--- javascript-source/core/commandCoolDown.js
+++ javascript-source/core/commandCoolDown.js
@@ -86,6 +86,8 @@
                command == 'vote' ||
                command == 'joinqueue' ||
                command == 'ffa' ||
+               command == 'pancake' ||
+               command == 'waffle' ||
                command == $.firstCommand ||
                command == $.secondCommand ||
                command == $.raffleCommand;
