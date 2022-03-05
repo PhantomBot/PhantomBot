0dc94dce9b66e18660e7dff3c37cfbb3671da3f0
gmt2001 <garymt2001@hotmail.com>
2022-03-05 15:01:09 -0500

    Fix initializing wrong SocketChannel type

--- source/com/gmt2001/wsclient/WSClient.java
+++ source/com/gmt2001/wsclient/WSClient.java
@@ -20,7 +20,7 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.websocketx.WebSocketCloseStatus;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
 import io.netty.handler.ssl.SslContext;
@@ -130,7 +130,7 @@ public class WSClient {
 
             Bootstrap b = new Bootstrap();
             b.group(this.group)
-                    .channel(NioServerSocketChannel.class)
+                    .channel(NioSocketChannel.class)
                     .handler(new WSClientInitializer(this));
 
             this.channel = b.connect(this.host, this.port).sync().channel();
