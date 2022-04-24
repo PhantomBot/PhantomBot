7ac2b0c612523bd4aa6a1688fed285d9b36c8986
gmt2001 <garymt2001@hotmail.com>
2022-04-24 08:15:36 -0400

    Fixed the timezone for TwitchAPIv5.getClipsToday

--- source/com/gmt2001/TwitchAPIv5.java
+++ source/com/gmt2001/TwitchAPIv5.java
@@ -18,11 +18,13 @@ package com.gmt2001;
 
 import com.gmt2001.datastore.DataStore;
 import java.text.SimpleDateFormat;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.json.JSONArray;
@@ -624,7 +626,7 @@ public class TwitchAPIv5 {
     /**
      * Gets a user object by ID
      *
-     * @param user
+     * @param userID
      * @return
      */
     public JSONObject GetUserByID(String userID) throws JSONException {
@@ -1165,10 +1167,11 @@ public class TwitchAPIv5 {
      * @return JSONObject clips object.
      */
     public JSONObject getClipsToday(String channel) throws JSONException {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
         Calendar c = Calendar.getInstance();
         c.set(Calendar.HOUR_OF_DAY, 0);
         c.set(Calendar.MINUTE, 0);
+        c.setTimeZone(TimeZone.getTimeZone(ZoneOffset.UTC));
         String start = sdf.format(c.getTime());
         c.add(Calendar.DAY_OF_MONTH, 1);
         String end = sdf.format(c.getTime());
