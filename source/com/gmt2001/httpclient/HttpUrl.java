a7da9e51842e0bf685de29df1c3c34220b9f494a
gmt2001 <garymt2001@hotmail.com>
2022-04-27 19:54:27 -0400

    Use decoded URI

--- source/com/gmt2001/httpclient/HttpUrl.java
+++ source/com/gmt2001/httpclient/HttpUrl.java
@@ -91,14 +91,14 @@ public final class HttpUrl {
         uri = uri.parseServerAuthority();
         HttpUrl u = new HttpUrl();
         u.scheme = uri.getScheme();
-        u.userInfo = uri.getRawUserInfo();
+        u.userInfo = uri.getUserInfo();
         u.host = uri.getHost();
         u.port = uri.getPort();
-        u.path = uri.getRawPath();
+        u.path = uri.getPath();
         u.querySep = querySep;
 
         if (uri.getQuery() != null) {
-            for (String s : uri.getRawQuery().split(querySep)) {
+            for (String s : uri.getQuery().split(querySep)) {
                 String[] param = s.split("=", 2);
                 if (param.length == 2) {
                     u.query.put(param[0], param[1]);
