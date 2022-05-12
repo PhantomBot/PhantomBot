eddb33be2f4000fad2bac03f216a45f0a975ac03
gmt2001 <garymt2001@hotmail.com>
2022-05-12 19:27:55 -0400

    Update HTTPAuthenticatedHandler on new OAuth

--- source/tv/phantombot/PhantomBot.java
+++ source/tv/phantombot/PhantomBot.java
@@ -131,6 +131,7 @@ public final class PhantomBot implements Listener {
     private WsPanelHandler panelHandler;
     private WsYTHandler ytHandler;
     private HTTPOAuthHandler oauthHandler;
+    private HTTPAuthenticatedHandler httpAuthenticatedHandler;
 
     /* PhantomBot Information */
     private static PhantomBot instance;
@@ -473,6 +474,8 @@ public final class PhantomBot implements Listener {
             this.pubSubEdge.setOAuth(CaselessProperties.instance().getProperty("apioauth", ""));
         }
 
+        this.httpAuthenticatedHandler.updateAuth(CaselessProperties.instance().getProperty("webauth"), this.getPanelOAuth().replace("oauth:", ""));
+
         EventBus.instance().postAsync(new PropertiesReloadedEvent());
     }
 
@@ -689,7 +692,8 @@ public final class PhantomBot implements Listener {
                     CaselessProperties.instance().getPropertyAsBoolean("usehttps", true), CaselessProperties.instance().getProperty("httpsFileName", ""),
                     CaselessProperties.instance().getProperty("httpsPassword", ""), this.getBotName());
             new HTTPNoAuthHandler().register();
-            new HTTPAuthenticatedHandler(CaselessProperties.instance().getProperty("webauth"), this.getPanelOAuth().replace("oauth:", "")).register();
+            this.httpAuthenticatedHandler = new HTTPAuthenticatedHandler(CaselessProperties.instance().getProperty("webauth"), this.getPanelOAuth().replace("oauth:", ""));
+            this.httpAuthenticatedHandler.register();
             new HTTPPanelAndYTHandler(CaselessProperties.instance().getProperty("paneluser", "panel"), this.getPanelPassword()).register();
             this.oauthHandler = (HTTPOAuthHandler) new HTTPOAuthHandler(CaselessProperties.instance().getProperty("paneluser", "panel"), this.getPanelPassword()).register();
             if (CaselessProperties.instance().getPropertyAsBoolean("useeventsub", false)) {
