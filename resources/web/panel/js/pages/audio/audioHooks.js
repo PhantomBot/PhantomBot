c6b4cf8511dad0e23dc20cfce3620722393954d7
Sartharon <572591+Sartharon@users.noreply.github.com>
2022-06-06 12:07:52 +0200

    Call function instead of trying to cast it ...

--- resources/web/panel/js/pages/audio/audioHooks.js
+++ resources/web/panel/js/pages/audio/audioHooks.js
@@ -393,7 +393,7 @@ $(function() {
             .append(helpers.getDropdownGroup('command-audio', 'Audio Hook', 'Select an Audio Hook', audioNames, 'Audio hook to be played when the command is ran.'))
             // Append a select option for the command permission.
             .append(helpers.getDropdownGroup('command-permission', 'User Level', 'Viewers',
-                helpers.getPermGroupNames, 'Users who can run the command.'))
+                helpers.getPermGroupNames(), 'Users who can run the command.'))
             // Add an advance section that can be opened with a button toggle.
             .append($('<div/>', {
                 'class': 'collapse',
