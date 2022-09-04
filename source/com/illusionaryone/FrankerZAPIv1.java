44cc0983c1719e9b83381265ccbe4b47832b85e2
gmt2001 <garymt2001@hotmail.com>
2022-09-04 12:15:36 -0400

    Close resources

--- source/com/illusionaryone/FrankerZAPIv1.java
+++ source/com/illusionaryone/FrankerZAPIv1.java
@@ -108,8 +108,9 @@ public class FrankerZAPIv1 {
                 inputStream = urlConn.getErrorStream();
             }
 
-            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")));
-            jsonText = readAll(rd);
+            try ( BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream, Charset.forName("UTF-8")))) {
+                jsonText = readAll(rd);
+            }
             jsonResult = new JSONObject(jsonText);
             fillJSONObject(jsonResult, true, "GET", urlAddress, urlConn.getResponseCode(), "", "", jsonText);
         } catch (JSONException ex) {
