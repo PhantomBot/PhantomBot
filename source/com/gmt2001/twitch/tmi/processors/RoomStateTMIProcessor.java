5627d64f78483fb36b757a100fec89cfa49df4ff
gmt2001 <garymt2001@hotmail.com>
2022-09-12 21:25:59 -0400

    Implement GLOBALUSERSTATE, NOTICE, ROOMSTATE, USERNOTICE, and WHISPER Remove old SubscriberBulkGifter tracker in favor of a simpler one

--- source/com/gmt2001/twitch/tmi/processors/RoomStateTMIProcessor.java
+++ source/com/gmt2001/twitch/tmi/processors/RoomStateTMIProcessor.java
@@ -1,13 +1,39 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2016-2022 phantombot.github.io/PhantomBot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.gmt2001.twitch.tmi.processors;
 
+import com.gmt2001.twitch.tmi.TMIMessage;
+import tv.phantombot.event.EventBus;
+import tv.phantombot.event.irc.message.IrcPrivateMessageEvent;
+
 /**
+ * Handles the ROOMSTATE TMI Command
  *
  * @author gmt2001
  */
-public class RoomStateTMIProcessor {
+public class RoomStateTMIProcessor extends AbstractTMIProcessor {
+
+    public RoomStateTMIProcessor() {
+        super("ROOMSTATE");
+    }
+
+    @Override
+    protected void onMessage(TMIMessage item) {
+        EventBus.instance().postAsync(new IrcPrivateMessageEvent(this.session(), "jtv", item.parameters(), item.tags()));
+    }
 
 }
--- source/com/gmt2001/twitch/tmi/processors/WhisperTMIProcessor.java
+++ source/com/gmt2001/twitch/tmi/processors/WhisperTMIProcessor.java
@@ -1,13 +1,40 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Copyright (C) 2016-2022 phantombot.github.io/PhantomBot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.gmt2001.twitch.tmi.processors;
 
+import com.gmt2001.twitch.tmi.TMIMessage;
+import tv.phantombot.event.EventBus;
+import tv.phantombot.event.irc.message.IrcPrivateMessageEvent;
+
 /**
+ * Handles the WHISPER TMI Command
  *
  * @author gmt2001
  */
-public class WhisperTMIProcessor {
+public class WhisperTMIProcessor extends AbstractTMIProcessor {
+
+    public WhisperTMIProcessor() {
+        super("WHISPER");
+    }
+
+    @Override
+    protected void onMessage(TMIMessage item) {
+        com.gmt2001.Console.out.println("[WHISPER] " + item.nick() + ": " + item.parameters());
+        EventBus.instance().postAsync(new IrcPrivateMessageEvent(this.session(), item.nick(), item.parameters(), item.tags()));
+    }
 
 }
