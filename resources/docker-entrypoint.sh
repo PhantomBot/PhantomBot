655fc74eba14082da9d96ebc951edd6eb319a8d1
Bonny1992 <bonny1992@gmail.com>
2022-09-27 03:26:06 +0200

    Added the ability to use UID and GID variables instead of the --user directive

--- resources/docker-entrypoint.sh
+++ resources/docker-entrypoint.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 set -e
 
+id phantombot
+
+if [[ ${UID+x} && ${GID+x} ]]; then
+	if [[ "$(id -u phantombot)" != $UID ]]; then
+		echo "Setting user to UID/GID: $UID / $GID"
+		groupmod -g $GID phantombot
+		usermod -u $UID -g $GID phantombot
+	fi
+fi
+
+id phantombot
+
 # allow the container to be started with `--user`
 if [ "$(id -u)" = '0' -a ! -v ALLOW_ROOT ]; then
+	mkdir -p /opt/PhantomBot_data/logs /opt/PhantomBot_data/dbbackup /opt/PhantomBot_data/addons /opt/PhantomBot_data/config /opt/PhantomBot_data/gameslist
+	touch /opt/PhantomBot_data/gameslist/gamesList.txt
+	chown -R phantombot:phantombot /opt/PhantomBot_data;
 	find /opt/PhantomBot \! -type l \! -user phantombot -exec chown phantombot:phantombot '{}' +
 	find /opt/PhantomBot_data \! -type l \! -user phantombot -exec chown phantombot:phantombot '{}' +
 	exec gosu phantombot "$0" "$@"
