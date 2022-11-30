c5296858b83ebcebf4418c36848f4bbb2da5fef1
gmt2001 <garymt2001@hotmail.com>
2022-11-29 19:57:40 -0500

    jsHint

--- javascript-source/core/initCommands.js
+++ javascript-source/core/initCommands.js
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* global java, Packages */
+/* global Packages */
 
 (function() {
     var bot = $.botName.toLowerCase();
