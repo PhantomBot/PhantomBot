36cc4d413bcc0ffe13efd1575c6550e544ca6954
gmt2001 <garymt2001@hotmail.com>
2022-12-02 10:04:40 -0500

    jsHint

--- development-resources/javascript-source/custom/test.js
+++ development-resources/javascript-source/custom/test.js
@@ -15,6 +15,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* global Packages */
+
 // Provides commands for running tests
 (function () {
     var testTimeoutId = null;
