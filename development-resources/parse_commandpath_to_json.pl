6254cc29a05e9536a1bb72f3ed1042ae7adad437
gmt2001 <garymt2001@hotmail.com>
2023-01-03 17:51:13 -0500

    Update Copyright

--- development-resources/parse_commandpath_to_json.pl
+++ development-resources/parse_commandpath_to_json.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 # 
-# Copyright (C) 2016-2022 phantombot.github.io/PhantomBot
+# Copyright (C) 2016-2023 phantombot.github.io/PhantomBot
 # 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- source/com/illusionaryone/TwitchAlertsAPIv1.java
+++ source/com/illusionaryone/TwitchAlertsAPIv1.java
@@ -1,7 +1,7 @@
 /* astyle --style=java --indent=spaces=4 */
 
  /*
- * Copyright (C) 2016-2022 phantombot.github.io/PhantomBot
+ * Copyright (C) 2016-2023 phantombot.github.io/PhantomBot
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
