0f5065c4e681dfa8a53e93421d767a9438677e3f
gmt2001 <garymt2001@hotmail.com>
2023-01-04 19:16:51 -0500

    Fix Twitter RFC shit

--- source/com/gmt2001/TwitterAPI.java
+++ source/com/gmt2001/TwitterAPI.java
@@ -363,7 +363,7 @@ public class TwitterAPI implements ApiClientCallback, Listener {
             }
 
             if (startTime != null) {
-                req.startTime(startTime);
+                req.startTime(startTime.withNano(0));
             }
 
             try {
@@ -457,7 +457,7 @@ public class TwitterAPI implements ApiClientCallback, Listener {
             req.maxResults(sinceId != null && sinceId.isBlank()
                     ? CaselessProperties.instance().getPropertyAsInt("twittertimelinelimit", 15)
                     : CaselessProperties.instance().getPropertyAsInt("twittertimelineextendedlimit", 30))
-                    .startTime(OffsetDateTime.now().minusDays(1));
+                    .startTime(OffsetDateTime.now().minusDays(1).withNano(0));
 
             if (sinceId != null && !sinceId.isBlank()) {
                 req.sinceId(sinceId);
@@ -492,7 +492,7 @@ public class TwitterAPI implements ApiClientCallback, Listener {
      */
     public List<TweetToUser> getRetweetsOfMe(String sinceId) {
         if (this.authenticated()) {
-            List<Tweet> tweets = this.getUserTimeline(sinceId, OffsetDateTime.now().minusDays(1));
+            List<Tweet> tweets = this.getUserTimeline(sinceId, OffsetDateTime.now().minusDays(1).withNano(0));
 
             if (tweets != null && !tweets.isEmpty()) {
                 List<TweetToUser> ret = new ArrayList<>();
