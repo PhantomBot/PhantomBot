75f14440703e70cce0b440334f8245c330ea5c0d
gmt2001 <garymt2001@hotmail.com>
2023-01-10 19:17:18 -0500

    Add missing null checks

--- source/tv/phantombot/PhantomBot.java
+++ source/tv/phantombot/PhantomBot.java
@@ -524,10 +524,21 @@ public final class PhantomBot implements Listener {
             this.pubSubEdge.setOAuth(CaselessProperties.instance().getProperty("apioauth", ""));
         }
 
-        this.httpAuthenticatedHandler.updateAuth(CaselessProperties.instance().getProperty("webauth"), this.getPanelOAuth().replace("oauth:", ""));
-        this.oauthHandler.updateAuth();
-        this.httpPanelHandler.updateAuth();
-        this.httpSetupHandler.updateAuth();
+        if (this.httpAuthenticatedHandler != null) {
+            this.httpAuthenticatedHandler.updateAuth(CaselessProperties.instance().getProperty("webauth"), this.getPanelOAuth().replace("oauth:", ""));
+        }
+
+        if (this.oauthHandler != null) {
+            this.oauthHandler.updateAuth();
+        }
+
+        if (this.httpPanelHandler != null) {
+            this.httpPanelHandler.updateAuth();
+        }
+
+        if (this.httpSetupHandler != null) {
+            this.httpSetupHandler.updateAuth();
+        }
 
         EventBus.instance().postAsync(new PropertiesReloadedEvent());
     }
