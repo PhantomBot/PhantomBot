607da75502ba2ce97697bbea86c1f65cfbfaa48f
gmt2001 <garymt2001@hotmail.com>
2023-02-21 19:14:05 -0500

    Implement EventSub hooks Finish prediction open implementation Use const

--- javascript-source/handlers/predictionHandler.js
+++ javascript-source/handlers/predictionHandler.js
@@ -19,40 +19,158 @@
 
 (function () {
     let currentPrediction = null;
+    let isCommandPrediction = false;
+
+    /*
+     * @event eventSubPredictionBegin
+     */
+    $.bind('eventSubPredictionBegin', function (event) {
+        if ($.jsString(event.event().broadcasterUserId()) === $.jsString($.username.getIDCaster())) {
+            let prediction = null;
+
+            try {
+                prediction = {
+                    id: $.jsString(event.event().id()),
+                    title: $.jsString(event.event().title()),
+                    outcomes: [],
+                    locked: false
+                };
+
+                for (let i = 0; i < event.event().outcomes().size(); i++) {
+                    prediction.outcomes.push({
+                        index: i + 1,
+                        id: $.jsString(event.event().outcomes().get(i).id()),
+                        title: $.jsString(event.event().outcomes().get(i).title())
+                    });
+                }
+            } catch (e) {
+                prediction = null;
+                $.log.error(e);
+            }
+
+            if (prediction !== null) {
+                currentPrediction = prediction;
+
+                if (isCommandPrediction) {
+                    let msg = $.lang.get('predictionhandler.open.header', currentPrediction.title);
+
+                    for (const outcome of currentPrediction.outcomes) {
+                        msg += $.lang.get('predictionhandler.option', outcome.index, outcome.title);
+                    }
+
+                    $.say(msg);
+                }
+            }
+        }
+    });
+
+    /*
+     * @event eventSubPredictionEnd
+     */
+    $.bind('eventSubPredictionEnd', function (event) {
+        if ($.jsString(event.event().broadcasterUserId()) === $.jsString($.username.getIDCaster())) {
+            try {
+                if (currentPrediction !== null && isCommandPrediction) {
+                    if (event.event().status() === Packages.com.gmt2001.twitch.eventsub.subscriptions.channel.prediction.PredictionEnd.Status.RESOLVED) {
+                        const winningOutcomeId = $.jsString(event.event().winningOutcomeId());
+                        let winningOutcome = null;
+
+                        for (const outcome of currentPrediction.outcomes) {
+                            if (outcome.id === winningOutcomeId) {
+                                winningOutcome = outcome;
+                            }
+                        }
+
+                        if (winningOutcome !== null) {
+                            $.say($.lang.get('predictionhandler.resolve', currentPrediction.title, winningOutcome.title));
+                        }
+                    } else {
+                        $.say($.lang.get('predictionhandler.cancel'));
+                    }
+                }
+            } catch (e) {
+                $.log.error(e);
+            }
+
+            currentPrediction = null;
+            isCommandPrediction = false;
+        }
+    });
+
+    /*
+     * @event eventSubPredictionLock
+     */
+    $.bind('eventSubPredictionLock', function (event) {
+        if ($.jsString(event.event().broadcasterUserId()) === $.jsString($.username.getIDCaster())) {
+            if (currentPrediction !== null) {
+                currentPrediction.locked = true;
+
+                if (isCommandPrediction) {
+                    $.say($.lang.get('predictionhandler.lock'));
+                }
+            }
+        }
+    });
+
+    /*
+     * @event eventSubWelcome
+     */
+    $.bind('eventSubWelcome', function (event) {
+        if (!event.isReconnect()) {
+            const subscriptions = [
+                Packages.com.gmt2001.twitch.eventsub.subscriptions.channel.prediction.PredictionBegin,
+                Packages.com.gmt2001.twitch.eventsub.subscriptions.channel.prediction.PredictionEnd,
+                Packages.com.gmt2001.twitch.eventsub.subscriptions.channel.prediction.PredictionLock
+            ];
+
+            for (const subscription of subscriptions) {
+                const newSubscription = new subscription($.username.getIDCaster());
+                try {
+                    newSubscription.create().block();
+                } catch (ex) {
+                    $.log.error(ex);
+                }
+            }
+        }
+    });
 
     /*
      * @event command
      */
     $.bind('command', function (event) {
-        let sender = event.getSender(),
+        const sender = event.getSender(),
             command = $.jsString(event.getCommand(), '').toLowerCase(),
             args = $.jsArgs(event.getArgs());
 
             if (command === 'prediction') {
                 let handled = false;
                 if (args.length > 0) {
-                    let action = args[0].toLowerCase();
-                    let subaction = args.length > 1 ? args[1].toLowerCase() : null;
+                    const action = args[0].toLowerCase();
+                    const subaction = args.length > 1 ? args[1].toLowerCase() : null;
                     if (action === 'open') {
                         handled = true;
                         if (subaction === 'example') {
                             $.say($.whisperPrefix(sender) + $.lang.get('predictionhandler.open.example'));
                         } else if (args.length < 5 || args[2].trim().length === 0 || args[3].trim().length === 0 || args[4].trim().length === 0) {
                             $.say($.whisperPrefix(sender) + $.lang.get('predictionhandler.open.usage'));
+                        } else if (args.length > 13) {
+                            $.say($.whisperPrefix(sender) + $.lang.get('predictionhandler.open.toomanyoptions'));
                         } else {
                             try {
-                                let choices = new Packages.java.util.ArrayList();
+                                const choices = new Packages.java.util.ArrayList();
 
                                 for (let i = 3; i < args.length; i++) {
                                     choices.add($.javaString(args[i]));
                                 }
 
-                                let response = $.helix.createPrediction($.javaStrimg(args[2]), $.duration(args[1]), choices);
+                                const response = $.helix.createPrediction($.javaStrimg(args[2]), $.duration(args[1]), choices);
 
-                                if (response.has("data")) {
-
-                                } else if (response.has("message")) {
-                                    $.log.error(response.getString("message"));
+                                if (response.has('data') && response.getJSONArray('data').length() > 0) {
+                                    isCommandPrediction = true;
+                                } else if (response.has('message')) {
+                                    $.log.error(response.getString('message'));
+                                } else {
+                                    $.log.error('!prediction open - unknown failure ## ' + response.toString());
                                 }
                             } catch (e) {
                                 $.log.error(e);
@@ -72,5 +190,6 @@
     */
     $.bind('initReady', function () {
         $.registerChatCommand('./handlers/predictionHandler.js', 'prediction', $.PERMISSION.Admin);
+        $.registerChatSubcommand('prediction', 'open', $.PERMISSION.Admin);
     });
 })();
