e4e23056417e5b8695c0ebdc2f91a843a6119003
gmt2001 <garymt2001@hotmail.com>
2023-02-26 18:49:24 -0500

    Fix wsEvent wth ro closes PhantomBot/PhantomBot#3093

--- source/tv/phantombot/panel/WsAlertsPollsHandler.java
+++ source/tv/phantombot/panel/WsAlertsPollsHandler.java
@@ -79,41 +79,14 @@ public class WsAlertsPollsHandler implements WsFrameHandler {
                 com.gmt2001.Console.debug.println(jso.toString());
             }
 
-            if (jso.has("socket_event")) {
-                handleSocketEvent(ctx, frame, jso);
+            if (jso.has("pollState")) {
+                handlePollState(ctx, frame, jso);
             }
         }
     }
 
-    private void handleSocketEvent(ChannelHandlerContext ctx, WebSocketFrame frame, JSONObject jso) {
-        String script = jso.getString("script");
-        String arguments = jso.getJSONObject("args").optString("arguments");
-        JSONArray jsonArray = jso.getJSONObject("args").optJSONArray("args");
-        String uniqueID = jso.has("socket_event") ? jso.getString("socket_event") : "";
-
-        JSONStringer jsonObject = new JSONStringer();
-        List<String> tempArgs = new LinkedList<>();
-        String[] args = null;
-
-        if (jsonArray != null) {
-            for (int i = 0; i < jsonArray.length(); i++) {
-                tempArgs.add(jsonArray.getString(i));
-            }
-        }
-
-        if (!tempArgs.isEmpty()) {
-            int i = 0;
-            args = new String[tempArgs.size()];
-
-            for (String str : tempArgs) {
-                args[i] = str;
-                ++i;
-            }
-        }
-
-        EventBus.instance().postAsync(new WebPanelSocketUpdateEvent(uniqueID, script, arguments, args));
-        jsonObject.object().key("query_id").value(uniqueID).endObject();
-        WebSocketFrameHandler.sendWsFrame(ctx, frame, WebSocketFrameHandler.prepareTextWebSocketResponse(jsonObject.toString()));
+    private void handlePollState(ChannelHandlerContext ctx, WebSocketFrame frame, JSONObject jso) {
+        EventBus.instance().postAsync(new WebPanelSocketUpdateEvent("pollState", "./systems/pollSystem.js", null, null));
     }
 
     public void sendJSONToAll(String jsonString) {
