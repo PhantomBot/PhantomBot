362c39a93d90b89e8655cbc5e93d362440c8bcb7
gmt2001 <garymt2001@hotmail.com>
2023-03-15 19:04:46 -0400

    Move lang functions to bootstrap Fix !mute not printing success emssage to chat before applying

--- javascript-source/core/lang.js
+++ javascript-source/core/lang.js
@@ -18,117 +18,16 @@
 /**
  * lang.js
  *
- * Provide a language API
- * Use the $.lang API
- *
- * NOTE: Reading from/writing to the lang data directly is not possbile anymore!
- * Use the register(), exists() and get() functions!
+ * Handles lang commands
  */
 (function() {
-    var data = [],
-        curLang = $.jsString($.inidb.exists('settings', 'lang') ? $.inidb.get('settings', 'lang') : 'english');
-
-    /**
-     * @function load
-     */
-    function load(force) {
-        $.bot.loadScriptRecursive('./lang/english', true, (force ? force : false));
-        if (curLang !== 'english') {
-            $.bot.loadScriptRecursive('./lang/' + curLang, true, (force ? force : false));
-        }
-
-        if ($.isDirectory('./scripts/lang/custom')) {
-            $.bot.loadScriptRecursive('./lang/custom', true, (force ? force : false));
-        }
-
-        // Set "response_@chat" to true if it hasn't been set yet, so the bot isn't muted when using a fresh install
-        if (!$.inidb.exists('settings', 'response_@chat')) {
-            $.setIniDbBoolean('settings', 'response_@chat', true);
-        }
-    }
-
-    /**
-     * @function register
-     * @export $.lang
-     * @param {string} key
-     * @param {string} string
-     */
-    function register(key, string) {
-        if (key && string) {
-            data[key.toLowerCase()] = string;
-        }
-        if (key && string.length === 0) {
-            data[key.toLowerCase()] = '<<EMPTY_PLACEHOLDER>>';
-        }
-    }
-
-    /**
-     * @function get
-     * @export $.lang
-     * @param {string} key
-     * @returns {string}
-     */
-    function get(key) {
-        var string = data[key.toLowerCase()],
-            i;
-
-        if (string === undefined) {
-            $.log.warn('Lang string for key "' + key + '" was not found.');
-            return '';
-        }
-
-        if (string === '<<EMPTY_PLACEHOLDER>>') {
-            return '';
-        }
-
-        for (i = 1; i < arguments.length; i++) {
-            while (string.indexOf("$" + i) >= 0) {
-                string = string.replace("$" + i, arguments[i]);
-            }
-        }
-        return string;
-    }
-
-    /**
-     * @function paramCount
-     * @export $.lang
-     * @param {string} key
-     * @returns {Number}
-     */
-    function paramCount(key) {
-        var string = data[key.toLowerCase()],
-            i,
-            ctr = 0;
-
-        if (!string) {
-            return 0;
-        }
-
-        for (i = 1; i < 99; i++) {
-            if (string.indexOf("$" + i) >= 0) {
-                ctr++;
-            } else {
-                break;
-            }
-        }
-        return ctr;
-    }
-
-    /**
-     * @function exists
-     * @export $.lang
-     * @param {string} key
-     * @returns {boolean}
-     */
-    function exists(key) {
-        return key !== undefined && key !== null && data[key.toLowerCase()] !== undefined && data[key.toLowerCase()] !== null;
-    }
+    $.getSetIniDbBoolean('settings', 'response_@chat', true);
 
     /**
      * @event command
      */
     $.bind('command', function(event) {
-        var sender = event.getSender().toLowerCase(),
+        let sender = event.getSender().toLowerCase(),
             command = event.getCommand(),
             args = event.getArgs(),
             action = args[0],
@@ -139,15 +38,14 @@
          */
         if (command.equalsIgnoreCase('lang')) {
             if (!action) {
-                $.say($.whisperPrefix(sender) + get('lang.curlang', curLang));
+                $.say($.whisperPrefix(sender) + get('lang.curlang', $.jsString($.getSetIniDbString('settings', 'lang', 'english'))));
             } else {
                 action = action.toLowerCase();
                 if (!$.fileExists('./scripts/lang/' + action + '/main.js')) {
                     $.say($.whisperPrefix(sender) + get('lang.lang.404'));
                 } else {
                     $.inidb.set('settings', 'lang', action);
-                    curLang = action;
-                    load(true);
+                    $.lang.load(true);
                     $.say($.whisperPrefix(sender) + get('lang.lang.changed', action));
                 }
             }
@@ -160,8 +58,13 @@
             inversedState = !$.getIniDbBoolean('settings', 'response_@chat');
 
             $.setIniDbBoolean('settings', 'response_@chat', inversedState);
-            $.reloadMisc();
-            $.say($.whisperPrefix(sender) + (inversedState ? get('lang.response.enabled') : get('lang.response.disabled')));
+            if (!inversedState) {
+                $.say($.whisperPrefix(sender) + get('lang.response.disabled'));
+                $.reloadMisc();
+            } else {
+                $.reloadMisc();
+                $.say($.whisperPrefix(sender) + get('lang.response.enabled'));
+            }
         }
 
         /**
@@ -184,15 +87,4 @@
         $.registerChatCommand('./core/lang.js', 'mute', $.PERMISSION.Admin);
         $.registerChatCommand('./core/lang.js', 'toggleme', $.PERMISSION.Admin);
     });
-
-    /** Export functions to API */
-    $.lang = {
-        exists: exists,
-        get: get,
-        register: register,
-        paramCount: paramCount
-    };
-
-    // Run the load function to enable modules, loaded after lang.js, to access the language strings immediately
-    load();
 })();
