71df4d58b24197e7ac1a95618d5dcea93182ced5
gmt2001 <garymt2001@hotmail.com>
2023-05-20 20:05:17 -0400

    Add memory tracking to stress tools Add startup/shutdown logging to stress tools

--- development-resources/javascript-source/custom/stresstools.js
+++ development-resources/javascript-source/custom/stresstools.js
@@ -24,7 +24,37 @@
     let count = 0;
     let start = Packages.java.time.Instant.now();
 
+    function doStatus() {
+        let memHeap = Packages.com.gmt2001.Reflect.getHeapMemoryUsage();
+        let memNonHeap = Packages.com.gmt2001.Reflect.getNonHeapMemoryUsage();
+        let line1 = 'uptime=' + Packages.java.time.Duration.between(start, Packages.java.time.Instant.now()).toString();
+        let line2 = 'numMessages=' + numMessages + ' <> numMessagesExt=' + numMessagesExt;
+        let line3 = 'present=' + $.users.length + ' <> seen=' + count;
+        let line4 = 'Heap used=' + Packages.org.apache.commons.io.FileUtils.byteCountToDisplaySize(memHeap.getUsed())
+            + ' <> committed=' + Packages.org.apache.commons.io.FileUtils.byteCountToDisplaySize(memHeap.getCommitted());
+        let line5 = 'Non-Heap used=' + Packages.org.apache.commons.io.FileUtils.byteCountToDisplaySize(memNonHeap.getUsed())
+            + ' <> committed=' + Packages.org.apache.commons.io.FileUtils.byteCountToDisplaySize(memNonHeap.getCommitted());
+        $.consoleLn('');
+        $.consoleLn(line1);
+        $.consoleLn(line2);
+        $.consoleLn(line3);
+        $.consoleLn(line4);
+        $.consoleLn(line5);
+        $.consoleLn('');
+        $.log.file('stress', '');
+        $.log.file('stress', line1);
+        $.log.file('stress', line2);
+        $.log.file('stress', line3);
+        $.log.file('stress', line4);
+        $.log.file('stress', line5);
+    }
+
+    $.consoleLn('Starting stress tools');
+    $.log.file('stress', 'Starting stress tools');
+
     $.inidb.RemoveFile('stress_seen');
+
+    doStatus();
 
     let pinger = setInterval(function() {
         $.consoleLn('PING');
@@ -32,18 +62,7 @@
     }, 300000);
 
     let status = setInterval(function() {
-        let line1 = 'uptime=' + Packages.java.time.Duration.between(start, Packages.java.time.Instant.now()).toString();
-        let line2 = 'numMessages=' + numMessages + ' <> numMessagesExt=' + numMessagesExt;
-        let line3 = 'present=' + $.users.length + ' <> seen=' + count;
-        $.consoleLn('');
-        $.consoleLn(line1);
-        $.consoleLn(line2);
-        $.consoleLn(line3);
-        $.consoleLn('');
-        $.log.file('stress', '');
-        $.log.file('stress', line1);
-        $.log.file('stress', line2);
-        $.log.file('stress', line3);
+        doStatus();
     }, 600000);
 
     $.bind('ircModeration', function (event) {
@@ -58,5 +77,17 @@
             $.inidb.set('stress_seen', event.getSender(), 'true');
             count++;
         }
+    });
+
+    $.bind('shutdown', function () {
+        clearInterval(pinger);
+        clearInterval(status);
+        doStatus();
+        $.consoleLn('');
+        $.consoleLn('End of session');
+        $.consoleLn('');
+        $.log.file('stress', '');
+        $.log.file('stress', 'End of session');
+        $.log.file('stress', '');
     });
 })();
