8b72a666eb52377e8a8434909dbbdd3bc09d5fb9
gmt2001 <garymt2001@hotmail.com>
2023-08-02 21:36:40 -0400

    Convert PanelUser to full JOOQ POJO

--- source/tv/phantombot/panel/PanelUser/PanelUserHandler.java
+++ source/tv/phantombot/panel/PanelUser/PanelUserHandler.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016-2023 phantombot.github.io/PhantomBot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package tv.phantombot.panel.PanelUser;
 
 import java.util.Arrays;
@@ -297,7 +313,7 @@ public final class PanelUserHandler {
             user.setLastLoginNOW();
 
             try {
-            user.update();
+            user.doupdate();
             } catch (Exception ex) {
                 com.gmt2001.Console.err.printStackTrace(ex);
             }
@@ -373,7 +389,7 @@ public final class PanelUserHandler {
         user.setPassword(newPassword);
 
         try {
-            user.update();
+            user.doupdate();
         } catch (Exception ex) {
             com.gmt2001.Console.err.printStackTrace(ex);
             return PanelMessage.SaveError;
@@ -442,7 +458,7 @@ public final class PanelUserHandler {
      * Only existing and {@link PanelUser#canBeEdited() editable} {@link PanelUser panel users} can be deleted
      * @param username The user's name which should be deleted
      * @return The fitting {@link PanelMessage panel message}
-     * @see PanelUser#delete() User deletion
+     * @see PanelUser#dodelete() User deletion
      * @see PanelUser#LookupByUsername(String) exists
      */
     public static PanelMessage deleteUser(String username) {
@@ -456,7 +472,7 @@ public final class PanelUserHandler {
         }
 
         try {
-            user.delete();
+            user.dodelete();
         } catch (Exception ex) {
             com.gmt2001.Console.err.printStackTrace(ex);
             return PanelMessage.SaveError;
@@ -543,7 +559,7 @@ public final class PanelUserHandler {
         }
 
         try {
-            user.update();
+            user.doupdate();
         } catch (Exception ex) {
             com.gmt2001.Console.err.printStackTrace(ex);
             return PanelMessage.SaveError;
@@ -589,7 +605,7 @@ public final class PanelUserHandler {
         String password = user.generateNewPassword();
 
         try {
-            user.update();
+            user.doupdate();
         } catch (Exception ex) {
             com.gmt2001.Console.err.printStackTrace(ex);
             return PanelMessage.SaveError;
