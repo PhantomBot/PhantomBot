e3ce0281a6d2bccbca3c46fce3b939234d645062
gmt2001 <garymt2001@hotmail.com>
2023-08-03 21:37:53 -0400

    Convert TableVersionRecord to Record2

--- source/com/gmt2001/datastore2/meta/TableVersionRecord.java
+++ source/com/gmt2001/datastore2/meta/TableVersionRecord.java
@@ -16,21 +16,17 @@
  */
 package com.gmt2001.datastore2.meta;
 
-import org.jooq.Field;
-import org.jooq.Record1;
-import org.jooq.Record2;
-import org.jooq.Row2;
 import org.jooq.Table;
-import org.jooq.impl.UpdatableRecordImpl;
 
 import com.gmt2001.datastore2.Datastore2;
+import com.gmt2001.datastore2.records.Record2;
 
 /**
  * A record in {@link TableVersion} denoting the DDL version of a managed Datastore2 table
  *
  * @author gmt2001
  */
-public final class TableVersionRecord extends UpdatableRecordImpl<TableVersionRecord> implements Record2<String, Long> {
+public final class TableVersionRecord extends Record2<TableVersionRecord, String, Long> {
     /**
      * Version of this record implementation
      */
@@ -40,7 +36,8 @@ public final class TableVersionRecord extends UpdatableRecordImpl<TableVersionRe
      * Constructor
      */
     public TableVersionRecord() {
-        super(TableVersion.instance());
+        super(TableVersion.instance(), () -> TableVersion.instance().TABLE,
+            () -> TableVersion.instance().VERSION);
         this.attach(Datastore2.instance().dslContext().configuration());
     }
 
@@ -76,7 +73,7 @@ public final class TableVersionRecord extends UpdatableRecordImpl<TableVersionRe
      * @param name the name of the table
      */
     public void table(String name) {
-        this.set(0, name);
+        this.value1(name);
     }
 
     /**
@@ -85,7 +82,7 @@ public final class TableVersionRecord extends UpdatableRecordImpl<TableVersionRe
      * @param table the table to retrieve the name from
      */
     public void table(Table<?> table) {
-        this.set(0, table.getName());
+        this.value1(table.getName());
     }
 
     /**
@@ -94,7 +91,7 @@ public final class TableVersionRecord extends UpdatableRecordImpl<TableVersionRe
      * @return the name of the table
      */
     public String table() {
-        return (String) this.get(0);
+        return this.value1();
     }
 
     /**
@@ -103,7 +100,7 @@ public final class TableVersionRecord extends UpdatableRecordImpl<TableVersionRe
      * @param version the version
      */
     public void version(Long version) {
-        this.set(1, version);
+        this.value2(version);
     }
 
     /**
@@ -112,107 +109,7 @@ public final class TableVersionRecord extends UpdatableRecordImpl<TableVersionRe
      * @return the version
      */
     public Long version() {
-        return (Long) this.get(1);
-    }
-
-    /**
-     * The primary key for this record, which is {@link #table()}
-     *
-     * @return the primary key
-     */
-    @Override
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public Record1<String> key() {
-        return (Record1) super.key();
-    }
-
-    @Override
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public Row2<String, Long> fieldsRow() {
-        return (Row2) super.fieldsRow();
-    }
-
-    @Override
-    @SuppressWarnings({"unchecked", "rawtypes"})
-    public Row2<String, Long> valuesRow() {
-        return (Row2) super.valuesRow();
-    }
-
-    /**
-     * The {@link Field} which holds the name of the managed table whose version is stored in this record
-     *
-     * @return the field
-     */
-    @Override
-    public Field<String> field1() {
-        return TableVersion.instance().TABLE;
-    }
-
-    /**
-     * The {@link Field} which holds the version of the managed table
-     *
-     * @return the field
-     */
-    @Override
-    public Field<Long> field2() {
-        return TableVersion.instance().VERSION;
-    }
-
-    /**
-     * The name of the managed table whose version is stored in this record
-     *
-     * @return the name of the table
-     */
-    @Override
-    public String value1() {
-        return this.table();
-    }
-
-    /**
-     * The version of the managed table
-     *
-     * @return the version
-     */
-    @Override
-    public Long value2() {
-        return this.version();
-    }
-
-    /**
-     * Sets the name of the managed table whose version is stored in this record
-     *
-     * @param name the name of the table
-     * @return {@code this}
-     */
-    @Override
-    public Record2<String, Long> value1(String name) {
-        this.table(name);
-        return this;
-    }
-
-    /**
-     * Sets the version of the managed table
-     *
-     * @param version the version
-     * @return this
-     */
-    @Override
-    public Record2<String, Long> value2(Long version) {
-        this.version(version);
-        return this;
-    }
-
-    /**
-     * Sets the name and version of the managed table whose version is stored in this record
-     *
-     * @param name the name of the table
-     * @param version the version
-     * @return {@code this}
-     */
-    public Record2<String, Long> values(String name, Long version) {
-        this.value1(name);
-        this.value2(version);
-        return this;
+        return this.value2();
     }
 
     /**
@@ -222,29 +119,8 @@ public final class TableVersionRecord extends UpdatableRecordImpl<TableVersionRe
      * @param version the version
      * @return {@code this}
      */
-    public Record2<String, Long> values(Table<?> table, Long version) {
+    public org.jooq.Record2<String, Long> values(Table<?> table, Long version) {
         this.table(table);
-        this.value2(version);
-        return this;
-    }
-
-    /**
-     * The name of the managed table whose version is stored in this record
-     *
-     * @return the name of the table
-     */
-    @Override
-    public String component1() {
-        return this.table();
-    }
-
-    /**
-     * The version of the managed table
-     *
-     * @return the version
-     */
-    @Override
-    public Long component2() {
-        return this.version();
+        return this.value2(version);
     }
 }
