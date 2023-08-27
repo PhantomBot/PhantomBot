8054f35daddd69b60381264d43a4e64166286ad7
gmt2001 <garymt2001@hotmail.com>
2023-08-27 19:33:33 -0400

    Explicitly suppress ping error

--- javascript-source/init.js
+++ javascript-source/init.js
@@ -647,7 +647,7 @@
 
                     // Check if the command exists or if the module is disabled or if the command is restricted.
                     if (!$.commandExists(command) || !isModuleEnabled($.getCommandScript(command)) || !$.commandRestrictionMet(command, subCommand)) {
-                        if (!event.isHandeled()) {
+                        if (!event.isHandeled() && !command.equalsIgnoreCase('pbinternalping')) {
                             $.log.error("Command doesn't exist or is disabled/restricted: " + command);
                         }
                         return;
