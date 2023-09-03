431c3068c58e50931f82f9e28e1623969916fce3
gmt2001 <garymt2001@hotmail.com>
2023-09-03 08:49:46 -0400

    Rename namespace

--- source/com/gmt2001/module/CoreModule.java
+++ source/com/gmt2001/module/CoreModule.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.gmt2001.modules;
+package com.gmt2001.module;
 
 /**
  * A bot module which is a core module
--- source/com/gmt2001/module/Module.java
+++ source/com/gmt2001/module/Module.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.gmt2001.modules;
+package com.gmt2001.module;
 
 import tv.phantombot.event.Event;
 import tv.phantombot.event.command.CommandEvent;
