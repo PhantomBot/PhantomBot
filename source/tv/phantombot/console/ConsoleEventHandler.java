1f2448b011d1311490ea4917ec1442a17bc0bf2c
Branden <18152900+BrandenB@users.noreply.github.com>
2023-10-03 19:15:26 -0300

    - Added missing import

--- source/tv/phantombot/console/ConsoleEventHandler.java
+++ source/tv/phantombot/console/ConsoleEventHandler.java
@@ -27,6 +27,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
 import org.json.JSONException;
 import org.json.JSONObject;
