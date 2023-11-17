74d067b0944519ba264f8182e29072cf54d25c4e
gmt2001 <garymt2001@hotmail.com>
2023-11-16 19:31:45 -0500

    Revert "Fixed poll socket creation"
    This reverts commit 6c1a932fc0d0eef28fb4cdfdaff52ea54708586c.

--- source/tv/phantombot/panel/WsAlertsPollsHandler.java
+++ source/tv/phantombot/panel/WsAlertsPollsHandler.java
@@ -84,7 +84,7 @@ public class WsAlertsPollsHandler implements WsFrameHandler {
     }
 
     private void handlePollState(ChannelHandlerContext ctx, WebSocketFrame frame, JSONObject jso) {
-        EventBus.instance().postAsync(new WebPanelSocketUpdateEvent("pollState", "./systems/pollSystem.js", new String[], false));
+        EventBus.instance().postAsync(new WebPanelSocketUpdateEvent("pollState", "./systems/pollSystem.js", null, null));
     }
 
     public void sendJSONToAll(String jsonString) {
