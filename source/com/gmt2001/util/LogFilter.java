ba88c53e3af485d3d00f9e56cc30166835aa90d2
gmt2001 <garymt2001@hotmail.com>
2025-04-26 16:00:39 -0400

    Fix LogFilter adding *** everywhere

--- source/com/gmt2001/util/LogFilter.java
+++ source/com/gmt2001/util/LogFilter.java
@@ -26,26 +26,34 @@ public class LogFilter {
 
     public static String filter(String message) {
         if (message.contains(CaselessProperties.instance().getProperty("oauth", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("oauth", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("oauth", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("apioauth", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("apioauth", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("apioauth", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("refresh", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("refresh", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("refresh", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("apirefresh", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("apirefresh", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("apirefresh", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("apptoken", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("apptoken", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("apptoken", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("discord_token", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("discord_token", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("discord_token", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("streamlabskey", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("streamlabskey", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("streamlabskey", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("youtubekey", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("youtubekey", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("youtubekey", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("clientid", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("clientid", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("clientid", ""), "***");
         } else if (message.contains(CaselessProperties.instance().getProperty("clientsecret", ""))) {
-            message = message.replace(CaselessProperties.instance().getProperty("clientsecret", ""), "***");
+            message = replace(message, (CaselessProperties.instance().getProperty("clientsecret", ""), "***");
         }
+        return message;
+    }
+
+    private static String replace(String message, String find, String replace) {
+        if (!find.isBlank()) {
+            message = message.replace(find, replace);
+        }
+
         return message;
     }
 }
