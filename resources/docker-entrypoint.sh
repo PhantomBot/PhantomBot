6e7d4b4675a644bccaa6cc000ed254a74b2c8276
gmt2001 <garymt2001@hotmail.com>
2025-06-11 17:59:52 -0400

    Fix Docker not always creating directories Fix Docker not creating script/lang directories

--- resources/docker-entrypoint.sh
+++ resources/docker-entrypoint.sh
@@ -9,10 +9,11 @@ if [[ ${UID+x} && ${GID+x} ]]; then
 	fi
 fi
 
+mkdir -p /opt/PhantomBot_data/logs /opt/PhantomBot_data/dbbackup /opt/PhantomBot_data/addons /opt/PhantomBot_data/config /opt/PhantomBot_data/gameslist /opt/PhantomBot_data/scripts/custom /opt/PhantomBot_data/scripts/discord /opt/PhantomBot_data/scripts/lang
+touch /opt/PhantomBot_data/gameslist/gamesList.txt
+
 # allow the container to be started with `--user`
 if [ "$(id -u)" = '0' -a ! -v ALLOW_ROOT ]; then
-	mkdir -p /opt/PhantomBot_data/logs /opt/PhantomBot_data/dbbackup /opt/PhantomBot_data/addons /opt/PhantomBot_data/config /opt/PhantomBot_data/gameslist
-	touch /opt/PhantomBot_data/gameslist/gamesList.txt
 	chown -R phantombot:phantombot /opt/PhantomBot_data;
 	find /opt/PhantomBot \! -type l \! -user phantombot -exec chown phantombot:phantombot '{}' +
 	find /opt/PhantomBot_data \! -type l \! -user phantombot -exec chown phantombot:phantombot '{}' +
